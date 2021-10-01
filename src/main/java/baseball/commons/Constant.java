7c9100cdad454a490202907b628a5c6bd8bf88c4
Raemin KANG <foals18@gmail.com>
2021-10-01 14:06:36 +0900

    feat: 상대방(컴퓨터) 난수 생성
    * 제공받은 Randoms API를 이용하여 난수 생성
    * 공통 상수 클래스 생성
    
    BREAKING CHANGE: Pitcher.java - pitching()

---
+++ src/main/java/baseball/commons/Constant.java
@@ -0,0 +1,22 @@
+/*
+ * Constant.java
+ * java-baseball-precourse
+ *
+ * Version 0.1
+ *
+ * Created by 강래민 on 01/10/2021.
+ *
+ * Copyright © 2021 raemerrr. All rights reserved.
+ */
+
+package baseball.commons;
+
+public final class Constant {
+
+    private Constant() {
+    }
+
+    public static Integer GAME_PITCHING_COUNT = 3;
+    public static Integer GAME_BALL_START = 1;
+    public static Integer GAME_BALL_END = 9;
+}
---
+++ src/main/java/baseball/utils/Pitcher.java
@@ -0,0 +1,38 @@
+/*
+ * Pitcher.java
+ * java-baseball-precourse
+ *
+ * Version 0.1
+ *
+ * Created by 강래민 on 01/10/2021.
+ *
+ * Copyright © 2021 raemerrr. All rights reserved.
+ */
+
+package baseball.utils;
+
+
+import baseball.commons.Constant;
+import nextstep.utils.Randoms;
+
+public class Pitcher {
+
+    private static final Pitcher PITCHER = new Pitcher();
+
+    private Pitcher() {
+    }
+
+    /**
+     * 상대방(컴퓨터) 난수 생성
+     */
+    public static String pitching() {
+        String result = "";
+        while (result.length() < Constant.GAME_PITCHING_COUNT) {
+            Integer num = Randoms.pickNumberInRange(Constant.GAME_BALL_START, Constant.GAME_BALL_END);
+            if (!result.contains(num.toString())) {
+                result += num.toString();
+            }
+        }
+        return result;
+    }
+}
