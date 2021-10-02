7b9deb99cd358ca2e5ff1760764c367894ce740d
uyoung <uyoung5410@gmail.com>
2021-10-03 07:34:57 +0900

    클래스명 변경

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -4,7 +4,7 @@ import nextstep.utils.Randoms;
 
 import java.util.*;
 
-public class BaseBallNumber {
+public class BaseballNumber {
 
     private static final int BALL_MIN_NUMBER = 1;
     private static final int BALL_MAX_NUMBER = 9;
@@ -12,19 +12,19 @@ public class BaseBallNumber {
 
     private final List<Integer> ballNumbers;
 
-    public BaseBallNumber(String numberString) {
+    public BaseballNumber(String numberString) {
         validateOnlyNumber(numberString);
         validateSize(numberString);
         validateDuplicate(numberString);
         this.ballNumbers = toNumbers(numberString);
     }
 
-    public static BaseBallNumber generate() {
+    public static BaseballNumber generate() {
         Set<String> numbersSet = new LinkedHashSet<>();
         while (numbersSet.size() != BASEBALL_SIZE) {
             numbersSet.add(Randoms.pickNumberInRange(BALL_MIN_NUMBER, BALL_MAX_NUMBER) + "");
         }
-        return new BaseBallNumber(String.join("", numbersSet));
+        return new BaseballNumber(String.join("", numbersSet));
     }
 
     private void validateOnlyNumber(String numberString) {
--- src/test/java/baseball/domain/BaseballNumberTest.java
+++ src/test/java/baseball/domain/BaseballNumberTest.java
@@ -5,38 +5,38 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-public class BaseBallNumberTest {
+public class BaseballNumberTest {
     @Test
     void 세자리의_다른숫자로_자동생성하는_기능() {
-        BaseBallNumber baseBallNumber = BaseBallNumber.generate();
-        assertThat(baseBallNumber.toHashSet().size() == 3);
+        BaseballNumber baseballNumber = BaseballNumber.generate();
+        assertThat(baseballNumber.toHashSet().size() == 3);
     }
 
     @Test
     void 규칙에_일치하는_문자열_입력시_잘_생성되는지() {
-        BaseBallNumber baseBallNumber = new BaseBallNumber("123");
-        assertThat(baseBallNumber.toHashSet().size() == 3);
-        assertThat(baseBallNumber.toString()).contains("1", "2", "3");
+        BaseballNumber baseballNumber = new BaseballNumber("123");
+        assertThat(baseballNumber.toHashSet().size() == 3);
+        assertThat(baseballNumber.toString()).contains("1", "2", "3");
     }
 
     @Test
     void 자릿수_맞지않는_문자열_입력시_에러발생_하는지() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new BaseBallNumber("1234"))
+                .isThrownBy(() -> new BaseballNumber("1234"))
                 .withMessageContaining("[ERROR]", "자리의");
     }
 
     @Test
     void 중복된_숫자를_가진_문자열_입력시_에러발생_하는지지() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new BaseBallNumber("112"))
+                .isThrownBy(() -> new BaseballNumber("112"))
                 .withMessageContaining("[ERROR]", "중복");
     }
 
     @Test
     void 숫자가_아닌_문자를_포함한_문자열_입력시_에러발생_하는지() {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> new BaseBallNumber("1a2"))
+                .isThrownBy(() -> new BaseballNumber("1a2"))
                 .withMessageContaining("[ERROR]", "숫자만");
     }
 }
