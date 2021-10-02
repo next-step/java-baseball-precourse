939f1dc67a4b57e49471d099d5454e00ec200531
전주영 <stary7stars@gmail.com>
2021-10-02 19:10:31 +0900

    feat: 게임 진행 부분 구현
    * singleton 형태로 구현
    * 사용자에게 input 받는 메소드
      - 향후 예외처리 추가 예정
    * 전체적인 게임이 계속되는 부분과
       한 판의 게임을 플레이하는 부분을 구분

---
+++ src/main/java/baseball/service/Game.java
@@ -0,0 +1,76 @@
+package baseball.service;
+
+import nextstep.utils.Console;
+import baseball.util.Constant;
+
+public class Game {
+    private static final Game GAME = new Game();
+
+    private Game(){}
+
+    public static Game getGameInstance() {
+        return GAME;
+    }
+
+    // 게임 진행 메소드
+    public void run() {
+        do {
+            // 난수 생성
+            String number = Opponent.getOpponent().makeNumber();
+            // 게임 진행
+            play(number);
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+        }
+        while (isEndFromInput());
+    }
+
+    // 사용자가 문제 맞추는 메소드
+    private void play(String number) {
+        Score score = Score.getInstance();
+        do {
+            // 입력 받기
+            String input = getInputNumber();
+            // 채점하기
+            score.clearScore();
+            score.checkScore(number, input);
+        }
+        while(getResult(score.getStrike(), score.getBall()));
+    }
+
+    // 3개다 맞을 때 false 반환
+    private boolean getResult(int strike, int ball) {
+        if (strike == Constant.NUMBER_SIZE) {
+            System.out.println("3스트라이크");
+            return false;
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return true;
+        }
+        if (strike > 0) System.out.printf("%d스트라이크 ", strike);
+        if (ball > 0) System.out.printf("%d볼", ball);
+        System.out.println();
+        return true;
+    }
+
+
+    // 사용자 input - 숫자 입력
+    private String getInputNumber() {
+        System.out.print("숫자를 입력해주세요: ");
+
+        String input = Console.readLine();
+        // 예외처리
+
+        return input;
+    }
+
+    // 사용자 input - 게임 계속할지 여부 (1이면 true 반환하여 계속 진행)
+    private boolean isEndFromInput() {
+        System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+
+        String input = Console.readLine();
+        // 예외처리 추가
+
+        return input.charAt(0)-'0' == Constant.CONTINUE_GAME;
+    }
+}
