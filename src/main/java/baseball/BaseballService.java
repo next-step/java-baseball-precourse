e230ec9e15d545b4588d9e047225e1fc68b613e7
HK <hyoin01@gmail.com>
2021-10-02 17:59:29 +0900

    feat(service) : add game, baseball repeat
    - game start when input 1, but exit when input 2.
    - game continues until matching numbers.

--- src/main/java/baseball/BaseballService.java
+++ src/main/java/baseball/BaseballService.java
@@ -1,7 +1,42 @@
 package baseball;
 
+import nextstep.utils.Console;
+
 public class BaseballService {
 
+  private final BaseBall baseBall;
+
+  public BaseballService() {
+    this.baseBall = new BaseBall();
+  }
+
   public void execute() {
+    for(String input = "1"; input.equals("1");) {
+      baseball();
+
+      System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+      input = input();
+    }
+  }
+
+  private void baseball() {
+    for(boolean isContinue = true; isContinue;) {
+      System.out.print("숫자를 입력해주세요 : ");
+
+      String input = Console.readLine();
+      isContinue = !baseBall.isAnswer();
+    }
+
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+  }
+
+  private String input() {
+    String input = Console.readLine();
+    if (!"1".equals(input) && !"2".equals(input)) {
+      System.out.println("[ERROR] 잘못된 입력입니다. 게임을 종료합니다.");
+      return "2";
+    }
+
+    return input;
   }
 }
