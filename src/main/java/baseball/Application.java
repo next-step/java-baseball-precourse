8b2637e5804b16ea2f83428eec9ea1dfa50044e9
cus4703 <cus4703@gmail.com>
2021-10-02 18:55:46 +0900

    # main Method 에서 GameMachine 클래스를 이용하여 runningBaseBallGame 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.service.GameMachine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        GameMachine machine = new GameMachine();
+        machine.runningBaseBallGame();
     }
 }
