1b56339f49a3e2a4efabf2888b736c7e642d0fb9
yoonmin-kim <rladbsals23@gmail.com>
2021-10-02 19:40:22 +0900

    feat(Computer) : 4. 같은 수가 같은 자리에 있는지 체크하여 스트라이크 카운트를 측정하고 힌트에 포함한다
    Before:
    
    Computer:
    - getRandomValue() 에서 init(), cleart()도 함께 수행
    
    After:
    
    Computer:
    - getRandomValue() 와 같이 get메소드 일 경우 단순히 radomValue값 만 반환 하도록 수정
    - init(), clear(), getRandomValue() 를 동시에 수행하는 별도의 메소드 추가 => getClearRandomValue()
    - 힌트, 스트라이크, 볼 변수추가 => hint, strike, ball
    - 컴퓨터가 생성한 수와 사용자가 입력한 수를 비교하여 동일값, 동일한 자리인지
      확인하는 메소드 추가 => checkStrike()
    - 두 조건이 만족 할 경우 스트라이크 카운트 증가
    - 힌트생성 메소드 추가 => makeHint()
    
    ComputerTest:
    - 다음의 테스트 추가
    - 컴퓨터가 생성한 값의 모든 수 모든자리를 맞출 경우 3스트라이크 인지 확인
    - "3스트라이크 " 를 출력하는지 확인
    - 두 수, 두 자리만 맞출 경우 2스트라이크 인지 확인
    - "2스트라이크 " 를 출력하는지 확인
    - ConfigBaseball.INPUT_SIZE 가 변경 될 경우 TEST에서도 별도로 변경하지 않아도 되도록 수정
    
    Computer, ComputerTest 수정

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,6 +9,10 @@ import java.util.Set;
 import baseball.config.ConfigBaseball;
 import nextstep.utils.Randoms;
 
+/**
+ * 게임을 위한 랜덤 값 생성
+ * 생성된 랜덤 값 과 사용자 입력 값 검증을 위한 도메인
+ */
 public class Computer {
 	private Set randomSet = new HashSet<Integer>();
 	private List randomValue = new ArrayList();
@@ -15,10 +20,44 @@ public class Computer {
 	private final int PICK_NUMBER_MIN = ConfigBaseball.PICK_NUMBER_MIN;
 	private final int PICK_NUMBER_MAX = ConfigBaseball.PICK_NUMBER_MAX;
 
-	public List getRandomValue() {
+	private String hint = "";
+	private int strike = 0;
+	private int ball = 0;
+
+	public List getClearRandomValue() {
 		clear();
 		init();
-		return randomValue;
+		return getRandomValue();
+	}
+
+	public void checkStrike(List randomValue, List userInput) {
+		if (Arrays.equals(randomValue.toArray(), userInput.toArray())) {
+			setStrike(INPUT_SIZE);
+			return;
+		}
+
+		for (int i = 0; i < INPUT_SIZE; i++) {
+			countStrike((int)randomValue.get(i), (int)userInput.get(i));
+		}
+	}
+
+	private void countStrike(int randomValue, int userInput) {
+		if (randomValue == userInput) {
+			strike++;
+		}
+	}
+
+	public void makeHint() {
+		StringBuilder builder = new StringBuilder();
+
+		if (getStrike() != 0) {
+			builder.append(getStrike()).append("스트라이크 ");
+		}
+
+		if (getBall() != 0) {
+			builder.append(getBall()).append("볼 ");
+		}
+		setHint(builder.toString());
 	}
 
 	private void init() {
@@ -40,6 +79,36 @@ public class Computer {
 		if (!randomValue.isEmpty()) {
 			randomValue.clear();
 		}
+		setHint("");
+		setStrike(0);
+		setBall(0);
 	}
 
+	private void setStrike(int strike) {
+		this.strike = strike;
+	}
+
+	private void setBall(int ball) {
+		this.ball = ball;
+	}
+
+	private void setHint(String hint) {
+		this.hint = hint;
+	}
+
+	public List getRandomValue() {
+		return randomValue;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public String getHint() {
+		return hint;
+	}
 }
--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -1,26 +1,63 @@
 package baseball.domain;
 
+import static baseball.config.ConfigBaseball.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
 
+	private Computer computer = null;
+
+	@BeforeEach
+	void init() {
+		computer = new Computer();
+	}
+
 	@Test
 	@DisplayName("1부터 9까지 중복되지 않는 임의의 수 3개를 선택한다.")
 	void 게임시작_직후_3개의수생성() {
-		Computer computer = new Computer();
+		List randomValue = computer.getClearRandomValue();
 
-		List randomValue = computer.getRandomValue();
-
-		assertThat(randomValue.size()).isEqualTo(3);
+		assertThat(randomValue.size()).isEqualTo(INPUT_SIZE);
 		assertThat(randomValue.get(0)).isNotEqualTo(randomValue.get(1));
 		assertThat(randomValue.get(1)).isNotEqualTo(randomValue.get(2));
 		assertThat(randomValue.get(2)).isNotEqualTo(randomValue.get(0));
 
+	}
+
+	@Test
+	@DisplayName("같은 수가 같은 자리에 있는지 체크하여 쓰리스트라이크 확인")
+	void 쓰리스트라이크_확인() {
+		ArrayList randomValue = (ArrayList)computer.getClearRandomValue();
+		ArrayList userInput = (ArrayList)randomValue.clone();
+
+		computer.checkStrike(randomValue, userInput);
+		assertThat(computer.getStrike()).isEqualTo(INPUT_SIZE);
+
+		computer.makeHint();
+		assertThat(computer.getHint()).isEqualTo(INPUT_SIZE + "스트라이크 ");
+	}
+
+	@Test
+	@DisplayName("같은 수가 같은 자리에 있는지 체크하여 투스트라이크 확인")
+	void 투스트라이크_확인() {
+		ArrayList randomValue = (ArrayList)computer.getClearRandomValue();
+		ArrayList userInput = (ArrayList)randomValue.clone();
+		userInput.set(0, 11);
+
+		computer.checkStrike(randomValue, userInput);
+		assertThat(computer.getStrike()).isEqualTo(INPUT_SIZE - 1);
+		assertThat(Arrays.equals(randomValue.toArray(), userInput.toArray())).isFalse();
+
+		computer.makeHint();
+		assertThat(computer.getHint()).isEqualTo(INPUT_SIZE - 1 + "스트라이크 ");
 	}
 
 }
