58a044525be16c59dca8e053f26fa6f9b3351076
yoonmin-kim <rladbsals23@gmail.com>
2021-10-02 20:04:27 +0900

    refactor(User) : getter와 기능메소드 구분, INPUT_SIZE 적용
    Before:
    
    User:
    - getInput() 에서 clear()도 함께수행
    - inputNumber() 10줄 넘어감
    
    After:
    
    User:
    - getInput() 은 단순히 input 값 만 반환하도록 수정
    - clear() 동시에 수행하는 별도의 메소드 추가 => clearInputReadLine()
    - inputNumber() 10줄 넘어가지 않도록 메소드로 분리 및 변수 인라인 적용
    - final 변수 추가 => READLINE_MESSAGE
    
    ConfigBaseball:
    - ERROR_MESSAGE 에서 "3개의 숫자" -> (INPUT_SIZE + "개의 숫자") 로 변경
    - final 변수 추가 => READLINE_MESSAGE
    
    User, ConfigBaseball 수정

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -7,37 +7,44 @@ import java.util.List;
 import baseball.config.ConfigBaseball;
 import nextstep.utils.Console;
 
+/**
+ * 사용자의 입력기능, 입력값 오류검증을 위한 도메인
+ */
 public class User {
 
 	private List input = new ArrayList<Integer>();
 	private List numberList = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
 	private boolean isInputted = false;
+	private final String READLINE_MESSAGE = ConfigBaseball.READLINE_MESSAGE;
 	private final String ERROR_MESSAGE = ConfigBaseball.ERROR_MESSAGE;
 	private final int INPUT_SIZE = ConfigBaseball.INPUT_SIZE;
 
-	public List getInput() {
+	public List clearInputReadLine() {
 		clear();
 		while (!isInputted) {
 			isInputted = inputNumber();
 		}
 
-		return input;
+		return getInput();
 	}
 
 	private boolean inputNumber() {
-		System.out.print("숫자를 입력해주세요 : ");
-		String readLine = Console.readLine();
+		String readLine = getReadLine();
 
-		boolean isValidated = validateReadLine(readLine);
-		if (!isValidated) {
+		if (!validateReadLine(readLine)) {
 			return false;
 		}
 
 		for (int i = 0; input.size() != INPUT_SIZE; i++) {
-			int parseInt = Integer.parseInt(String.valueOf(readLine.charAt(i)));
-			input.add(parseInt);
+			input.add(Integer.parseInt(String.valueOf(readLine.charAt(i))));
 		}
 		return true;
+	}
+
+	private String getReadLine() {
+		System.out.print(READLINE_MESSAGE);
+		String readLine = Console.readLine();
+		return readLine;
 	}
 
 	/**
@@ -86,7 +93,7 @@ public class User {
 	}
 
 	/**
-	 * 입력한 값의 크기가 3이 아닐 경우 [ERROR]메시지 출력
+	 * 입력한 값의 크기가 INPUT_SIZE 가 아닐 경우 [ERROR]메시지 출력
 	 */
 	private boolean validateLength(String readLine) {
 		if (readLine.length() != INPUT_SIZE) {
@@ -108,4 +115,7 @@ public class User {
 		isInputted = false;
 	}
 
+	public List getInput() {
+		return input;
+	}
 }
--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import static baseball.config.ConfigBaseball.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
@@ -9,14 +10,17 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
+	/**
+	 * 기능개발용 테스트(UI로직)이며 과제 제출시 삭제예정
+	 */
 	@Test
-	@DisplayName("사용자는 컴퓨터가 생각하고 있는 3개의 숫자를 입력한다.")
+	@DisplayName("사용자는 컴퓨터가 생각하고 있는 3개의 숫자를 입력한다. 잘못 입력할 경우 [ERROR]메시지 출력")
 	void 게임시작_직후_3개의수입력() {
 		User user = new User();
 
-		List input = user.getInput();
+		List input = user.clearInputReadLine();
 
-		assertThat(input.size()).isEqualTo(3);
+		assertThat(input.size()).isEqualTo(INPUT_SIZE);
 
 	}
 
