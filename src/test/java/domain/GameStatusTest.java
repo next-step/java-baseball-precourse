ec2cc7f1824fa228a13f6b364be6dcbe28a2d238
ghkvud2 <41367105+ghkvud2@users.noreply.github.com>
2021-10-03 01:48:43 +0900

    Game 도메인 추가 및 테스트 작성

---
+++ src/test/java/domain/GameStatusTest.java
@@ -0,0 +1,41 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("게임 결과 테스트")
+class GameStatusTest {
+
+	@Test
+	@DisplayName("스트라이크나 볼을 하나도 못맞췄을 때")
+	void isNothing() {
+		Game game = new Game();
+		assertThat(game.getGameStatus()).isEqualTo(GameStatus.NOTHING);
+		assertThat(game.toString()).isEqualTo("낫싱");
+	}
+
+	@Test
+	@DisplayName("스트라이크나 볼을 맞췄을 때")
+	void isIncorrect() {
+		Game game = new Game();
+		game.addStike();
+		game.addStike();
+		game.addBall();
+		assertThat(game.getGameStatus()).isEqualTo(GameStatus.INCORRECT);
+		assertThat(game.toString()).isEqualTo("2스트라이크 1볼");
+	}
+
+	@Test
+	@DisplayName("스트라이크를 모두 맞췄을 때")
+	void isCorrect() {
+		Game game = new Game();
+		game.addStike();
+		game.addStike();
+		game.addStike();
+		assertThat(game.getGameStatus()).isEqualTo(GameStatus.CORRECT);
+		assertThat(game.toString()).isEqualTo("3개의 숫자를 모두 맞히셨습니다! 게임끝");
+	}
+
+}
