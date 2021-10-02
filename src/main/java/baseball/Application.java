f40e5a93cc4cd5bffd175dadfb2fd4edf86f6619
yoonmin-kim <rladbsals23@gmail.com>
2021-10-03 03:47:45 +0900

    feat(Application) : 7. (4, 5, 6)번의 기능이 3스트라이크가 나올때까지 반복 되도록 만든다
    Application:
    - Computer 와 User를 이용하여 3스트라이크가 나올때까지 입력을 받도록 구현
    - 스트라이크와 볼에 대한 힌트 출력
    
    Application 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import java.util.List;
+
+import baseball.domain.Computer;
+import baseball.domain.User;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-    }
+
+	public static void main(String[] args) {
+		Computer computer = new Computer();
+		User user = new User();
+		List randomValue = computer.getClearRandomValue();
+		List userInput = null;
+
+		while (!computer.isGameClear()) {
+			userInput = user.getClearInputReadLine();
+
+			computer.checkStrike(randomValue, userInput);
+			computer.checkBall(randomValue, userInput);
+			computer.checkNothing();
+
+			String hint = computer.makeHint();
+			System.out.println(hint);
+		}
+
+	}
+
 }
