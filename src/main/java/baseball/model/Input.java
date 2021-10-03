eb9209c5004f64475d6a5cf222ece6f7392289f9
gunan <power4889@gmail.com>
2021-10-03 18:57:36 +0900

    feat(Input): 자신의 타겟값에 대한 결과가 스트라이크인지 볼인지 맞추는 기능 추가

--- src/main/java/baseball/model/Input.java
+++ src/main/java/baseball/model/Input.java
@@ -1,8 +1,38 @@
 package baseball.model;
 
+import java.util.Set;
+
 public class Input extends Number {
+
+	private Status status;
+
+	private enum Status {
+		STRIKE, BALL
+	}
 
 	public Input(int index, int value) {
 		super(index, value);
+	}
+
+	public void hit(Set<Target> targets) {
+		for (Target target : targets) {
+			hit(target);
+		}
+	}
+
+	private void hit(Target target) {
+		if (this.equals(target) && this.on(target)) {
+			status = Status.STRIKE;
+		} else if (this.equals(target) && !this.on(target)) {
+			status = Status.BALL;
+		}
+	}
+
+	public boolean isStrike() {
+		return status == Status.STRIKE;
+	}
+
+	public boolean isBall() {
+		return status == Status.BALL;
 	}
 }
