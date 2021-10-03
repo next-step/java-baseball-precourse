f70676ee1f6035f2b8b90442e992498c7ce7bcd7
taeyanglim <taeyang7704@naver.com>
2021-10-03 14:45:08 +0900

    feature: add a SetTestClass

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,43 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void collection_set_size_test() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void collection_set_parameterized_contains_test(int inputNumbers) {
+        assertThat(numbers.contains(inputNumbers)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void collection_set_parameterized_contains_test_with_csv_source(int inputNumber, boolean expectedValue) {
+        assertThat(numbers.contains(inputNumber)).isEqualTo(expectedValue);
+    }
+
+}
