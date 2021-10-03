e185b02116d421aa44073fc385382e825d7cc84b
jongwon_lee <jongwon_lee@tmax.co.kr>
2021-10-04 02:13:31 +0900

    test: StringTest 요구사항 2 테스트 코드 구현
    - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 () 을 제거하고 "1,2"이 반환되는 테스트 코드 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,10 +6,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StringTest {
-	@DisplayName("\"1,2\"를 \",\"로 split했을 때 1과 2로 잘 분리되는지 확인")
+	@DisplayName("\"1,2\"를 ,로 split했을 때 1과 2로 잘 분리되는지 확인")
 	@Test
 	void splitTestForOneTwoWithComma() {
 		assertThat("1,2".split(","))
-			.containsExactly("1", "2");
+			.contains("1", "2");
 	}
+
+	@DisplayName("\"1\"을 , 로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인")
+	@Test
+	void splitTestForOneArrayWithComma() {
+		assertThat("1".split(","))
+			.containsExactly("1");
+	}
+
+	@DisplayName("\"(1,2)\" 값이 주어졌을 때 String의 substring() 메소드를 활용해 () 을 제거하고 \"1,2\"를 반환되는지 확인")
+	@Test
+	void substringTest() {
+		assertThat("(1,2)".substring(1,4))
+			.isEqualTo("1,2");
+	}
+
+
 }
