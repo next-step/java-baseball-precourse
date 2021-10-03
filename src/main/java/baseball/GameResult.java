fcf1f82baf842910d94be3e75b1d13d44e4dc1cb
ghkvud2 <41367105+ghkvud2@users.noreply.github.com>
2021-10-03 15:34:22 +0900

    (feature) Balls
    컴퓨터와 사용자의 Ball 객체 비교 로직 구현 및 테스트

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,72 @@
+package baseball;
+
+public class GameResult {
+
+	private int strike;
+	private int ball;
+
+	private void addStrike() {
+		this.strike++;
+	}
+
+	private void addBall() {
+		this.ball++;
+	}
+
+	public GameResultStatus status() {
+
+		if (strike == 3) {
+			return GameResultStatus.COMPLETE;
+		}
+		if (strike > 0 || ball > 0) {
+			return GameResultStatus.INCOMPLETE;
+		}
+		return GameResultStatus.NOTHING;
+	}
+
+	public void recordScore(BallStatus ballStatus) {
+
+		if (ballStatus == BallStatus.STRIKE) {
+			addStrike();
+		}
+
+		if (ballStatus == BallStatus.BALL) {
+			addBall();
+		}
+	}
+
+	private boolean isNothing() {
+		return strike == 0 && ball == 0;
+	}
+
+	private boolean hasBoth() {
+		return strike > 0 && ball > 0;
+	}
+
+	private boolean hasOnlyStrike() {
+		return strike > 0 && ball == 0;
+	}
+
+	private boolean hasOnlyBall() {
+		return strike == 0 && ball > 0;
+	}
+
+	@Override
+	public String toString() {
+
+		if (hasBoth()) {
+			return String.format("%d스트라이크 %d볼", strike, ball);
+		}
+
+		if (hasOnlyStrike()) {
+			return String.format("%d스트라이크", strike);
+		}
+
+		if (hasOnlyBall()) {
+			return String.format("%d볼", ball);
+		}
+
+		return "낫싱";
+	}
+
+}
---
+++ src/test/java/baseball/BallsTest.java
@@ -0,0 +1,57 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("3개의 볼을 비교하는 테스트")
+class BallsTest {
+
+	private Balls balls;
+
+	@BeforeEach
+	void setUp() {
+		balls = new Balls(Arrays.asList(1, 2, 3));
+	}
+
+	@Test
+	@DisplayName("3스트라이크")
+	void _3_스트라이크() {
+		Balls userBalls = new Balls(Arrays.asList(1, 2, 3));
+		GameResult gameResult = balls.play(userBalls);
+		assertGameResult(gameResult, GameResultStatus.COMPLETE, "3스트라이크");
+	}
+
+	@Test
+	@DisplayName("1스트라이크 2볼")
+	void _1_스트라이크_2볼() {
+		Balls userBalls = new Balls(Arrays.asList(1, 3, 2));
+		GameResult gameResult = balls.play(userBalls);
+		assertGameResult(gameResult, GameResultStatus.INCOMPLETE, "1스트라이크 2볼");
+	}
+
+	@Test
+	@DisplayName("3볼")
+	void _3볼() {
+		Balls userBalls = new Balls(Arrays.asList(3, 1, 2));
+		GameResult gameResult = balls.play(userBalls);
+		assertGameResult(gameResult, GameResultStatus.INCOMPLETE, "3볼");
+	}
+
+	@Test
+	@DisplayName("낫싱")
+	void 낫싱() {
+		Balls userBalls = new Balls(Arrays.asList(4, 5, 6));
+		GameResult gameResult = balls.play(userBalls);
+		assertGameResult(gameResult, GameResultStatus.NOTHING, "낫싱");
+	}
+
+	private void assertGameResult(GameResult gameResult, GameResultStatus status, String message) {
+		assertThat(gameResult.status()).isEqualTo(status);
+		assertThat(gameResult.toString()).isEqualTo(message);
+	}
+}
