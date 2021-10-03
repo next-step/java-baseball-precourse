628daa81857fc0a323f7b5751ae83187288fe4fb
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-03 21:40:00 +0900

    test : 랜덤한 3자리 수 생성 (중복제거)

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -1,12 +1,15 @@
 package baseball.controller;
 
-import baseball.AppConfig;
+import baseball.domain.GameConfig;
 import baseball.service.GameService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static baseball.AppConfig.getGameController;
+import java.util.LinkedHashSet;
+
 import static baseball.AppConfig.getGameService;
+import static baseball.domain.PickNumbers.getPickNumbersSet;
+import static baseball.domain.PickNumbers.makePickNumbers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameControllerTest {
@@ -22,6 +25,30 @@ class GameControllerTest {
 
         assertThat(gameService1).isSameAs(gameController2.getGameService());
         assertThat(gameService2).isSameAs(gameController1.getGameService());
+    }
+
+    @Test
+    @DisplayName("랜덤한 3자리 수 생성 (중복제거)")
+    void makePickNumbersTest() {
+        LinkedHashSet<Integer> pickNumbersSet = new LinkedHashSet<>();
+        int pickNumber = 0;
+
+        boolean isMakePickNumbers = makePickNumbers();
+        if (isMakePickNumbers) {
+            pickNumbersSet = getPickNumbersSet();
+
+            StringBuilder sb = new StringBuilder();
+            for (Integer pick : pickNumbersSet) {
+                sb.append(pick);
+            }
+            pickNumber = Integer.parseInt(sb.toString());
+        }
+
+        System.out.println("pickNumber = " + pickNumber);
+        assertThat(pickNumber).isGreaterThanOrEqualTo(123).isLessThanOrEqualTo(987);
+
+        System.out.println("pickNumbersSet.size() = " + pickNumbersSet.size());
+        assertThat(pickNumbersSet.size()).isEqualTo(GameConfig.PICK_NUMBER_SIZE.getValue());
     }
 
 }
