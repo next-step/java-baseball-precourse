2e62f88b0ae07408cd00e0f86c9807f3e6ba76db
gunan <power4889@gmail.com>
2021-10-03 19:48:57 +0900

    feat: Application.java의 main() 실행 시 Viewer가 실행되도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.view.GameViewer;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-    }
+	public static void main(String[] args) {
+		// TODO 숫자 야구 게임 구현
+		GameViewer viewer = GameViewer.getInstance();
+		viewer.start();
+	}
 }
