7cd2768ce6795ad950dac9ad304815e8577e7f44
kingr <kingr@DESKTOP-OFI41O8>
2021-10-03 18:11:04 +0900

    feat : 컴퓨터 (상대방) 클레스 update 	- 랜덤 3자리 수 생성 	- 스트라이크 카운트 반환 기능 	- 볼 카운트 반환 기능

--- src/main/java/baseball/game/Computer.java
+++ src/main/java/baseball/game/Computer.java
@@ -5,12 +5,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import baseball.util.Static;
+import nextstep.utils.Randoms;
+
 /**
  * @auhor Kim JeongJin <kingroma92@gmail.com>
  * @version 1.0
  * @since 1.0
  * */
 public class Computer {
+	
 	/**
 	 * 초기 컴퓨터가 가질 숫자 저장 List
 	 * */
@@ -29,7 +33,33 @@ public class Computer {
 	 * 랜덤함수를 이용해 3가지의 다른 랜덤 수를 생성한다
 	 * */
 	private void makeRandomValues() {
+		values = new ArrayList<String>();
 		
+		for ( int i = 0 ; i < Static.GAME_MAX_LEN ; i ++ ) {
+			String value = makeOneRandomValue();
+			valueSets.add(value);
+			values.add(value);
+		}
+	}
+	
+	private String makeOneRandomValue() {
+		String value = null;
+		
+		while ( value == null ) {
+			value = getOneDiffRandomValue();
+		}
+		
+		return value;
+	}
+	
+	private String getOneDiffRandomValue() {
+		String value = String.valueOf(Randoms.pickNumberInRange(1, 9)) ;
+		
+		if ( valueSets.contains(value) ) {
+			value = null ; 
+		}
+		
+		return value ;
 	}
 	
 	/**
@@ -39,8 +69,23 @@ public class Computer {
 	 * @return 스크라이크 카운트 수 
 	 * */
 	public int getStrikeCount(String input) {
-		return 0 ; 
+		int count = 0 ;
+		String[] split = input.split("");
+		
+		for ( int i = 0 ; i < Static.GAME_MAX_LEN ; i ++ ) {
+			count += isStrike(values.get(i),split[i]);
+		}
+		
+		return count ; 
 	}
+	
+	private int isStrike(String input1, String input2) {
+		if ( input1.equals(input2) ) {
+			return 1 ;
+		}
+		
+		return 0 ;
+	} 
 	
 	/**
 	 * 입력된 값의 볼 카운트를 반환한다
@@ -49,6 +94,22 @@ public class Computer {
 	 * @return 볼 카운트 수 
 	 * */
 	public int getBallCount(String input) {
+		int count = 0 ; 
+		
+		String[] split = input.split("");
+		
+		for ( int i = 0 ; i < Static.GAME_MAX_LEN ; i ++ ) {
+			count += isBall(split[i], i);
+		}
+			
+		return count ; 
+	}
+	
+	private int isBall(String input, int index) {
+		if ( !values.get(index).equals(input) && valueSets.contains(input) ) {
+			return 1 ; 
+		}
+		
 		return 0 ;
 	}
 }
