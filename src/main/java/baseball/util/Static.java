3530c65dcd6585f22174750ec1831624d8b6ecb8
kingr <kingr@DESKTOP-OFI41O8>
2021-10-03 17:20:03 +0900

    feat : 전역변수 클레스 	- Static.java ( GAME_MAX_LEN )

---
+++ src/main/java/baseball/util/Static.java
@@ -0,0 +1,13 @@
+package baseball.util;
+
+/**
+ * @auhor Kim JeongJin <kingroma92@gmail.com>
+ * @version 1.0
+ * @since 1.0
+ * */
+public class Static {
+	/**
+	 * 야구 게임의 최대 자리수 지정
+	 * */
+	public static final int GAME_MAX_LEN = 3 ;
+}
