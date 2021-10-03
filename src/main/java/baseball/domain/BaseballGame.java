d4c405c6760681da63c5c55cd749b7b514c72577
Jiho Park <jhpar8@gmail.com>
2021-10-04 00:08:28 +0900

    Update Comments

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -12,15 +12,24 @@ public class BaseballGame {
     private int strikeCount;
     private int ballCount;
 
+    /**
+     * 생성자
+     */
     public BaseballGame() {
         makeNewGame();
     }
 
+    /**
+     * 볼카운트 초기화 메소드
+     */
     private void initBallCount(){
         strikeCount = 0;
         ballCount = 0;
     }
 
+    /**
+     * 새로운 게임을 시작하는 메소드
+     */
     private void makeNewGame(){
         do {
             target = createTarget();
@@ -28,6 +37,10 @@ public class BaseballGame {
         } while (!BaseballUtils.isValidInput(target));
     }
 
+    /**
+     * 컴퓨터가 생성하는 랜덤 3자리를 생성한다.
+     * @return String Target
+     */
     private String createTarget(){
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 3; i++){
@@ -37,18 +50,27 @@ public class BaseballGame {
     }
 
 
+    /**
+     * Stike, Ball을 판별하는 메소드
+     * @param targetCharacter
+     * @param inputCharacter
+     */
     private void matching(char targetCharacter, char inputCharacter) {
-        if (targetCharacter == inputCharacter) {
+        if (targetCharacter == inputCharacter) {  // 같다면, Strike
             strikeCount++;
             return;
         }
-        if (target.contains(String.valueOf(inputCharacter))){
+        if (target.contains(String.valueOf(inputCharacter))){  // Stike가 아니라면, Ball인지 체크
             ballCount++;
         }
     }
 
+    /**
+     * 입력 값과 타겟 값과 비교.
+     * @param input
+     */
     private void matchTarget(String input) {
-        initBallCount();
+        initBallCount();  // 객체의 볼 카운트 초기화
         if (target.equals(input)) {
             strikeCount = 3;
             System.out.println(getResultAnswer());
@@ -60,6 +82,10 @@ public class BaseballGame {
         System.out.println(getResultAnswer());
     }
 
+    /**
+     * 결과 값에 대한 응답 메시지 구현 메소드
+     * @return String Answer
+     */
     private String getResultAnswer() {
         if (strikeCount == 0 && ballCount == 0) {
             return "낫싱";
@@ -93,7 +119,7 @@ public class BaseballGame {
     }
 
     /**
-     *
+     * 게임 플레이 메소드
      * @param input
      * @return
      */
@@ -110,6 +136,10 @@ public class BaseballGame {
         return Boolean.TRUE;
     }
 
+    /**
+     * 게임 시작 메소드
+     * @return
+     */
     public Boolean start() {
         do {
             System.out.print("숫자를 입력해주세요 : ");
