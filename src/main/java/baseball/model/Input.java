d0a31de739484f81ccd0ef70d598ef64594fe5a1
gunan <power4889@gmail.com>
2021-10-03 18:52:24 +0900

    feat: 입력된 숫자 하나에 해당하는 Input객체와 정답 하나에 해당하는 Target객체 추가

---
+++ src/main/java/baseball/model/Input.java
@@ -0,0 +1,8 @@
+package baseball.model;
+
+public class Input extends Number {
+
+	public Input(int index, int value) {
+		super(index, value);
+	}
+}
---
+++ src/main/java/baseball/model/Target.java
@@ -0,0 +1,8 @@
+package baseball.model;
+
+public class Target extends Number {
+
+	public Target(int index, int value) {
+		super(index, value);
+	}
+}
