3c6ec2fc37f60d18a3b3ea590a4cf18c4c6681e4
praivesi <praivesi@gmail.com>
2021-10-03 20:53:11 +0900

    feat: Ball 객체 생성시 공의 번호를 검증하는 로직 추가 (기능 목록 1번)

--- src/test/java/baseball/Ball.java
+++ src/test/java/baseball/Ball.java
@@ -1,12 +1,19 @@
 package baseball;
 
 public class Ball {
-    private int location;
-    private int ballNumber;
+    public static final int MIN_BALL_NUMBER = 1;
+    public static final int MAX_BALL_NUMBER = 9;
 
-    public Ball(int location, int ballNumber) {
-        this.location = location;
+    private int ballNumber;
+    private int location;
+
+    public Ball(int ballNumber, int location) {
+        if (ballNumber < MIN_BALL_NUMBER || ballNumber > MAX_BALL_NUMBER) {
+            throw new IllegalArgumentException("공의 번호는 " + MIN_BALL_NUMBER + "과 " + MAX_BALL_NUMBER + " 범위 내의 정수값이어야 합니다.");
+        }
+
         this.ballNumber = ballNumber;
+        this.location = location;
     }
 
     public MATCH_RESULT match(Ball counterBall) {
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BallTest {
 
@@ -11,14 +12,24 @@ public class BallTest {
 
     @BeforeEach
     void setup(){
-        ball = new Ball(1, 3);
+        ball = new Ball(3, 1);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,3,STRIKE", "2,3,BALL", "1,4,NOTHING", "2,5,NOTHING"}, delimiter = ',')
-    void 하나의_공이_주어질_경우_스트라이크_볼_낫싱_여부를_판단한다(int location, int ballNumber, MATCH_RESULT expected){
-        Ball counterBall = new Ball(location, ballNumber);
+    @CsvSource(value = {"3,1,STRIKE", "3,2,BALL", "4,1,NOTHING", "5,2,NOTHING"}, delimiter = ',')
+    void 하나의_공이_주어질_경우_스트라이크_볼_낫싱_여부를_판단한다(int ballNumber, int location, MATCH_RESULT expected){
+        Assertions.assertThat(ball.match(new Ball(ballNumber, location))).isEqualTo(expected);
+    }
 
-        Assertions.assertThat(ball.match(counterBall)).isEqualTo(expected);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 5, 6, 9})
+    void 공의_번호가_1_에서_9_사이의_값이면_Ball_객체_생성시_예외가_발생하지_않는다(int ballNumber){
+        Assertions.assertThatNoException().isThrownBy(() -> new Ball(ballNumber, 0));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 10, 987654321})
+    void 공의_번호가_1_보다_작거나_9보다_크면_Ball_객체_생성시_IllegalArgumentException_예외가_발생한다(int ballNumber){
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new Ball(ballNumber, 0));
     }
 }
