a46f3bf449b911253af690e7bf665099edd7a40c
gunan <power4889@gmail.com>
2021-10-03 19:50:10 +0900

    feat(Controller): 잘못된 입력값에 대한 validation 기능 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -5,6 +5,7 @@ import baseball.dto.ResultDto;
 import baseball.model.Game;
 import baseball.model.Input;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -28,6 +29,11 @@ public class GameController {
 	}
 
 	public ResponseDto check(String number) {
+		try {
+			validate(number);
+		} catch (IllegalArgumentException e) {
+			return ResponseDto.error(e.getMessage());
+		}
 		ResultDto result = game.check(inputsFrom(number));
 		if (result.isEnd()) {
 			return ResponseDto.ok(result.getMessage());
@@ -41,5 +47,34 @@ public class GameController {
 			inputs.add(new Input(inputs.size(), Character.getNumericValue(input)));
 		}
 		return inputs;
+	}
+
+	private void validate(String number) throws IllegalArgumentException {
+		if (!isNumber(number)) {
+			throw new IllegalArgumentException("[ERROR]1~9 사이의 숫자만 입력해주세요");
+		}
+		if (number.length() != 3) {
+			throw new IllegalArgumentException("[ERROR]3개의 숫자를 입력해주세요");
+		}
+		if (!isUniqueNumber(number)) {
+			throw new IllegalArgumentException("[ERROR]중복되지 않은 숫자를 입력해주세요");
+		}
+	}
+
+	private boolean isNumber(String number) {
+		try {
+			Integer.parseInt(number);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	private boolean isUniqueNumber(String number) {
+		Set<Character> set = new HashSet<>();
+		for (char c : number.toCharArray()) {
+			set.add(c);
+		}
+		return set.size() == 3;
 	}
 }
