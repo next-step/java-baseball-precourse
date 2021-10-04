4c0e4058b608a4abecbb5831d74fe3e17f1a5209
dharana77 <dharana7723@gmail.com>
2021-10-05 00:44:40 +0900

    feat: baseball 숫자 생성 (baseball/model/BaseballNumber) - getNumber를 통한 value값 get 메소드 - setNumber 메소드를 통해 초기 컴퓨터 랜덤숫자 생성 - pickRandomNumber 메소드에서 numberList 해쉬셋을 인자로 받아 중복 방지

---
+++ src/main/java/baseball/model/BaseballNumber.java
@@ -0,0 +1,36 @@
+package baseball.model;
+import nextstep.utils.Randoms;
+
+import java.util.HashSet;
+
+public class BaseballNumber {
+    private int number;
+
+    public BaseballNumber(){
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setNumber(){
+        int computerNumber = 0;
+        HashSet<Integer> numberList = new HashSet<Integer>();
+        for(int i=0;i<3;i++){
+            int pickedNumber = pickRandomNumber(numberList);
+            computerNumber += pickedNumber;
+            numberList.add(pickedNumber);
+        }
+        number = computerNumber;
+    }
+
+    public int pickRandomNumber(HashSet<Integer>numberList){
+        int pickedNumber = Randoms.pickNumberInRange(1,9);
+        while(numberList.contains(pickedNumber)){
+            pickedNumber = Randoms.pickNumberInRange(1,9);
+        }
+        return pickedNumber;
+    }
+
+
+}
