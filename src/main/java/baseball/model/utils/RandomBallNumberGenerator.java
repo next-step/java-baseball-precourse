e8215c3c232d53d5f53584ad889ac366fa0e5833
ParkJiwoon <bcp0109@gmail.com>
2021-10-05 02:42:13 +0900

    refactor: 필요 없는 주석 삭제

--- src/main/java/baseball/model/utils/RandomBallNumberGenerator.java
+++ src/main/java/baseball/model/utils/RandomBallNumberGenerator.java
@@ -10,13 +10,6 @@ public class RandomBallNumberGenerator {
     private static final int START_INCLUSIVE  = 1;
     private static final int END_INCLUSIVE    = 9;
 
-    /**
-     * 랜덤한 숫자를 가진 List 를 생성한다
-     * 중복을 피하기 위해 랜덤한 숫자를 직접 만드는 대신 Index 를 만든 후 뽑는 방식
-     *
-     * @param answerSize  랜덤한 배열의 사이즈
-     * @return      랜덤하게 만들어진 숫자 List
-     */
     public static List<Integer> generate(AnswerSize answerSize) {
         List<Integer> result = new ArrayList<>(answerSize.size());
 
