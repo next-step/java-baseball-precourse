eec384082658da55968aab6cda9501925f6868b2
praivesi <praivesi@gmail.com>
2021-10-04 20:31:57 +0900

    refactor: 중복되지 않은 3개의 정수를 랜덤 생성하는 RandomInputGenerator 클래스를 구현함

---
+++ src/main/java/baseball/RandomInputGenerator.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+
+public class RandomInputGenerator {
+    public ArrayList<Integer> genRandomInts() {
+        ArrayList<Integer> randomInts = new ArrayList<>();
+
+        while (randomInts.size() < GlobalVariables.MAX_BALL_CNT) {
+            int randomInt = Randoms.pickNumberInRange(GlobalVariables.MIN_BALL_NUMBER, GlobalVariables.MAX_BALL_NUMBER);
+
+            insertOnlyDistinct(randomInts, randomInt);
+        }
+
+        return randomInts;
+    }
+
+    private static void insertOnlyDistinct(ArrayList<Integer> into, int insertValue) {
+        if (into.contains(insertValue)) return;
+
+        into.add(insertValue);
+    }
+}
---
+++ src/test/java/baseball/RandomInputGeneratorTest.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class RandomInputGeneratorTest {
+    @Test
+    void 중복되지_않은_3개의_정수를_가진_리스트를_생성한다() {
+        ArrayList<Integer> randoms = new RandomInputGenerator().genRandomInts();
+
+        Assertions.assertThat(randoms.size()).isEqualTo(GlobalVariables.MAX_BALL_CNT);
+
+        for (int i = 0; i < GlobalVariables.MAX_BALL_CNT; i++) {
+            Assertions.assertThat(randoms).containsOnlyOnce(randoms.get(i));
+        }
+    }
+}
