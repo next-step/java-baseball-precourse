7efbf5d8de2b1f56200c4ea53ec1326fa264277e
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-04 14:48:25 +0900

    feat : 매치결과를 출력하는 View 생성

---
+++ src/main/java/baseball/domain/PickNumberMatchResultView.java
@@ -0,0 +1,42 @@
+package baseball.domain;
+
+public class PickNumberMatchResultView {
+
+    private int strike;
+    private int ball;
+
+    public PickNumberMatchResultView() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+
+    public void addStrikeCount() {
+        this.strike += 1;
+    }
+
+    public void addBallCount() {
+        this.ball += 1;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder matchResult = new StringBuilder();
+        if (strike > 0) {
+            matchResult.append(strike + GameMessage.STRIKE.getMsg());
+        }
+
+        if (ball > 0) {
+            matchResult.append(ball + GameMessage.BALL.getMsg());
+        }
+
+        if (strike == 0 && ball == 0) {
+            matchResult.append(GameMessage.NOTHING.getMsg());
+        }
+
+        return matchResult.toString();
+    }
+}
