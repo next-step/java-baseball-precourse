e8a1a3632c1bc4c2f49d4f49a3443dc51b16e6fb
김민지 <21600069@handong.edu>
2021-10-04 19:31:38 +0900

    feat : 볼, 스트라이크 세기 전 Params 생성
    BeforeCounting

---
+++ src/main/java/baseball/BeforeCounting.java
@@ -0,0 +1,57 @@
+package baseball;
+
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeforeCounting {
+    public List<Integer> answerNumList;
+    public List<Integer> inputNumList;
+    public int answerNum;
+
+    public BeforeCounting() {
+        this.answerNumList = new ArrayList<>();
+        this.inputNumList = new ArrayList<>();
+        this.answerNum = 0;
+    }
+
+    public void makeAnswerNumList() {
+        int resultRandom = 0;
+        while(answerNumList.size() < 3) {
+            resultRandom = Randoms.pickNumberInRange(1, 9);
+            addAnswerNumList(resultRandom);
+        }
+    }
+
+    public void addAnswerNumList(Integer resultRandom) {
+        if(!answerNumList.contains(resultRandom)) {
+            answerNumList.add(resultRandom);
+        }
+    }
+
+    public void makeInputNumList(String inputVal) {
+        for(int i = 0; i < 3; i++) {
+            inputNumList.add(Integer.parseInt(inputVal.split("")[i]));
+        }
+    }
+
+    public void answerNumListToInt() {
+        clearInputNum();
+        for (int i = answerNumList.size() - 1; i >= 0; i--) {
+            answerNum += answerNumList.get(2 - i) * Math.pow(10 , i);
+        }
+    }
+
+    public void clearAnswerNumList() {
+        this.answerNumList = new ArrayList<>();
+    }
+
+    public void clearInputNumList() {
+        this.inputNumList = new ArrayList<>();
+    }
+
+    public void clearInputNum() {
+        this.answerNum = 0;
+    }
+}
