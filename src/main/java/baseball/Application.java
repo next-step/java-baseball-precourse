ba16749b80c5f522c9991e98293d2a8976360342
이한수 <hslee901104@gmail.com>
2021-10-04 15:35:00 +0900

    completeStudy

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("##### 야구게임을 시작합니다 ####");
+        System.out.println("##### 야구게임을 시작합니다 #####");
 
         BaseballService bbService = new BaseballService();
         bbService.running();
