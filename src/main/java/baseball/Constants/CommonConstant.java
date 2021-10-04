40c7376ecdcfe70e935adf2b58e78433f08fff82
jparangdev <jparangdev@gmail.com>
2021-10-04 23:28:06 +0900

    <chore> (Constant) : 클래스 안에 있던 메세지들을 공통 메시지로 변경
    CommonConstant.java
    - 랜덤 생성에 쓰일 범위값 추가
    
    MessageConstant.java
    - 에러 메시지 추가

--- src/main/java/baseball/Constants/CommonConstant.java
+++ src/main/java/baseball/Constants/CommonConstant.java
@@ -3,4 +3,6 @@ package baseball.Constants;
 public class CommonConstant {
 
 	public static final int INPUT_MAX_COUNT = 3;
+	public static final int MAX_RANGE = 9;
+	public static final int MIN_RANGE = 1;
 }
--- src/main/java/baseball/Constants/MessageConstant.java
+++ src/main/java/baseball/Constants/MessageConstant.java
@@ -3,8 +3,13 @@ package baseball.Constants;
 public class MessageConstant {
 	public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞추셨습니다! 게임 끝";
-	public static final String NEW_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-	public static final String ENDING_MESSAGE = "총 %i 번의 게임을 진행했고 적중률은 %f % 입니다.";
-	public static final String INPUT_MISMATCH_MESSAGE = "[ERROR] 올바른 값을 입력해주세요";
+	public static final String NEW_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2 를 입력하세요.";
+	public static final String ENDING_MESSAGE_FORMAT = "총 %d 번의 게임을 진행했고 적중률은 %s 입니다.";
+	public static final String STRIKE ="스트라이크";
+	public static final String BALL ="볼";
+	public static final String NOTHING_MESSAGE = "낫싱";
+	public static final String ERROR_ONE_OR_TOW = "[ERROR] 1,2중에 입력해주세요";
+	public static final String ERROR_DUPLICATED = "[ERROR] 중복된 입력이 존재합니다.";
+	public static final String ERROR_INVALID = "[ERROR] 입력한 내용을 다시 확인해주세요.";
 
 }
