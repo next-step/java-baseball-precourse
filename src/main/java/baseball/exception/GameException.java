35f32ad5a7efdb8a87301475978ba4f6a39653f1
전주영 <stary7stars@gmail.com>
2021-10-04 12:34:31 +0900

    feat: 예외처리 추가
    * GameException 추가

---
+++ src/main/java/baseball/exception/GameException.java
@@ -0,0 +1,10 @@
+package baseball.exception;
+
+public class GameException extends Exception {
+    public GameException() {
+    }
+
+    public GameException(String message) {
+        super(message);
+    }
+}
