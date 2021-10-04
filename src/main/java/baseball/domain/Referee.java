7ed3acfa8ae4bae32db3b96eab9f581a5a5026c9
jaesungahn91 <jaesungahn91@gmail.com>
2021-10-04 22:21:08 +0900

    feat(Referee): 특정 자리에 같은 숫자가 있는지 확인 할 수 있다

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Referee {
 
@@ -14,4 +15,13 @@ public class Referee {
         return count;
     }
 
+    public int getStrike(List<Integer> comNumbers, List<Integer> playerNumbers) {
+        int count = 0;
+        for(int i=0; i<3; i++) {
+            if(Objects.equals(comNumbers.get(i), playerNumbers.get(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
