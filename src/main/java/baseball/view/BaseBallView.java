8a4890a9672838e065c44bcfc801c2e817cbbbff
leesukjune <robin00q@naver.com>
2021-10-04 17:06:10 +0900

    feat: UI 로직 추가
    게임에서 필요한 UI 로직을 추가한다.
    - 숫자입력에 필요한 로직을 추가
    - 결과를 출력하기 위해 필요한 로직을 추가
    - 게임이 끝나고 새롭게 플레이 할 것인지에 대한 로직을 추가

---
+++ src/main/java/baseball/view/BaseBallView.java
@@ -0,0 +1,21 @@
+package baseball.view;
+
+import nextstep.utils.Console;
+
+public class BaseBallView {
+
+	public static String requestInputNumber() {
+		System.out.print("숫자를 입력해주세요 : ");
+		return Console.readLine();
+	}
+
+	public static void printResult(String result) {
+		System.out.println(result);
+	}
+
+	public static String requestForReplay() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		return Console.readLine();
+	}
+}
