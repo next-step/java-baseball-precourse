8f11d1a598e4892e2c131d8ec46307dcef216866
symba <sybarits@gmail.com>
2021-10-05 02:30:07 +0900

    feat: 게임 메인
    게임 메인 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        Controller game = new Controller();
+        System.out.println("prigram start");
+        while(true) {
+            game.run();
+        }
     }
 }
