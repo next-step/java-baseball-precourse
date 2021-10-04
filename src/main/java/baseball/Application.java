39025b25669140ef028ec5f197cafb9463d6a337
jaesungahn91 <jaesungahn91@gmail.com>
2021-10-04 23:06:23 +0900

    feat(Application): TODO 숫자 야구 게임 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import baseball.domain.Count;
+import baseball.domain.Facilitator;
+import baseball.domain.Referee;
+import baseball.view.Command;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        Command command = new Command();
+        Facilitator facilitator = new Facilitator();
+        Referee referee = new Referee();
+
+        boolean gameContinue = true;
+        while (gameContinue){
+            List<Integer> comNumbers = facilitator.numbersGenerator();
+            boolean isStruckOut = false;
+            while(!isStruckOut) {
+                List<Integer> playerNumbers = facilitator.splitNumbers(command.askPlayerNumbers());
+                Count count = referee.atBat(comNumbers, playerNumbers);
+                command.printCount(count.getBall(), count.getStrike());
+                isStruckOut = referee.isStruckOut(count.getStrike());
+                if(isStruckOut) {
+                    command.printGameClear();
+                }
+            }
+            gameContinue = facilitator.isContinueGame(command.askContinueGame());
+        }
     }
 }
