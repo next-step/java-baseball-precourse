eed74e117c24788b112041caae50df79a06a263b
SC <pch838811@gmail.com>
2021-10-04 22:34:18 +0900

    refactor : output message 정리
    static class 에서 관리하도록 분리

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,19 +1,12 @@
 package baseball.view;
 
+import static baseball.view.OutputView.Message.*;
+
 import baseball.model.BallCount;
 
 public class OutputView {
 
 	private static final int ZERO = 0;
-	private static final String EMPTY_STRING = "";
-	private static final String STRIKE_MESSAGE = "%d스트라이크 ";
-	private static final String BALL_MESSAGE = "%d볼";
-	private static final String NOTHING_MESSAGE = "낫싱";
-	private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-	private static final String ROUND_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
-	private static final String RESTART_CHECK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-	private static final String GAME_END_MESSAGE = "게임이 종료되었습니다.";
-	private static final String USER_INPUT_ERROR_MESSAGE = "[ERROR] 사용자 입력이 잘못되었습니다.";
 
 	public void printInputNumber() {
 		System.out.print(INPUT_NUMBER_MESSAGE);
@@ -59,5 +52,17 @@ public class OutputView {
 
 	public void printUserInputError() {
 		System.out.println(USER_INPUT_ERROR_MESSAGE);
-	}
+	 }
+
+	 static class Message {
+		 static final String EMPTY_STRING = "";
+		 static final String STRIKE_MESSAGE = "%d스트라이크 ";
+		 static final String BALL_MESSAGE = "%d볼";
+		 static final String NOTHING_MESSAGE = "낫싱";
+		 static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+		 static final String ROUND_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+		 static final String RESTART_CHECK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+		 static final String GAME_END_MESSAGE = "게임이 종료되었습니다.";
+		 static final String USER_INPUT_ERROR_MESSAGE = "[ERROR] 사용자 입력이 잘못되었습니다.";
+	 }
 }
