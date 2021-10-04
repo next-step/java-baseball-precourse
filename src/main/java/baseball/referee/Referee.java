c8c1933f886e0e4d97eb8d455f71cdfb49d5d321
LeeYoungJun <dldudwns72@gmail.com>
2021-10-04 13:56:05 +0900

    test (정답과 사용자 입력값을 비교하여  판별 값 Test)

--- src/main/java/baseball/referee/Referee.java
+++ src/main/java/baseball/referee/Referee.java
@@ -1,2 +1,34 @@
-package baseball.referee;public class Referee {
+package baseball.referee;
+
+import baseball.balls.Ball;
+import baseball.balls.Balls;
+
+public class Referee {
+
+    private Balls computerBalls;
+    private Ball userBall;
+
+    public Referee(Balls computerBalls, Ball userBall) {
+        this.computerBalls = computerBalls;
+        this.userBall = userBall;
+    }
+
+    public String judge() {
+        for (int i = 0; i < computerBalls.getBallList().size(); i++) {
+            Ball computerBall = computerBalls.getBallList().get(i);
+
+            if (computerBall.getNumber() == userBall.getNumber() && computerBall.getPosition() == userBall.getPosition()) {
+                return "STRIKE";
+            }
+
+            if (computerBall.getNumber() == userBall.getNumber() && computerBall.getPosition() != userBall.getPosition()) {
+                return "BALL";
+            }
+
+        }
+
+        return "NOTHING";
+    }
+
+
 }
--- src/test/java/baseball/referee/RefereeTest.java
+++ src/test/java/baseball/referee/RefereeTest.java
@@ -1,2 +1,65 @@
-package baseball.referee;public class RefereeTest {
+package baseball.referee;
+
+import baseball.balls.Ball;
+import baseball.balls.Balls;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RefereeTest {
+
+    private Balls computerBalls;
+
+    @BeforeEach
+    public void setUP() {
+        Ball firstBall = new Ball(1,2);
+        Ball secondBall = new Ball(2,4);
+        Ball thirdBall = new Ball(3,6);
+
+        computerBalls = new Balls(Arrays.asList(firstBall,secondBall,thirdBall));
+    }
+
+    @Test
+    @DisplayName("정답과 사용자의 공을 비교하여 같은 위치에 같은 숫자일 경우 STRIKE 리턴")
+    public void isStrike() {
+        Ball userBall = new Ball(1, 2);
+
+        Referee referee = new Referee(computerBalls, userBall);
+
+        String refereeResult = referee.judge();
+
+        assertThat(refereeResult).isEqualTo("STRIKE");
+    }
+
+    @Test
+    @DisplayName("정답과 사용자의 공을 비교하여 다른 위치에 같은 숫자일 경우 BALL 리턴")
+    public void isBall() {
+        Ball userBall = new Ball(1, 4);
+
+        Referee referee = new Referee(computerBalls, userBall);
+
+        String refereeResult = referee.judge();
+
+        assertThat(refereeResult).isEqualTo("BALL");
+    }
+
+    @Test
+    @DisplayName("정답에 일치하는 숫자가 사용자 공에 없다면 NOTHING 리턴")
+    public void isNOTHING() {
+        Ball userBall = new Ball(2, 8);
+
+        Referee referee = new Referee(computerBalls, userBall);
+
+        String refereeResult = referee.judge();
+
+        assertThat(refereeResult).isEqualTo("NOTHING");
+    }
+
+
+
+
 }
