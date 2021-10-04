608f7d1c2f145e3cce5d262873282f63a9d14fe9
SC <pch838811@gmail.com>
2021-10-04 20:55:11 +0900

    feat : 현재 게임에 대해 나타내는 BaseballGame 구현
    - 랜덤 숫자로 3자리수를 생성하여 문제를 생성
    - 게임의 상태를 가짐 : 현재 진행중이거나 3스트라이크로 종료됨

---
+++ src/main/java/baseball/model/BaseballGame.java
@@ -0,0 +1,63 @@
+package baseball.model;
+
+import static baseball.model.BallStatus.*;
+import static baseball.model.GameStatus.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import nextstep.utils.Randoms;
+
+public class BaseballGame {
+
+	private static final int NUMBER_SIZE = 3;
+	private static final int STRIKE_OUT = 3;
+
+	private final Balls question;
+	private final BallCount ballCount;
+	private final GameStatus gameStatus;
+
+	public BaseballGame() {
+		List<Integer> numbers = new ArrayList<>();
+
+		while (numbers.size() < NUMBER_SIZE) {
+			final int number = Randoms.pickNumberInRange(1, 9);
+			if (!numbers.contains(number)) {
+				numbers.add(number);
+			}
+		}
+
+		this.question = Balls.of(numbers);
+		this.ballCount = BallCount.from(Arrays.asList(NOTHING, NOTHING, NOTHING));
+		this.gameStatus = PROGRESS;
+	}
+
+	private BaseballGame(Balls balls, BallCount ballCount, GameStatus gameStatus) {
+		this.question = balls;
+		this.ballCount = ballCount;
+		this.gameStatus = gameStatus;
+	}
+
+	public BaseballGame hit(List<Integer> inputNumbers) {
+		final Balls answer = Balls.of(inputNumbers);
+		final BallCount ballCount = question.match(answer);
+
+		if (isOut(ballCount)) {
+			return new BaseballGame(this.question, ballCount, END);
+		}
+		return new BaseballGame(this.question, ballCount, PROGRESS);
+	}
+
+	public BallCount getBallCount() {
+		return ballCount;
+	}
+
+	private boolean isOut(BallCount ballCount) {
+		return ballCount.countStrike() == STRIKE_OUT;
+	}
+
+	public boolean isFinished() {
+		return this.gameStatus == END;
+	}
+}
