e1366b83ca73a68a70ee8b9779d33222f262068f
gmoon0929 <gmoon0929@gmail.com>
2021-10-05 02:57:52 +0900

    feat(Computer): 컴퓨터 도메인 기능 추가
    - getHintFromPlayerNumbers: 생성된 세 자리 난수와 플레이어의 숫자를 비교하여 Hint 반환

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,48 @@
+package baseball.model;
+
+import java.util.Objects;
+
+import baseball.functional.NumbersGenerator;
+
+public class Computer {
+	private final Numbers values;
+
+	public Computer(Numbers values) {
+		this.values = values;
+	}
+
+	public static Computer create(NumbersGenerator generator) {
+		return new Computer(generator.createNumbers());
+	}
+
+	public Hint getHintFromPlayerNumbers(Player player) {
+		Hint hint = Hint.create();
+
+		for (int i = 0; i < Numbers.SIZE; i++) {
+			Number number = values.get(i);
+			int digit = i + 1;
+
+			boolean existsNumber = player.isContainsNumber(number);
+			boolean sameDigit = digit == player.getNumberDigit(number);
+			hint = hint.hit(StrikeZone.of(existsNumber, sameDigit));
+		}
+		return hint;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Computer computer = (Computer)o;
+		return Objects.equals(values, computer.values);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(values);
+	}
+}
---
+++ src/test/java/baseball/model/ComputerTest.java
@@ -0,0 +1,45 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import baseball.functional.NumbersGenerator;
+
+class ComputerTest {
+	@Test
+	@DisplayName("컴퓨터는 NumbersGenerator를 통해 주입받아 생성할 수 있다.")
+	void create() {
+		// given
+		NumbersGenerator generator = () -> new Numbers(Arrays.asList(1, 2, 3));
+
+		// when
+		Computer actual = Computer.create(generator);
+
+		// then
+		assertThat(actual)
+			.isNotNull();
+	}
+
+	@Test
+	@DisplayName("플레이어의 숫자를 비교하여 힌트를 제공한다.")
+	void getHintFromPlayerNumbers() {
+		// given
+		Player player = Player.create("123");
+		Computer computer = Computer.create(() -> new Numbers(Arrays.asList(1, 2, 4)));
+
+		// when
+		Hint hint = computer.getHintFromPlayerNumbers(player);
+
+		// then
+		Hint expected = Hint.create();
+		expected = expected.hit(StrikeZone.STRIKE);
+		expected = expected.hit(StrikeZone.STRIKE);
+		expected = expected.hit(StrikeZone.NOTHING);
+		assertThat(hint)
+			.isEqualTo(expected);
+	}
+}
