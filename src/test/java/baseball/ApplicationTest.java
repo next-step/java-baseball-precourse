b708fc0883d96dd265be8ae31a9c493e161e9570
devsigner <devsigner9920@icloud.com>
2021-10-04 16:02:32 +0900

    style : 기존코드 인덴트 변경
    변경 내용:
    - 기존 소스코드 인덴트를 4에서 2로 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -11,40 +11,40 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mockStatic;
 
 public class ApplicationTest extends NSTest {
-    @BeforeEach
-    void beforeEach() {
-        super.setUp();
-    }
+  @BeforeEach
+  void beforeEach() {
+    super.setUp();
+  }
 
-    @Test
-    void 낫싱() {
-        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
-            mockRandoms
-                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-                    .thenReturn(1, 3, 5);
-            running("246");
-            verify("낫싱");
-        }
+  @Test
+  void 낫싱() {
+    try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+      mockRandoms
+        .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+        .thenReturn(1, 3, 5);
+      running("246");
+      verify("낫싱");
     }
+  }
 
-    @Test
-    void 게임종료_후_재시작() {
-        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
-            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-                    .thenReturn(7, 1, 3)
-                    .thenReturn(5, 8, 9);
-            run("713", "1", "597", "589", "2");
-            verify("3스트라이크", "게임 끝", "1스트라이크 1볼");
-        }
+  @Test
+  void 게임종료_후_재시작() {
+    try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+      mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+        .thenReturn(7, 1, 3)
+        .thenReturn(5, 8, 9);
+      run("713", "1", "597", "589", "2");
+      verify("3스트라이크", "게임 끝", "1스트라이크 1볼");
     }
+  }
 
-    @AfterEach
-    void tearDown() {
-        outputStandard();
-    }
+  @AfterEach
+  void tearDown() {
+    outputStandard();
+  }
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+  @Override
+  public void runMain() {
+    Application.main(new String[]{});
+  }
 }
