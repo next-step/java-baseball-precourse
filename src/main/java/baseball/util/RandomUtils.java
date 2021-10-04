75f12f17ccdf9baec7c151d7b95d24424676e080
leesukjune <robin00q@naver.com>
2021-10-04 21:29:16 +0900

    refactor: RandomUtils 는 랜덤을 생성하는 유틸로써, 상수를 받아서 처리하지 않도록 변경
    상수를 받아 처리하는 경우, 도메인에 종속될 우려가 있어 상수를 받지않고 처리하도록 변경

--- src/main/java/baseball/util/RandomUtils.java
+++ src/main/java/baseball/util/RandomUtils.java
@@ -9,36 +9,18 @@ import nextstep.utils.Randoms;
 
 public class RandomUtils {
 
-	private static final int MINIMUM_RESULT_SIZE = 1;
-
 	/**
-	 * startInclusive ~ endInclusive 까지의 범위의 숫자를 랜덤하게 생성하되, 겹치는 숫자가 없도록 생성한다.
-	 *
-	 * @param startInclusive    범위의 시작값, 리턴 범위내에 포함된다.
-	 * @param endInclusive        범위의 끝값, 리턴 범위내에 포함된다.
-	 * @param resultSize        몇 개 짜리 겹치지않는 숫자를 반환할 것인지에 대한 값
+	 * 1 ~ 9 까지의 범위의 숫자를 랜덤하게 생성하되, 겹치는 숫자가 없도록 3개 생성
 	 *
 	 * @return 지정한 범위 내의 숫자들이 겹치지 않은 상태로 들어있는 List
 	 */
-	public static List<Integer> generateNoDuplicateNumbers(final int startInclusive, final int endInclusive,
-		final int resultSize) {
-		validateResultSize(resultSize, endInclusive - startInclusive + 1);
-
+	public static List<Integer> generateNoDuplicateThreeBaseball() {
 		final Set<Integer> numbers = new HashSet<>();
 
-		while (numbers.size() < resultSize) {
-			numbers.add(Randoms.pickNumberInRange(startInclusive, endInclusive));
+		while (numbers.size() < 3) {
+			numbers.add(Randoms.pickNumberInRange(1, 9));
 		}
 
 		return new ArrayList<>(numbers);
-	}
-
-	private static void validateResultSize(int resultSize, int availableSize) {
-		if (resultSize < MINIMUM_RESULT_SIZE) {
-			throw new IllegalArgumentException("[ERROR] resultSize 는 1보다 작을 수 없습니다.");
-		}
-		if (availableSize < resultSize) {
-			throw new IllegalArgumentException("[ERROR] 범위내의 모든숫자보다 resultSize 값이 클 수 없습니다.");
-		}
 	}
 }
--- src/test/java/baseball/util/RandomUtilsTest.java
+++ src/test/java/baseball/util/RandomUtilsTest.java
@@ -1,6 +1,5 @@
 package baseball.util;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
@@ -14,44 +13,13 @@ class RandomUtilsTest {
 	@DisplayName("랜덤하게 생성된 3개의 숫자가 중복되지 않는지 테스트")
 	@Test
 	void generateNoDuplicateNumbers() {
-		// given
-		final int startInclusive = 1;
-		final int endInclusive = 9;
-		final int resultSize = 3;
+		// given:none
 
 		// when
-		final List<Integer> numbers = RandomUtils.generateNoDuplicateNumbers(startInclusive, endInclusive, resultSize);
+		final List<Integer> numbers = RandomUtils.generateNoDuplicateThreeBaseball();
 
 		// then
 		assertEquals(3, numbers.size());
 		assertEquals(3, new HashSet<>(numbers).size());
-	}
-
-	@DisplayName("ResultSize 가 생성될 수 있는 모든 숫자보다 큰 경우 에러를 반환하는 테스트")
-	@Test
-	void generateNoDuplicateNumbers_resultSize_larger_than_available() {
-		// given
-		final int startInclusive = 1;
-		final int endInclusive = 9;
-		final int resultSize = 10;
-
-		// when then
-		assertThatThrownBy(() -> RandomUtils.generateNoDuplicateNumbers(startInclusive, endInclusive, resultSize))
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] 범위내의 모든숫자보다 resultSize 값이 클 수 없습니다.");
-	}
-
-	@DisplayName("ResultSize 가 1보다 작은경우")
-	@Test
-	void generateNoDuplicateNumbers_resultSize_smaller_than_1() {
-		// given
-		final int startInclusive = 1;
-		final int endInclusive = 9;
-		final int howMuch = 0;
-
-		// when then
-		assertThatThrownBy(() -> RandomUtils.generateNoDuplicateNumbers(startInclusive, endInclusive, howMuch))
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessageContaining("[ERROR] resultSize 는 1보다 작을 수 없습니다.");
 	}
 }
