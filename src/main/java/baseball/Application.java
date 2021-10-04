8a82440b3c62eb4cdfb5dba5b0e3d15320afd10c
bluecheer <bluecheer86@gmail.com>
2021-10-05 00:15:20 +0900

    feat($baseball): 야구게임 시작과 종료 구현
    기능 추가 $baseball:
    - 야구게임 시작종료 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import java.util.List;
+
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        boolean isContinue = true;
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> pitcher = null;
+        while(isContinue) {
+            if (pitcher == null) {
+                pitcher = baseballGame.pitcher();
+            }
+            List<Integer> swing = baseballGame.swing();
+            BaseBall baseBall = baseballGame.run(pitcher, swing);
+            boolean result = baseballGame.result(baseBall);
+            if (result == true) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하라면 2를 입력하세요.");
+                String menuStr = Console.readLine();
+                if ("2".equals(menuStr)) {
+                    isContinue = false;
+                }
+                // 새 게임
+                pitcher = baseballGame.pitcher();
+            }
+        }
     }
 }
