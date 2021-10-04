578a7429c994a15e7efad8458dbc84f16489f92e
byun <bjs1616@gmail.com>
2021-10-05 06:13:19 +0900

    feat: 종료 여부
    종료 여부를 알기위해 사용자에게 물어보고 종료를 선택하면 프로세스 종료, 계속하기 선택시 새로운 정답지로 게임 시작

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -10,11 +10,19 @@ public class BaseballGame {
 
     public void startGame(){
         List<Integer> answer = GameUtils.generateAnswer();
-        Record result;
+        Record record;
         do {
+            System.out.println("입력해주세요 : ");
             String input = Console.readLine();
-            result = GameUtils.match(answer, input);
-        }while (result.isCorrect());
+            record = GameUtils.match(answer, input);
+        }while (record.isCorrect());
+        askOfEndGame();
+    }
+
+    private void askOfEndGame() {
+        System.out.println("정답을 맞추셨습니다. 새로운 게임을 진행하시려면 1번, 끝내시려면 2번을 눌러주세요");
+        String input = Console.readLine();
+        if("2".equals(input)) this.end = true;
     }
 
     public boolean isEnd() {
