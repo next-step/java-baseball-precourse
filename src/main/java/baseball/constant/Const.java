bdb3f9294552a0407f6c34cb9e1463d9779ce228
taeyanglim <taeyang7704@naver.com>
2021-10-04 14:40:06 +0900

    feat: add Const class(include constant) in constant package

---
+++ src/main/java/baseball/constant/Const.java
@@ -0,0 +1,8 @@
+package baseball.constant;
+
+public class Const {
+    public static final int RANDOM_MIN_RANGE = 1;
+    public static final int RANDOM_MAX_RANGE = 9;
+    public static final int GENERATE_NUMBER_SIZE = 3;
+    public static final String NUMBER_REGEX = "^[0-9]*$";
+}
