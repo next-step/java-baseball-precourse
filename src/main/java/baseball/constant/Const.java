225fdaee92b8eaf7a95193edc2510dcd324699ef
taeyanglim <taeyang7704@naver.com>
2021-10-04 17:19:45 +0900

    feat: add a RESTART_FLAG in Const class AND add STRIKE, BALL, NOTHING in MessageEnum

--- src/main/java/baseball/constant/Const.java
+++ src/main/java/baseball/constant/Const.java
@@ -5,4 +5,5 @@ public class Const {
     public static final int RANDOM_MAX_RANGE = 9;
     public static final int GENERATE_NUMBER_SIZE = 3;
     public static final String NUMBER_REGEX = "^[0-9]*$";
+    public static final int RESTART_FLAG = 1;
 }
