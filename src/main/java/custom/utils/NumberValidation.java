fb024cce8441586be0bb3d05c5905e513c1101dc
venaCode <venaCode93@gmail.com>
2021-10-05 05:21:09 +0900

    feat(validation): add couple of Number Validation
    - check3digitsOfLength
    - checkContainOnlyNumber
    - checkDuplicateNumbers

---
+++ src/main/java/custom/utils/NumberValidation.java
@@ -0,0 +1,38 @@
+package custom.utils;
+
+public class NumberValidation {
+    /**
+     * 숫자 유효성 검사하는 api
+     */
+    NumberValidation(){
+
+    }
+
+    public static boolean check3digitsOfLength(String numStr){
+        if(numStr.length() != 3){
+            Message.printError("입력한 자릿수가 3자리가 아닙니다.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkContainOnlyNumber(String numStr){
+        if(!numStr.matches("^[1-9]+$")){
+            Message.printError("숫자가 아닌 문자가 포함되었습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkDuplicateNumbers(String userNum){
+        boolean[] numSet = new boolean[9];
+        for(int i =0; i<3; i++){
+            if(numSet[userNum.charAt(i) - '0']){
+                Message.printError("중복된 숫자가 입력되었습니다.");
+                return false;
+            }
+            numSet[userNum.charAt(0) - '0'] = true;
+        }
+        return true;
+    }
+}
