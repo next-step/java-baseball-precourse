03da78055aa15c20f150f14f876ab1fa48731694
hyun6ik <fkanwm@naver.com>
2021-10-05 04:30:20 +0900

    fix(exception) : exception 제거

--- src/main/java/baseball/exception/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package baseball.exception;
-
-import baseball.message.error.ErrorCode;
-
-public class ValidationException extends BusinessException {
-    public ValidationException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
