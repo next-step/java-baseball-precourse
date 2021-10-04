528583a9315aa5518756acc1cfded2757d4ebe72
symba <sybarits@gmail.com>
2021-10-05 02:27:06 +0900

    feat: 사용자 입력과 출력
    사용자로부터 입력을 받고 유효성 확인
    사용자에게 게임 진행 안내를 위한 기능

---
+++ src/main/java/baseball/UserInput.java
@@ -0,0 +1,66 @@
+package baseball;
+
+import nextstep.utils.Console;
+
+public class UserInput {
+
+    public String ball() throws Exception {
+        String input = Console.readLine();
+        if (input == null || input.equals("")) {
+            throw new Exception("입력을 해야 합니다.");
+        }
+        if (input.length() != 3) {
+            throw new Exception("숫자 3개를 입력해야 합니다.");
+        }
+        if (!checkNumber(input)) {
+            throw new Exception("숫자를 입력해야 합니다.");
+        }
+        if (checkDup(input)) {
+            throw new Exception("중복 숫자를 입력하면 안됩니다.");
+        }
+        return input;
+    }
+
+    private boolean checkDup(String input) {
+        if (input.charAt(0) == input.charAt(1)
+                || input.charAt(1) == input.charAt(2)
+                || input.charAt(0) == input.charAt(2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkNumber(String input) {
+        if (input.charAt(0) < 49 || input.charAt(0) > 57 ) {
+            return false;
+        }
+        if (input.charAt(1) < 49 || input.charAt(1) > 57 ) {
+            return false;
+        }
+        if (input.charAt(2) < 49 || input.charAt(2) > 57 ) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isNumber(String input) {
+        if (input.charAt(0) < 49 || input.charAt(0) > 57 ) {
+            return false;
+        }
+        return true;
+    }
+
+    public String restartGame() throws Exception {
+        String input = Console.readLine();
+        if (input == null || input.equals("")) {
+            throw new Exception("입력을 해야 합니다.");
+        }
+        if (input.length() != 1) {
+            throw new Exception("한글자를 입력해야 합니다.");
+        }
+        if (!isNumber(input)) {
+            throw new Exception("숫자를 입력해야합니다.");
+        }
+        return input;
+    }
+}
