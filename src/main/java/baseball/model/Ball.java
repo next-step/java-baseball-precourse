da4453158f4fb8f6d52a7089364cdb5c3d7e37a8
shinaaaa <shin029abc@gmail.com>
2021-10-05 03:27:15 +0900

    feat(model): Ball
    - 입력 값을 받을 model 추가

---
+++ src/main/java/baseball/model/Ball.java
@@ -0,0 +1,22 @@
+package baseball.model;
+
+public class Ball {
+    private int ball;
+    private int strike;
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+}
