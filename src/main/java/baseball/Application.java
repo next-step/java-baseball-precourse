fc867f701b820da8154fae51313a7708bb9a9a1d
venaCode <venaCode93@gmail.com>
2021-10-05 05:36:32 +0900

    feat(main): writing test code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,30 @@
 package baseball;
 
+import custom.utils.Message;
+import custom.utils.NumberValidation;
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        String comNumbers = ""+Randoms.pickNumberInRange(1,9)+Randoms.pickNumberInRange(1,9)+Randoms.pickNumberInRange(1,9);
+        System.out.println("Com's Numbers : " + comNumbers);
 
-        String test;
+        boolean isNumOkay = true;
+        while(isNumOkay){
+            Message.printInform("숫자를 입력해주세요 : ");
+            String userNumbers = Console.readLine();
+
+            if(NumberValidation.is3digitsOfLength(userNumbers)){
+                continue;
+            }
+            if(NumberValidation.containOnlyNumber(userNumbers)){
+
+            }
+        }
+
+
+
     }
+
 }
