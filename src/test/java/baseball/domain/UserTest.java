5129a28314f9b01f951a126067ec2ee992665ed4
taeyanglim <taeyang7704@naver.com>
2021-10-04 16:59:14 +0900

    test: add input invalid parameter to isGameOver Method Test AND modify invalid UserInputTest

--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -1,10 +1,12 @@
 package baseball.domain;
 
-import baseball.constant.Const;
 import baseball.exception.InvalidUserInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,40 +31,27 @@ public class UserTest {
         assertThat(user.getNumbers()).containsExactly(4, 1, 3);
     }
 
-    @Test
-    @DisplayName("숫자 유형이 아닌 값 리스트 삽입 테스트")
-    void input_user_not_number_type_test() {
-        String receiveUserInput = "4ab";
+    @ParameterizedTest
+    @CsvSource(value = {
+            "4ab:유효하지 않은 문자",
+            "51382:3개의 숫자를 입력해주세요",
+            "82:3개의 숫자를 입력해주세요",
+            "882:중복된 숫자를 입력하였습니다."
+    }, delimiter = ':')
+    @DisplayName("유효하지 않은 사용자 입력 리스트 삽입 테스트")
+    void invalid_input_test(String receiveUserInput, String errorMessage) {
         assertThatThrownBy(() -> user.initNumber(receiveUserInput))
+                .isInstanceOf(InvalidUserInputException.class)
+                .hasMessageContaining(errorMessage);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "--", "__", "```", "exit"})
+    @DisplayName("게임 종료를 위해 숫자가 아닌 유형의 값을 입력했을 경우")
+    void invalid_input_game_over_test(String input) {
+        assertThatThrownBy(() -> user.isGameStop(input))
                 .isInstanceOf(InvalidUserInputException.class)
                 .hasMessageContaining("유효하지 않은 문자");
-    }
-
-    @Test
-    @DisplayName("입력제한 길이보다 크게 입력된 경우 테스트")
-    void input_larger_than_input_limit_length_test() {
-        String receiveUserInput = "51382";
-        assertThatThrownBy(() -> user.initNumber(receiveUserInput))
-                .isInstanceOf(InvalidUserInputException.class)
-                .hasMessageContaining(Const.GENERATE_NUMBER_SIZE + "개의 숫자를 입력해주세요.");
-    }
-
-    @Test
-    @DisplayName("입력제한 길이보다 작게 입력된 경우 테스트")
-    void input_smaller_than_input_limit_length_test() {
-        String receiveUserInput = "82";
-        assertThatThrownBy(() -> user.initNumber(receiveUserInput))
-                .isInstanceOf(InvalidUserInputException.class)
-                .hasMessageContaining(Const.GENERATE_NUMBER_SIZE + "개의 숫자를 입력해주세요.");
-    }
-
-    @Test
-    @DisplayName("중복된 숫자를 입력했을 경우 테스트")
-    void input_duplicate_number_test() {
-        String receiveUserInput = "882";
-        assertThatThrownBy(() -> user.initNumber(receiveUserInput))
-                .isInstanceOf(InvalidUserInputException.class)
-                .hasMessageContaining("중복된 숫자를 입력하였습니다.");
     }
 
 }
