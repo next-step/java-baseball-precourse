ab41918ee2ee98c99bda66b38eff63c98bbec3b3
byun <bjs1616@gmail.com>
2021-10-05 05:38:54 +0900

    feat: create random answer generator
    랜덤으로 3개의 숫자를 뽑아내는 제너레이터 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+
+        List<Integer> answer = GameUtils.generateAnswer();
+
     }
 }
---
+++ src/main/java/baseball/GameUtils.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GameUtils {
+
+    public static List<Integer> generateAnswer(){
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < 3){
+            int number = Randoms.pickNumberInRange(1,9);
+            numbers.add(number);
+        }
+        return new ArrayList<>(numbers);
+    }
+
+}
