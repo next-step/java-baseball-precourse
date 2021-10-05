6db0dd790981a04ee59584d1a71847bbe2649743
Ttungga <torb2002@gmail.com>
2021-10-05 21:22:23 +0900

    feat: 숫자 입력 메시지 표시 기능
    변경 사항
    - BaseballView 클래스 추가
    - BaseballView: showRequestInputMessage 메서드 추가

---
+++ src/main/java/baseball/view/BaseballView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class BaseballView {
+	private static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요: ";
+
+	public void showRequestInputMessage() {
+		System.out.printf(REQUEST_INPUT_MESSAGE);
+	}
+}
