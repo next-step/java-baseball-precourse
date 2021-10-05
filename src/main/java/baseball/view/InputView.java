e75ef4a2cbbfe2574f963d7eeab22da097a3d852
gmoon0929 <gmoon0929@gmail.com>
2021-10-05 21:13:55 +0900

    feat(InputView): 콘솔 입력 값을 받는 InputView 클래스 추가
    - getPlayer: 사용자가 입력한 문자열을 Player 반환
    - isRestartGame: 게임을 다시 시작할건지 여부
    - nextstep.utils.Console를 사용하여 개발

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,49 @@
+package baseball.view;
+
+import baseball.exception.DuplicateNumberException;
+import baseball.exception.InvalidNumberRangeException;
+import baseball.exception.InvalidNumbersSizeException;
+import baseball.model.Player;
+import nextstep.utils.Console;
+
+public final class InputView {
+	private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String RESTART_GAME_MESSAGE_FORMAT = "게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.";
+	private static final String ERROR_INVALID_INPUT_VALUE_MESSAGE_FORMANT = "[ERROR] 잘못된 값을 입력했습니다. %s";
+
+	private InputView() {
+	}
+
+	public static Player getPlayer() {
+		try {
+			System.out.println(INPUT_NUMBER_MESSAGE);
+			return Player.create(Console.readLine());
+		} catch (InvalidNumberRangeException | InvalidNumbersSizeException | DuplicateNumberException e) {
+			printErrorInvalidInPutValueMessage(e.getMessage());
+			return getPlayer();
+		}
+	}
+
+	public static boolean isRestartGame() {
+		askRestartGameMessage();
+
+		String commandString = Console.readLine();
+		GameConsoleCommand command = GameConsoleCommand.findFirstOrNull(commandString);
+		if (command == null) {
+			printErrorInvalidInPutValueMessage(commandString);
+			return isRestartGame();
+		}
+
+		return GameConsoleCommand.RESTART_GAME == command;
+	}
+
+	private static void askRestartGameMessage() {
+		System.out.println(String.format(RESTART_GAME_MESSAGE_FORMAT,
+			GameConsoleCommand.RESTART_GAME.getCommandStringValue(),
+			GameConsoleCommand.END_GAME.getCommandStringValue()));
+	}
+
+	private static void printErrorInvalidInPutValueMessage(String message) {
+		System.err.println(String.format(ERROR_INVALID_INPUT_VALUE_MESSAGE_FORMANT, message));
+	}
+}
---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,40 @@
+package baseball.view;
+
+import static org.assertj.core.api.BDDAssertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import baseball.model.Numbers;
+import baseball.model.Player;
+import nextstep.utils.Console;
+
+class InputViewTest {
+	@Test
+	@DisplayName("사용자가 입력한 숫자 문자열을 토대로 Player를 생성한다.")
+	void getPlayer() {
+		try (final MockedStatic<Console> mockConsole = mockStatic(Console.class)) {
+			mockConsole.when(Console::readLine)
+				.thenReturn("1234", "123");
+
+			then(InputView.getPlayer())
+				.isEqualTo(new Player(new Numbers(Arrays.asList(1, 2, 3))));
+		}
+	}
+
+	@Test
+	@DisplayName("게임을 다시 시작할건지 여부를 판단한다.")
+	void isRestartGame() {
+		try (final MockedStatic<Console> mockConsole = mockStatic(Console.class)) {
+			mockConsole.when(Console::readLine)
+				.thenReturn("test", GameConsoleCommand.END_GAME.getCommandStringValue());
+
+			then(InputView.isRestartGame())
+				.isFalse();
+		}
+	}
+}
