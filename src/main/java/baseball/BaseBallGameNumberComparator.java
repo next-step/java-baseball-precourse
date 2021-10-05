7a14b389b4dbd8fab6f8623e874e6e33c50fa54f
herbtea <herbtea30@gmail.com>
2021-10-05 21:12:07 +0900

    feat: 야구게임숫자 비교 스트라이크 테스트 및 기능 구현

--- src/main/java/baseball/BaseBallGameNumberComparator.java
+++ src/main/java/baseball/BaseBallGameNumberComparator.java
@@ -14,4 +14,25 @@ public class BaseBallGameNumberComparator {
     public BaseBallGameNumberComparator(int maxLength) {
         this.maxLength = maxLength;
     }
+
+    /**
+     * 숫자를 비교하여 스트라이크 갯수를 계산하는 함수
+     */
+    public int getStrikesCount(String randomGenerated, String input) {
+        int strikesCount = 0;
+        for ( int i = 0 ; i < maxLength ; i++ )
+            strikesCount += checkStrike(randomGenerated.charAt(i), input.charAt(i));
+
+        return strikesCount;
+    }
+
+    /**
+     * 동일한 자리의 숫자를 비교하여, 스트라이크 여부를 판별한다.
+     */
+    private int checkStrike(char randomGenerated, char input) {
+        if (randomGenerated == input)
+            return 1;
+
+        return 0;
+    }
 }
--- src/test/java/baseball/BaseBallGameNumberComparatorTest.java
+++ src/test/java/baseball/BaseBallGameNumberComparatorTest.java
@@ -3,6 +3,11 @@ package baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BaseBallGameNumberComparatorTest {
 
     private BaseBallGameNumberComparator baseBallGameNumberComparator;
@@ -10,5 +15,39 @@ public class BaseBallGameNumberComparatorTest {
     @BeforeEach
     void setUp() {
         baseBallGameNumberComparator = new BaseBallGameNumberComparator(BaseBallGame.DEFAULT_LENGTH);
+    }
+
+    @Test
+    public void 스트라이크_3개() {
+        List<String> inputs = Arrays.asList("123");
+        assertStrikesEquals("123", inputs, 3);
+    }
+
+    @Test
+    public void 스트라이크_1개() {
+        List<String> inputs = Arrays.asList("145", "729", "683");
+        assertStrikesEquals("123", inputs, 1);
+    }
+
+    @Test
+    public void 스트라이크_2개() {
+        List<String> inputs = Arrays.asList("125", "623", "193");
+        assertStrikesEquals("123", inputs, 2);
+    }
+
+    @Test
+    public void 스트라이트_전체테스트() {
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("123", "456"), 0);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("567", "598"), 1);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("236", "786"), 1);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("478", "458"), 2);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("369", "469"), 2);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("854", "853"), 2);
+        assertEquals(baseBallGameNumberComparator.getStrikesCount("456", "456"), 3);
+    }
+
+    private void assertStrikesEquals(String randomGenerated, List<String> inputs, int i) {
+        for(String input : inputs)
+            assertEquals(baseBallGameNumberComparator.getStrikesCount(randomGenerated, input), i);
     }
 }
