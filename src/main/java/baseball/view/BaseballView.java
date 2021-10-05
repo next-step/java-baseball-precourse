0f7f1f7cf68a7627fe1ce057a394ecfc04877c9b
Ttungga <torb2002@gmail.com>
2021-10-05 21:28:14 +0900

    feat: 에러 메시지 표시 기능
    변경 사항
    - BaseballView: showErrorMessage 메서드 추가

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -2,8 +2,13 @@ package baseball.view;
 
 public class BaseballView {
 	private static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요: ";
+	private static final String ERROR_MESSAGE = "[ERROR] 올바른 숫자를 입력해주세요. 중복된 숫자가 없는 세 자리 자연수를 입력해야 합니다.";
 
 	public void showRequestInputMessage() {
 		System.out.printf(REQUEST_INPUT_MESSAGE);
+	}
+
+	public void showErrorMessage() {
+		System.out.println(ERROR_MESSAGE);
 	}
 }
