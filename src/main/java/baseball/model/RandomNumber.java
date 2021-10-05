6cdc6d3f3d7dba41110fdbcede1f5f15180fefe8
devsigner <devsigner9920@icloud.com>
2021-10-06 02:26:51 +0900

    refactor(RandomNumber) : 랜덤한 수를 생성하는 로직을 개선하였습니다.
    변경 내용:
    - numbers 필드를 List형 number 필드로 변경
    - 내부에서 중복 수를 검사하는 로직을 삭제하고, LinkedHashSet을 사용하여, 중복되지 않은 랜덤 수를 생성
    - 불필요한 로직 수정
    - 변경한 클래스에 대해 Test 케이스 개선

--- src/main/java/baseball/model/RandomNumber.java
+++ src/main/java/baseball/model/RandomNumber.java
@@ -2,44 +2,30 @@ package baseball.model;
 
 import nextstep.utils.Randoms;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class RandomNumber {
   public static final int SIZE = 3;
-  private int[] numbers;
+  private final List<Integer> number;
 
   private RandomNumber() {
-    do {
-      numbers = generateNumber();
-    } while (!checkDuplicateNumber(numbers));
+    number = generateNumber();
   }
 
-  private boolean checkDuplicateNumber(int[] numbers) {
-    HashSet<Integer> hashSet = new HashSet<>();
-    for (int number : numbers) {
-      hashSet.add(number);
+  private List<Integer> generateNumber() {
+    Set<Integer> numberSet = new LinkedHashSet<>();
+    while (numberSet.size() != SIZE) {
+      numberSet.add(Randoms.pickNumberInRange(1, 9));
     }
 
-    if (hashSet.size() != SIZE) {
-      return false;
-    }
-
-    return true;
-  }
-
-  private int[] generateNumber() {
-    int[] randomNumbers = new int[SIZE];
-    for (int i = 0; i < SIZE; i++) {
-      randomNumbers[i] = Randoms.pickNumberInRange(1, 9);
-    }
-    return randomNumbers;
+    return new ArrayList<>(numberSet);
   }
 
   public static RandomNumber makeRandomNumber() {
     return new RandomNumber();
   }
 
-  public int[] getNumbers() {
-    return numbers;
+  public List<Integer> getNumber() {
+    return number;
   }
 }
--- src/test/java/baseball/model/RandomNumberTest.java
+++ src/test/java/baseball/model/RandomNumberTest.java
@@ -1,68 +1,30 @@
 package baseball.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RandomNumberTest {
-  List<int[]> duplicateNumbers;
   int BASE_BALL_SIZE = 3;
 
-  @BeforeAll
-  void setUp() {
-    duplicateNumbers = new ArrayList<>();
-    duplicateNumbers.add(new int[] {3, 3, 5});
-    duplicateNumbers.add(new int[] {6, 1, 1});
-    duplicateNumbers.add(new int[] {4, 2, 4});
-    duplicateNumbers.add(new int[] {7, 7, 7});
-    duplicateNumbers.add(new int[] {8, 8, 9});
-  }
-
   @Test
-  @Order(1)
-  @DisplayName("중복된 수가 있는지 체크하는 로직을 검증한다.")
-  void checkDuplicateNumber() {
-    for (int[] numbers : duplicateNumbers) {
-      // given
-      HashSet<Integer> hashSet = new HashSet<>();
-      boolean isDuplicate = false;
-      for (int number : numbers) {
-        hashSet.add(number);
-      }
-
-      // when
-      if (hashSet.size() != BASE_BALL_SIZE) {
-        isDuplicate = true;
-      }
-
-      // then
-      assertThat(isDuplicate).isTrue();
-    }
-  }
-
-  @Test
-  @Order(2)
   @DisplayName("RandomNumber 객체 생성 시 유효한 랜덤수가 생성되었는지 테스트한다.")
   public void makeRandomNumber() {
     // given
-    RandomNumber randomNumber = RandomNumber.makeRandomNumber();
-    HashSet<Integer> checkHashSet = new HashSet<>();
+    List<RandomNumber> randomNumbers = new ArrayList<>();
 
     // when
-    for (int number : randomNumber.getNumbers()) {
-      checkHashSet.add(number);
+    for (int i = 0; i < Short.MAX_VALUE; i++) {
+      randomNumbers.add(RandomNumber.makeRandomNumber());
     }
 
     // then
-    for (int i = 0; i < Short.MAX_VALUE; i++) {
-      assertThat(checkHashSet.size()).isEqualTo(RandomNumber.SIZE);
+    for (RandomNumber randomNumber : randomNumbers) {
+      assertThat(randomNumber.getNumber().size()).isEqualTo(BASE_BALL_SIZE);
     }
   }
 }
