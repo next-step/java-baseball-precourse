10844f4539114f58e8421d8099b84f9f15ba68c3
권기준 <gabunot@gmail.com>
2021-10-06 01:00:24 +0900

    숫자야구 메소드 분리

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -8,34 +8,43 @@ public class BaseBallGame {
 
     private static PlayResult playResult;
     private static Balls computerBalls = Balls.of();
+    private static Boolean gameContinue = true;
 
     public static void playBallsBallGame() {
-        executeBaseBall();
-    }
-
-    public static void executeBaseBall() {
-        while (true) {
+        while (gameContinue) {
             String playerBall = InputView.insertBallNumbers();
-            try {
-                playResult = computerBalls.playBalls(Balls.playerBallInit(playerBall));
-                if (playResult.strikeCount() == 3) {
-                    selectGamePlay();
-                    break;
-                }
-                ResultView.printResult(playResult);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
+            playStart(playerBall);
         }
     }
 
+    private static void playStart(String playerBall) {
+        try {
+            countHit(playerBall);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void countHit(String playerBall) {
+        playResult = computerBalls.playBalls(Balls.playerBallInit(playerBall));
+        if (playResult.strikeCount() == 3) {
+            selectGamePlay();
+            return;
+        }
+        ResultView.printResult(playResult);
+    }
+
+
     private static void selectGamePlay() {
-        ResultView.printThreeStrike();
         ResultView.answerContinuePlay();
         if (InputView.insertSelectGameContinue() == 1) {
             computerBalls = Balls.of();
-            executeBaseBall();
+            playBallsBallGame();
         }
+        gameEnd();
+    }
+
+    private static void gameEnd() {
+        gameContinue = false;
     }
 }
