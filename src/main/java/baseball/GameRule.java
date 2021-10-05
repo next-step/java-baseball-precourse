261ab9dddb6ab427231b1e4c8adee5fc5b1f8c41
bestsilver <dkvkxm99@gmail.com>
2021-10-06 04:46:12 +0900

    feat: GameRule 구현
    GameRule
    - 게임에서 공통 룰을 상수로 정의

---
+++ src/main/java/baseball/GameRule.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class GameRule {
+    public static final int MAX_LENGTH = 3;
+
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+
+    public static final int RESET = 1;
+    public static final int QUIT = 2;
+}
