7569a7682c55dbdcbd9689054457deed03ea44a8
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-06 01:25:55 +0900

    test : 스트라이크, 볼 카운트 테스트

--- src/test/java/baseball/service/GameServiceImplTest.java
+++ src/test/java/baseball/service/GameServiceImplTest.java
@@ -2,7 +2,6 @@ package baseball.service;
 
 import baseball.domain.PickNumberMatchResultView;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -46,6 +45,26 @@ class GameServiceImplTest {
 
         boolean result = (boolean) method.invoke(gameService, input);
         assertThat(result).isFalse();
+    }
+
+    @DisplayName("스트라이크, 볼을 카운트한다")
+    @ParameterizedTest(name = "컴퓨터 : [{0}] 플레이어 : [{1}]")
+    @CsvSource(value = {"1,2,3:1,2,3:3스트라이크", "4,5,6:6,5,4:1스트라이크 2볼", "7,8,9:9,7,8:3볼"}, delimiter = ':')
+    void addCountTest(String computerPickNumber, String ballNumber, String result) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        String[] computerPickNumberArr = computerPickNumber.split(",");
+        for (String str : computerPickNumberArr) {
+            set.add(Integer.parseInt(str));
+        }
+
+        String[] ballNumberArr = ballNumber.split(",");
+
+        Method method = gameService.getClass().getDeclaredMethod("matchResultCounter", LinkedHashSet.class, String[].class);
+        method.setAccessible(true);
+
+        PickNumberMatchResultView resultView = (PickNumberMatchResultView) method.invoke(gameService, set, ballNumberArr);
+        System.out.println(resultView.toString());
+        assertThat(resultView.toString()).isEqualTo(result);
     }
 
 }
