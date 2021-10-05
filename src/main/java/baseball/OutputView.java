f810fd463c9a2777821e03fcbe441a7afc3eefa8
bestsilver <dkvkxm99@gmail.com>
2021-10-06 04:44:17 +0900

    feat: OutputView 구현
    OutputView
    - Console에 출력하는 상수 스트링 정의

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public class OutputView {
+    public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+    public static final String GAME_COMMAND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public static final String MSG_INPUT_LENGTH = "[ERROR] 3자리의 숫자만 입력할 수 있습니다.";
+    public static final String MSG_DUPLICATE_VALUE = "[ERROR] 서로 다른 3가지 숫자를 입력해주세요.";
+    public static final String MSG_NUMBER_VALIDATE = "[ERROR] 1~9 범위의 3자리 숫자만 입력할 수 있습니다.";
+
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
+    public static final String VICTORY_WORD = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+
+}
