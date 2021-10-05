2dc6c453d095180176bf3bd1b027360eb279e8d0
bestsilver <dkvkxm99@gmail.com>
2021-10-06 05:20:56 +0900

    feat: Application 구현
    Application
    - 숫자 야구 게임을 진행한다.
    - 승리 시 게임의 재시작 및 종료 여부를 묻는다.
    - 재시작은 1, 종료는 2를 입력한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        int opinion = GameRule.RESET;
+
+        while (opinion == GameRule.RESET) {
+            new GameRunner().run();
+            OpinionInput opinionInput = new OpinionInput();
+            opinion = opinionInput.getOpinion();
+        }
     }
 }
