f16220ec5ba771ae5803a0b99cf957e10f16a3d9
hyeongbin_kim <hyeongbin_kim@tmax.co.kr>
2021-10-06 08:38:03 +0900

    feat: generate unique 3 numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+
 public class Application {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+    private static final int DIGIT_SIZE = 3;
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        String target = generateNumberString();
+
+    }
+
+    private static String generateNumberString(){
+        StringBuilder result = new StringBuilder();
+        int counter = 0;
+        while (counter < DIGIT_SIZE){
+            String number = String.valueOf(Randoms.pickNumberInRange(START_RANGE, END_RANGE));
+            if (result.indexOf(number) == -1){
+                result.append(number);
+                counter ++;
+            }
+        }
+
+        return result.toString();
     }
 }
