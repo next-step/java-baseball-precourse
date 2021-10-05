8411e4fbc4fd583997b677debad42cbf3f886038
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-06 01:32:41 +0900

    test : 매치결과 카운트, 출력 테스트

--- src/test/java/baseball/domain/PickNumberMatchResultViewTest.java
+++ src/test/java/baseball/domain/PickNumberMatchResultViewTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,4 +17,35 @@ class PickNumberMatchResultViewTest {
         assertThat(resultView.getStrike()).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("매치결과에 따라 카운트가 증가한다")
+    void increaseCountTest() {
+        PickNumberMatchResultView resultView = new PickNumberMatchResultView();
+
+        resultView.addStrikeCount();
+        resultView.addBallCount();
+        resultView.addBallCount();
+
+        assertThat(resultView.getBall()).isEqualTo(2);
+        assertThat(resultView.getStrike()).isEqualTo(1);
+    }
+
+    @DisplayName("매치결과를 출력한다")
+    @ParameterizedTest(name = "{2}")
+    @CsvSource(value = {"3:0:3스트라이크", "1:2:1스트라이크 2볼", "0:3:3볼"}, delimiter = ':')
+    void resultViewToStringTest(int strike, int ball, String result) {
+        PickNumberMatchResultView resultView = new PickNumberMatchResultView();
+
+        for (int i = 0; i < strike; i++) {
+            resultView.addStrikeCount();
+        }
+
+        for (int i = 0; i < ball; i++) {
+            resultView.addBallCount();
+        }
+
+        assertThat(resultView.getStrike()).isEqualTo(strike);
+        assertThat(resultView.getBall()).isEqualTo(ball);
+        assertThat(resultView.toString()).isEqualTo(result);
+    }
 }
