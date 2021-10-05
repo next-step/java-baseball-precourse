d285271ca81b21fa75faf6c82c6d75cd091c8b6c
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-06 01:12:41 +0900

    test : 입력된 값이 유효한지 테스트

--- src/test/java/baseball/service/GameServiceImplTest.java
+++ src/test/java/baseball/service/GameServiceImplTest.java
@@ -1,12 +1,16 @@
 package baseball.service;
 
-import baseball.controller.GameController;
-import org.junit.jupiter.api.BeforeEach;
+import baseball.domain.PickNumberMatchResultView;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static baseball.AppConfig.getGameController;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.LinkedHashSet;
+
 import static baseball.AppConfig.getGameService;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,12 +18,34 @@ class GameServiceImplTest {
 
     GameService gameService = getGameService();
 
-    @DisplayName("입력한 값 유효성 검증 (3자리의 수)")
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "23", "4567", "abc", "ABC", "!@#$%", "123$", "한글입력", "012", "510"})
-    void isValidReadPickNumber(String input) {
+    @DisplayName("입력한 값 유효성 검증")
+    @ParameterizedTest(name = "{index}. 입력한 값 유효성 검증 : [{0}]")
+    @ValueSource(strings = {"1", "23", "4567", "abc", "ABC", "!@#$%", "123$", "한글입력", "012", ""})
+    void isValidReadPickNumberTest(String input) {
         boolean validReadPickNumber = gameService.isValidReadPickNumber(input);
-        assertThat(validReadPickNumber).isEqualTo(false);
+        assertThat(validReadPickNumber).isFalse();
+    }
+
+    @DisplayName("숫자만 입력가능 (0 제외)")
+    @ParameterizedTest(name = "{index}. 숫자만 입력가능 (0 제외) : [{0}]")
+    @ValueSource(strings = {"aaa", "BBB", "!@#","한글테스트", "0", "100"})
+    void isValidOnlyNumberTest(String input) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = gameService.getClass().getDeclaredMethod("isValidOnlyNumber", String.class);
+        method.setAccessible(true);
+
+        boolean result = (boolean) method.invoke(gameService, input);
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("자리수 검증")
+    @ParameterizedTest(name = "{index}. 자리수 검증 : [{0}]")
+    @ValueSource(strings = {"1", "23", "4567"})
+    void isValidPickNumberSizeTest(String input) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = gameService.getClass().getDeclaredMethod("isValidPickNumberSize", String.class);
+        method.setAccessible(true);
+
+        boolean result = (boolean) method.invoke(gameService, input);
+        assertThat(result).isFalse();
     }
 
 }
