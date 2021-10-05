7524fb14a117e33707217dac20915817642f27c5
Ttungga <torb2002@gmail.com>
2021-10-05 21:53:22 +0900

    feat: 게임 종료 메시지 표시 기능
    변경 사항
    - BaseballView: showGameOverMessage 메서드 추가

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 public class BaseballView {
 	private static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요: ";
 	private static final String ERROR_MESSAGE = "[ERROR] 올바른 숫자를 입력해주세요. 중복된 숫자가 없는 세 자리 자연수를 입력해야 합니다.";
+	private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
+	private static final String ENDING_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public void showRequestInputMessage() {
 		System.out.printf(REQUEST_INPUT_MESSAGE);
@@ -21,6 +23,11 @@ public class BaseballView {
 		}
 		resultMessage += getBallMessage(result);
 		System.out.println(resultMessage);
+	}
+
+	public void showGameOverMessage() {
+		System.out.println(WIN_MESSAGE);
+		System.out.println(ENDING_MESSAGE);
 	}
 
 	private String getStrikeMessage(HashMap<String, Integer> result) {
