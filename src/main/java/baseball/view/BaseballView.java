6b7dac8c6fea56cf2d45600f2121f0bccdfc492c
Ttungga <torb2002@gmail.com>
2021-10-05 23:12:47 +0900

    feat: 게임 종료 입력값 에러 메시지 표시 기능
    변경 사항(BaseballView)
    - ERROR_MESSAGE, showErrorMessage 변수, 메서드 이름 수정: NUMBER_INPUT_ERROR_MESSAGE, showNumberInputErrorMessage
    - GAME_OVER_INPUT_ERROR_MESSAGE 변수 추가
    - showGameOverInputErrorMessage 메서드 추가

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -4,16 +4,13 @@ import java.util.HashMap;
 
 public class BaseballView {
 	private static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요: ";
-	private static final String ERROR_MESSAGE = "[ERROR] 올바른 숫자를 입력해주세요. 중복된 숫자가 없는 세 자리 자연수를 입력해야 합니다.";
 	private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
 	private static final String ENDING_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String NUMBER_INPUT_ERROR_MESSAGE = "[ERROR] 올바른 숫자를 입력해주세요. 중복된 숫자가 없는 세 자리 자연수를 입력해야 합니다.";
+	private static final String GAME_OVER_INPUT_ERROR_MESSAGE = "[ERROR] 잘못 입력하셨습니다. " + ENDING_MESSAGE;
 
 	public void showRequestInputMessage() {
 		System.out.printf(REQUEST_INPUT_MESSAGE);
-	}
-
-	public void showErrorMessage() {
-		System.out.println(ERROR_MESSAGE);
 	}
 
 	public void showResultMessage(HashMap<String, Integer> result) {
@@ -28,6 +25,14 @@ public class BaseballView {
 	public void showGameOverMessage() {
 		System.out.println(WIN_MESSAGE);
 		System.out.println(ENDING_MESSAGE);
+	}
+
+	public void showNumberInputErrorMessage() {
+		System.out.println(NUMBER_INPUT_ERROR_MESSAGE);
+	}
+
+	public void showGameOverInputErrorMessage() {
+		System.out.println(GAME_OVER_INPUT_ERROR_MESSAGE);
 	}
 
 	private String getStrikeMessage(HashMap<String, Integer> result) {
