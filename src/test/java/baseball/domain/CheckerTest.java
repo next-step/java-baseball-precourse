29e49fcb01ce8e810f5245b9f7ab6989d8e3b39e
right42 <chjm452@naver.com>
2021-10-06 23:23:40 +0900

    refactor: 확인자 테스트 케이스 추가

--- src/test/java/baseball/domain/CheckerTest.java
+++ src/test/java/baseball/domain/CheckerTest.java
@@ -1,18 +1,19 @@
 package baseball.domain;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CheckerTest {
 
-	@Test
-	void 체크시나리오_낫싱(){
+	@ParameterizedTest
+	@CsvSource(value = {"123:456", "789:456"}, delimiter = ':')
+	void 체크시나리오_낫싱(String answerValue, String goalValue){
 		Checker checker = new Checker();
-		Numbers answer = new Numbers("123");
-		Numbers goal = new Numbers("456");
+		Numbers answer = new Numbers(answerValue);
+		Numbers goal = new Numbers(goalValue);
 
 		checker.answerCheck(answer, goal);
 
@@ -22,11 +23,12 @@ class CheckerTest {
 		assertThat(strike).isZero();
 	}
 
-	@Test
-	void 체크시나리오_볼(){
+	@ParameterizedTest
+	@CsvSource(value = {"123:516:1", "456:548:2", "123:312:3"}, delimiter = ':')
+	void 체크시나리오_볼(String answerValue, String goalValue, int ballCount){
 		Checker checker = new Checker();
-		Numbers answer = new Numbers("123");
-		Numbers goal = new Numbers("451");
+		Numbers answer = new Numbers(answerValue);
+		Numbers goal = new Numbers(goalValue);
 
 		checker.answerCheck(answer, goal);
 
@@ -34,22 +36,35 @@ class CheckerTest {
 		assertThat(strike).isZero();
 
 		int ball = checker.getBall();
-		assertThat(ball).isEqualTo(1);
+		assertThat(ball).isEqualTo(ballCount);
 	}
 
-	@Test
-	void 체크시나리오_스트라이크(){
+	@ParameterizedTest
+	@CsvSource(value = {"123:156:1", "456:458:2", "123:123:3"}, delimiter = ':')
+	void 체크시나리오_스트라이크(String answerValue, String goalValue, int strikeCount){
 		Checker checker = new Checker();
-		Numbers answer = new Numbers("123");
-		Numbers goal = new Numbers("145");
+		Numbers answer = new Numbers(answerValue);
+		Numbers goal = new Numbers(goalValue);
 
 		checker.answerCheck(answer, goal);
 
-		int strike = checker.getStrike();
-		assertThat(strike).isEqualTo(1);
-
 		int ball = checker.getBall();
 		assertThat(ball).isZero();
+
+		int strike = checker.getStrike();
+		assertThat(strike).isEqualTo(strikeCount);
+
+	}
+
+	@Test
+	void 올스트라이크_확인(){
+		Checker checker = new Checker();
+		Numbers answer = new Numbers("123");
+		Numbers goal = new Numbers("123");
+
+		checker.answerCheck(answer, goal);
+
+		assertThat(checker.isAllStrike()).isTrue();
 	}
 
 }
