02f84ac59be743a73b5d29676072bd98714b1b41
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-10-06 10:42:50 +0900

    feat: 게임 생성 시 정답 (1부터 9까지 서로 다른 수로 이루어진 3자리 숫자) 주입

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -2,4 +2,34 @@ package baseball.domain;
 
 public class Game {
 
+    private final String answer;
+
+    public Game(final String answer) {
+        validate(answer);
+        this.answer = answer;
+    }
+
+    private void validate(final String answer) {
+        try {
+            Integer.parseInt(answer);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("[ERROR] 숫자 외 문자는 입력할 수 없습니다.");
+        }
+
+        if (answer.length() != 3) {
+            throw new IllegalArgumentException("[ERROR] 3자리 외 숫자는 입력할 수 없습니다.");
+        }
+
+        if (answer.contains("0")) {
+            throw new IllegalArgumentException("[ERROR] 각 자리에 1부터 9까지 외 다른 숫자는 입력할 수 없습니다.");
+        }
+
+        for (int i = 0; i < answer.length() - 1; i++) {
+            for (int j = i + 1; j < answer.length(); j++) {
+                if (answer.charAt(i) == answer.charAt(j)) {
+                    throw new IllegalArgumentException("[ERROR] 서로 같은 수로 이루어진 숫자를 입력할 수 없습니다.");
+                }
+            }
+        }
+    }
 }
--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -1,15 +1,29 @@
 package baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GameTest {
 
     @DisplayName("게임을 생성할 수 있다.")
     @Test
     void create() {
-        assertDoesNotThrow(Game::new);
+        assertDoesNotThrow(
+            () -> new Game("123")
+        );
+    }
+
+    @DisplayName("1부터 9까지 서로 다른 수로 이루어진 3자리의 수 외 다른 숫자를 게임의 정답으로 지정할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "a", "1", "12", "1234", "012", "102", "120", "112", "122", "111"})
+    void createWithInvalidAnswer(final String answer) {
+        assertThatThrownBy(
+            () -> new Game(answer)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
