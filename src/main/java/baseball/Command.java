f4cec0362f2c7ed84c271ac2215f71ee0295d8a7
gaeul <toheart@gmail.com>
2021-10-06 22:44:53 +0900

    refactor: switch 제거

--- src/main/java/baseball/Command.java
+++ src/main/java/baseball/Command.java
@@ -1,21 +1,10 @@
 package baseball;
 
 public enum Command {
-    RESTART("1"), EXIT("2");
-    private final String value;
-
-    Command(String value) {
-        this.value = value;
-    }
+    RESTART, EXIT;
 
     public static Command of(String value) {
-        switch (value) {
-            case "1":
-                return RESTART;
-            case "2":
-                return EXIT;
-            default:
-                throw new IllegalArgumentException("[ERROR] 종료 커맨드가 올바르지 않습니다");
-        }
+        if ("1".equals(value)) return RESTART;
+        return EXIT;
     }
 }
