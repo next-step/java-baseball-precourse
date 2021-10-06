6e148614e447f1915abd89bd8c269261d53999a2
hyeongbin_kim <hyeongbin_kim@tmax.co.kr>
2021-10-06 09:36:15 +0900

    feat: receive and validate user input
    - receive user input from the screen
    - check if the input is 3 lengths and contains digit only

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,21 @@
 package baseball;
 
+import nextstep.utils.Console;
 import nextstep.utils.Randoms;
 
 public class Application {
     private static final int START_RANGE = 1;
     private static final int END_RANGE = 9;
     private static final int DIGIT_SIZE = 3;
+    private static final String ONLY_DIGIT = "\\d+";
 
     public static void main(String[] args) {
+        System.out.println("숫자를 입력해 주세요: ");
         String target = generateNumberString();
-
+        String input = Console.readLine();
+        if (!validateInput(input)){
+            System.out.println("[ERROR] 1부터 9까지 숫자 3개만 입력해주세요. (예시) 123");
+        }
     }
 
     private static String generateNumberString(){
@@ -24,5 +30,12 @@ public class Application {
         }
 
         return result.toString();
+    }
+
+    private static boolean validateInput(String input){
+        if (input.matches(ONLY_DIGIT) && input.length() == 3){
+            return true;
+        }
+        return false;
     }
 }
