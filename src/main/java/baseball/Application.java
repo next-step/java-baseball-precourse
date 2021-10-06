6480fbab84e669721cd47ff455e5165f438a8297
gkfgran5037 <gkfgran5037@naver.com>
2021-10-06 21:08:55 +0900

    feat : 게임을 진행한다 - 1) 게임 재시작, 2) 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,19 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        Baseball game = new Baseball();
+        int numCnt = 3;
+        int answer;
+        while(true)
+        {
+            game.runBaseBallGeme(numCnt, game.setRandomNums(numCnt));
+            answer = game.goOrStop();
+            if (answer == 1) {
+                continue;
+            }
+            if (answer == 2) {
+                break;
+            }
+        }
     }
 }
