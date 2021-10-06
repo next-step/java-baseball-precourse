b851675d75f4c3c22e49aca5a3dd4b272a8a02b5
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-10-06 17:59:37 +0900

    test: 예제 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+
 import nextstep.test.NSTest;
 import nextstep.utils.Randoms;
 import org.junit.jupiter.api.AfterEach;
@@ -7,10 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mockStatic;
-
 public class ApplicationTest extends NSTest {
+
     @BeforeEach
     void beforeEach() {
         super.setUp();
@@ -20,8 +21,8 @@ public class ApplicationTest extends NSTest {
     void 낫싱() {
         try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
             mockRandoms
-                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-                    .thenReturn(1, 3, 5);
+                .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(1, 3, 5);
             running("246");
             verify("낫싱");
         }
@@ -31,10 +32,20 @@ public class ApplicationTest extends NSTest {
     void 게임종료_후_재시작() {
         try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
             mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-                    .thenReturn(7, 1, 3)
-                    .thenReturn(5, 8, 9);
+                .thenReturn(7, 1, 3)
+                .thenReturn(5, 8, 9);
             run("713", "1", "597", "589", "2");
             verify("3스트라이크", "게임 끝", "1스트라이크 1볼");
+        }
+    }
+
+    @Test
+    void 예제() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(7, 1, 3);
+            run("123", "145", "671", "216", "713", "2");
+            verify("1스트라이크 1볼", "1볼", "2볼", "1스트라이크", "3스트라이크");
         }
     }
 
