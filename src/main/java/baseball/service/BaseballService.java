e6350db3741b9ee4bb6e0237a689df8f23069f5c
kkwan0226 <kkwan0226@gmail.com>
2021-10-06 16:16:39 +0900

    feature(BaseballService.java): 사용자로부터 문자열을 입력 받는 기능 구현

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,11 +1,27 @@
 package baseball.service;
 
+import static baseball.util.BallListGenerator.*;
+
+import baseball.exception.BaseballException;
 import baseball.model.BallList;
 import baseball.util.BallListGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class BaseballService {
 
     public static BallList generateComputerBallList() {
         return new BallList(BallListGenerator.generateComputerBallList());
+    }
+
+    public static BallList inputUserBall() {
+        try {
+            String inputUserBallList = InputView.enterValue();
+            return generateUserBallList(inputUserBallList);
+        } catch (BaseballException baseballException) {
+            OutputView.error(baseballException);
+            return inputUserBall();
+        }
+
     }
 }
