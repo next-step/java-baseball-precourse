e6a1aa527248c3552691a6bcf90368368961b712
rura6502 <rura6502@hotmail.com>
2021-10-06 18:42:40 +0900

    feat(service): 게임 결과 출력 파트 구현
    * 게임 결과 상황별 출력 파트 구현

--- src/main/java/baseball/domain/GameMessagePrinter.java
+++ src/main/java/baseball/domain/GameMessagePrinter.java
@@ -6,8 +6,23 @@ public class GameMessagePrinter {
         System.out.print(gameMessage.getMessage());
     }
 
-    public static String print(BaseballGameScoreBoard result) {
-        //!TODO need implement
-        return "";
+    public static void print(BaseballGameScoreBoard board) {
+
+        if (board.isNothing()) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        String message = "";
+
+        if (board.getStrikeCount() > 0) {
+            message += String.format("%d스트라이크 ", board.getStrikeCount());
+        }
+
+        if (board.getBallCount() > 0) {
+            message += String.format("%d볼", board.getBallCount());
+        }
+
+        System.out.println(message.trim());
     }
 }
