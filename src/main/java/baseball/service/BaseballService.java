ab1f110403b15590a9ee8eb64d86bfb53bc59567
kkwan0226 <kkwan0226@gmail.com>
2021-10-06 16:18:57 +0900

    feature(BaseballService.java): ball count 출력 및 게임 재시작 기능 추가

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -4,6 +4,9 @@ import static baseball.util.BallListGenerator.*;
 
 import java.util.List;
 
+import baseball.code.BallCount;
+import baseball.code.ErrorMessage;
+import baseball.code.GameStatus;
 import baseball.exception.BaseballException;
 import baseball.model.BallList;
 import baseball.util.BallListGenerator;
@@ -29,5 +32,36 @@ public class BaseballService {
 
     public static List<Integer> ballCount(BallList computerBallList, BallList userBallList) {
         return computerBallList.ballCount(userBallList);
+    }
+
+    public static GameStatus checkBallCount(List<Integer> ballCount) {
+        OutputView.ballCount(BallCount.toString(ballCount));
+        if (ballCount.get(0).equals(3)) {
+            OutputView.endGame();
+            return toBeContinue();
+        }
+        return GameStatus.CONTINUE_GAME;
+    }
+
+    private static GameStatus toBeContinue() {
+        try {
+            return validateDecideRestart();
+
+        } catch (BaseballException baseballException) {
+            OutputView.error(baseballException);
+            return toBeContinue();
+        }
+    }
+
+    private static GameStatus validateDecideRestart() {
+        String decideRestart = InputView.decideRestart();
+        if (!decideRestart.equals(GameStatus.RESTART_GAME.getGameStatus())
+                && !decideRestart.equals(GameStatus.END_GAME.getGameStatus())) {
+            throw new BaseballException(ErrorMessage.INVALID_DECIDE_RESTART_STRING.getErrorMessage());
+        }
+        if (decideRestart.equals(GameStatus.END_GAME.getGameStatus())) {
+            return GameStatus.END_GAME;
+        }
+        return GameStatus.RESTART_GAME;
     }
 }
