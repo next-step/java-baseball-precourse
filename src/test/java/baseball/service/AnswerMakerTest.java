d28942fca14099f9d37ea2602ba7aeb2e8de66e6
rura6502 <rura6502@hotmail.com>
2021-10-06 16:29:58 +0900

    test(service): 정답 생성기 테스트 코드 구현
    정답(answer) 생성기 테스트 코드 구현
    * validation 테스트 코드 구현

---
+++ src/test/java/baseball/service/AnswerMakerTest.java
@@ -0,0 +1,48 @@
+package baseball.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.service.AnswerMaker.isValid;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswerMakerTest {
+
+    @Test
+    @DisplayName("제약사항 만족하는 answer 문자열 생성 검증")
+    void valid_make() {
+        List<String> answers = new ArrayList<>();
+        for (int i=0; i<1000; i++) {
+            answers.add(AnswerMaker.make());
+        }
+        assertThat(answers)
+          .allMatch(answer -> isValid(answer));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "12a", "ab2", "가나1", "", "  "})
+    @DisplayName("answer는 문자가 있을 수 없음")
+    void test_only_number_can_be_answer(String answer) {
+        assertThat(isValid(answer)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12", "1", "1234", "12513324123", "", " "})
+    @DisplayName("answer는 3개의 숫자로 이루어져야 함")
+    void test_only_three_numbers_can_be_answer(String answer) {
+        assertThat(isValid(answer)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"111", "112", "101", "012", "000", "001", "100"})
+    @DisplayName("answer는 3개의 중복되지 않는, 0을 제외한 숫자로 이루어져야 함")
+    void test_every_characters_should_be_unique_and_not_zero(String answer) {
+
+        assertThat(isValid(answer)).isFalse();
+    }
+}
