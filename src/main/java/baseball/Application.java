fcd8f9bbd42a40c459c324747147b89f414c1f77
jin338s <jin3378s@gmail.com>
2021-10-06 23:03:25 +0900

    Feat: 애플리케이션 구동 시 야구게임 시작.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.interfaces.controllers.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        GameController gameController = new GameController();
+
+        gameController.playBall();
     }
 }
