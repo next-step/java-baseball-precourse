60adc5f66eb019781e5741513959905e5487b31c
cothis <minho1030@naver.com>
2021-10-06 15:29:08 +0900

    feat(GameController): GameController 개발

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,37 @@
+package baseball.controller;
+
+public class GameController {
+	private final GameView gameView;
+	private final GameService gameService;
+
+	public GameController(GameView gameView, GameService gameService) {
+		this.gameView = gameView;
+		this.gameService = gameService;
+	}
+
+	public void index() {
+		do {
+			restartGame();
+		} while (isResume());
+	}
+
+	public void restartGame() {
+		gameService.generateAnswer();
+		boolean isAnswer = false;
+		do {
+			isAnswer = requestAnswer();
+		} while (!isAnswer);
+	}
+
+	public boolean isResume() {
+		String input = gameView.InputMenuMessage();
+		return "1".equals(input);
+	}
+
+	public boolean requestAnswer() {
+		String input = gameView.inputAnswerMessage();
+		CompareResult compareResult = gameService.compareAnswer(input);
+
+		return compareResult.isAnswer();
+	}
+}
