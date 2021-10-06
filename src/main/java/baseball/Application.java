8db626e35c66304411d002488c7a4a38213b55ea
sonyoon7 <sonyoon7@gmail.com>
2021-10-06 20:01:12 +0900

    게임 시작 메소드 호출 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+
+        BaseballGame baseballGame = new BaseballGame();
+        while (true) {
+            baseballGame.play();
+            if (!baseballGame.isNewGame()) {
+                break;
+            }
+        }
     }
 }
