e1b5896a997102e3d2dc4f65f11b2946cf1a4499
serena <noriming2@gmail.com>
2021-10-06 15:11:23 +0900

    feat(input): 입력 된 야구 숫자에 대한 검증 기능 추가
    1. 입력 된 숫자가 없으면 NoSuchElementException 발생
    2. 정규표현식으로 숫자의 범위와 개수를 확인
    3. HashSet을 이용하여 입력된 값을 중복을 확인
      - Set에 담긴 개수와 입력 된 개수 차이 확인
    4. 검증 실패 시 예외 발생
    5. 예외 발생에 대한 테스트 추가

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,24 +1,61 @@
 package baseball;
 
-import java.io.ByteArrayInputStream;
+import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import java.io.ByteArrayInputStream;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputTest {
 	@Test
-	void convert_string_to_numeric_list(){
+	void convert_string_to_numeric_list() {
 		input("123");
 
 		BaseballNumbers baseballNumbers = Input.readPlayerBaseballNumbers();
 
-		Assertions.assertThat(baseballNumbers.getBaseballNumbers())
-			.containsExactly(1,2,3);
+		assertThat(baseballNumbers.getBaseballNumbers())
+			.containsExactly(1, 2, 3);
 	}
 
-	private void input(String value){
+	@DisplayName("중복된 값이 입력되면 예외가 발생한다")
+	@Test
+	void given_duplicated_numbers_then_throw_exception() {
+		input("122");
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> Input.readPlayerBaseballNumbers())
+			.withMessage("입력 된 숫자에 중복이 있으면 안됩니다.");
+	}
+
+	@DisplayName("3개의 숫자가 아니면 예외가 발생한다")
+	@Test
+	void given_number_other_than_three_then_throw_exception() {
+		input("12d");
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> Input.readPlayerBaseballNumbers())
+			.withMessage("3개의 숫자(1~9)를 입력 하세요.");
+	}
+
+	@DisplayName("1과 9사이의 숫자가 아니면 예외가 발생한다")
+	@Test
+	void given_not_between_1_and_9_then_throw_exception() {
+		input("109");
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> Input.readPlayerBaseballNumbers())
+			.withMessage("3개의 숫자(1~9)를 입력 하세요.");
+	}
+
+	@DisplayName("입력 값이 없으면 예외가 발생한다")
+	@Test
+	void when_no_input_value_then_throw_exception() {
+		input("");
+		assertThatExceptionOfType(NoSuchElementException.class)
+			.isThrownBy(() -> Input.readPlayerBaseballNumbers());
+	}
+
+	private void input(String value) {
 		ByteArrayInputStream inputStream = new ByteArrayInputStream(value.getBytes());
 		System.setIn(inputStream);
 	}
-
 }
