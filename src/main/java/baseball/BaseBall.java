e3724c7786df90f34b53256ac139c41ec1dd1d59
Kim Yong Kwan <sellester@naver.com>
2021-10-06 20:05:44 +0900

    Update BaseBall.java
    feat : 출력 기능 구현

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -3,6 +3,8 @@ package baseball;
 import nextstep.utils.Console;
 
 public class BaseBall {
+	private static int strikeCount;
+	private static int ballCount;
 
 	private String inNumber() {
 		System.out.print("\n숫자를 입력해 주세요 : ");
@@ -12,5 +14,20 @@ public class BaseBall {
 	private String restartQuestion() {
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
 		return Console.readLine();
+	}
+
+	private void outputPrint() {
+		if (strikeCount != 0) {
+			System.out.print(strikeCount + "스트라이크 ");
+		}
+		if (ballCount != 0) {
+			System.out.print(ballCount + "볼");
+		}
+		if (strikeCount == 0 && ballCount == 0) {
+			System.out.print("낫싱");
+		}
+		if (strikeCount == 3) {
+			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+		}
 	}
 }
