b999f281c12e856fd91d2785acc801691142423a
islee <islee@howser.co.kr>
2021-10-06 19:02:44 +0900

    feat : 에러메세지 추가
    게임시작/종료 오류메세지 추가

---
+++ src/main/java/baseball/common/Const.java
@@ -0,0 +1,25 @@
+package baseball.common;
+
+public class Const {
+
+    public static final Integer IMPOSSIBLE_NUMBER = 0;
+    public static final Integer NEW_GAME = 1;
+    public static final Integer QUIT = 2;
+
+    public static final Integer BALL_COUNT = 3;
+
+    public static final String STRIKE = " 스트라이크 ";
+    public static final String BALL = " 볼 ";
+    public static final String NOTHING = "낫싱";
+
+    public static final String START_MESSAGE = "야구 숫자를 입력하세요 : ";
+    public static final String NEW_GAME_OR_QUIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String  WINNING_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String NUMBER_FORMAT_EXCEPTION = "[ERROR] 입력값이 올바르지 않습니다. 숫자를 입력해주세요.";
+    public static final String SIZE_EXCEPTION = "[ERROR] 자리수가 올바르지 않습니다.";
+    public static final String DUPLICATE_EXCEPTION = "[ERROR] 중복된 값이 있습니다.";
+    public static final String IMPOSSIBLE_NUMBER_EXCEPTION ="[ERROR] 0을 제외한 숫자를 입력하세요.";
+    public static final String NEW_GAME_OPTION_EXCEPTION ="[ERROR] 올바르지 않습니다. 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
