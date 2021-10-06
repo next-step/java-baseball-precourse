7584929fc10c981515b73d89434a08691adb5cda
jongwon_lee <jongwon_lee@tmax.co.kr>
2021-10-06 19:28:44 +0900

    feat: 게임 결과를 보여주는 view 기능 구현

--- src/main/java/baseball/model/ConstValue.java
+++ src/main/java/baseball/model/ConstValue.java
@@ -6,5 +6,9 @@ public interface ConstValue {
 	int BASEBALL_MAX_NUMBER = 9;
 
 	String PLAYER_INPUT_NUMBER = "숫자를 입력해 주세요: ";
+	String GAME_RULE_STRIKE = "스트라이크";
+	String GAME_RULE_BALL = "볼";
+	String GAME_RULE_NOTHING = "낫싱";
+	String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
 
 }
--- src/main/java/baseball/view/GameResultView.java
+++ src/main/java/baseball/view/GameResultView.java
@@ -1,2 +1,44 @@
-package baseball.view;public class GameResultView {
+package baseball.view;
+
+import baseball.model.ConstValue;
+import baseball.model.PlayerBaseballNumber;
+
+public class GameResultView {
+
+	public static void printGameRoundResult(PlayerBaseballNumber playerBaseballNumber) {
+		System.out.println(generateGameMessage(playerBaseballNumber));
+
+		if(playerBaseballNumber.getStrikeCount()==3)
+			System.out.println(ConstValue.GAME_END);
+	}
+
+	private static String generateGameMessage(PlayerBaseballNumber playerBaseballNumber) {
+		return generateStrikeCountMessgae(playerBaseballNumber) +
+			generateBallCountMessage(playerBaseballNumber) +
+			generateNothingMessgae(playerBaseballNumber);
+	};
+
+	private static String generateBallCountMessage(PlayerBaseballNumber playerBaseballNumber) {
+		if(playerBaseballNumber.getBallCount() > 0)
+			return Integer.toString(playerBaseballNumber.getBallCount()) +
+				ConstValue.GAME_RULE_BALL;
+
+		return "";
+	};
+
+	private static String generateStrikeCountMessgae(PlayerBaseballNumber playerBaseballNumber) {
+		if(playerBaseballNumber.getStrikeCount() > 0)
+			return Integer.toString(playerBaseballNumber.getStrikeCount()) +
+				ConstValue.GAME_RULE_STRIKE;
+
+		return "";
+	};
+
+	private static String generateNothingMessgae(PlayerBaseballNumber playerBaseballNumber) {
+		if(playerBaseballNumber.getStrikeCount() == 0 &&
+		playerBaseballNumber.getBallCount() == 0)
+			return ConstValue.GAME_RULE_NOTHING;
+
+		return "";
+	};
 }
