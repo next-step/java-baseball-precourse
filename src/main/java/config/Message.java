2407f6a943b92094b2967622e931dc4e772bd254
arumlee <arumlee.above@gmail.com>
2021-10-06 23:39:08 +0900

    add : 에러메시지
    중복된 세자리 숫자 입력시 에러 리턴

--- src/main/java/config/Message.java
+++ src/main/java/config/Message.java
@@ -9,4 +9,5 @@ public class Message {
 	public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 	public static final String CHOOSE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	public static final String GAME_END_MESSAGE = INPUT_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 끝";
+	public static final String ERROR_SIZE = "[ERROR] 중복이 없는 세자리 숫자로 다시 입력해주세요.";
 }
