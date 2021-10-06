a2c02ee2f012dff96c2067cdeba931f4c32ccef7
rura6502 <rura6502@hotmail.com>
2021-10-06 23:10:45 +0900

    chore(no scope): 테스트 코드 메소드 한글 네이밍 수정
    기존 테스트 코드 메소드 네이밍 방식 통일

--- src/test/java/baseball/service/AnswerMakerTest.java
+++ src/test/java/baseball/service/AnswerMakerTest.java
@@ -15,7 +15,7 @@ public class AnswerMakerTest {
 
     @Test
     @DisplayName("제약사항 만족하는 answer 문자열 생성 검증")
-    void valid_make() {
+    void 정답_생성_유효성_테스트() {
         List<String> answers = new ArrayList<>();
         for (int i=0; i<1000; i++) {
             answers.add(AnswerMaker.make());
@@ -27,21 +27,21 @@ public class AnswerMakerTest {
     @ParameterizedTest
     @ValueSource(strings = {"abc", "12a", "ab2", "가나1", "", "  "})
     @DisplayName("answer는 문자가 있을 수 없음")
-    void test_only_number_can_be_answer(String answer) {
+    void 정답_문자_포함_시_unvalid_테스트(String answer) {
         assertThat(isValid(answer)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"12", "1", "1234", "12513324123", "", " "})
     @DisplayName("answer는 3개의 숫자로 이루어져야 함")
-    void test_only_three_numbers_can_be_answer(String answer) {
+    void 정답_숫자_3개_아닐_시_unvalid_테스트(String answer) {
         assertThat(isValid(answer)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"111", "112", "101", "012", "000", "001", "100"})
     @DisplayName("answer는 3개의 중복되지 않는, 0을 제외한 숫자로 이루어져야 함")
-    void test_every_characters_should_be_unique_and_not_zero(String answer) {
+    void 정답_ZERO_또는_중복_포함_시_unvalid_테스트(String answer) {
 
         assertThat(isValid(answer)).isFalse();
     }
