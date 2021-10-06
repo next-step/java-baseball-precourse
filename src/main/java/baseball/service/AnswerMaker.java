be7ab98138fa8af62740564a47038d597763dd55
rura6502 <rura6502@hotmail.com>
2021-10-06 16:31:02 +0900

    chore(service): 주석 코드 정리

--- src/main/java/baseball/service/AnswerMaker.java
+++ src/main/java/baseball/service/AnswerMaker.java
@@ -24,32 +24,4 @@ public class AnswerMaker {
     public static boolean isValid(String answer) {
         return answer.matches("^(?!.*(.).*\\1)[1-9]{3}$");
     }
-
-//    public static boolean isValid(String answer) {
-//        return !isContainsZero(answer)
-//          && isLengthThree(answer)
-//          && isOnlyDigits(answer)
-//          && isNoRepeat(answer);
-//    }
-//
-//    private static boolean isContainsZero(String answer) {
-//        return answer.contains("0");
-//    }
-//
-//    private static boolean isLengthThree(String answer) {
-//        return answer.length() == 3;
-//    }
-//
-//    private static boolean isOnlyDigits(String answer) {
-//        boolean result = true;
-//        for (char c: answer.toCharArray()) {
-//            result = result && Character.isDigit(c);
-//        }
-//        return result;
-//    }
-//
-//    private static boolean isNoRepeat(final String answer) {
-//
-//
-//    }
 }
