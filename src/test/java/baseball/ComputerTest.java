4bfdd4433edc9cb1c8b305d82324dc5786a34954
serena <noriming2@gmail.com>
2021-10-06 12:18:04 +0900

    feat(baseballnumber): 서로 다른 숫자 3개를 선택하여 BaseballNumbers 생성
    1. Randoms 클래스를 이용하여 1에서 9 사이의 랜덤 숫자를 생성한다
    2. 생성된 숫자를 리스트 크기가 3이 될 때 까지 채운다.
    3. 숫자의 중복을 방지 하기 위하여 리스트에 숫자를 넣을 때 이미 있는 숫자인지 판단한다.

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+	@Test
+	void create_three_random_number() {
+
+		Computer computer = new Computer();
+		BaseballNumbers baseballNumbers = computer.createBaseballNumbers();
+
+		assertThat(baseballNumbers.getBaseballNumbers())
+			.hasSize(3);
+	}
+}
