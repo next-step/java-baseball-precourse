fc4e893059409ad48caac5ca5fa99ce74f48c70a
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-10-06 17:05:59 +0900

    feat: Count 값 객체를 플레이어가 읽을 수 있는 문자열로 변환하는 기능 추가

--- src/main/java/baseball/domain/Count.java
+++ src/main/java/baseball/domain/Count.java
@@ -1,11 +1,18 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Count {
 
     private static final int UPPER_BOUND = 3;
     private static final int LOWER_BOUND = 0;
+
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String DELIMITER = " ";
 
     private final int numberOfStrikes;
     private final int numberOfBalls;
@@ -23,6 +30,33 @@ public class Count {
 
         if (numberOfBalls < LOWER_BOUND || numberOfBalls > UPPER_BOUND) {
             throw new IllegalArgumentException("[ERROR] 볼 갯수는 0개~3개를 벗어난 범위의 값을 입력할 수 없습니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        final List<String> logs = new ArrayList<>();
+        onNothing(logs);
+        onStrikes(logs);
+        onBalls(logs);
+        return String.join(DELIMITER, logs);
+    }
+
+    private void onNothing(final List<String> logs) {
+        if (numberOfStrikes == 0 && numberOfBalls == 0) {
+            logs.add(NOTHING);
+        }
+    }
+
+    private void onStrikes(final List<String> logs) {
+        if (numberOfStrikes > 0) {
+            logs.add(numberOfStrikes + STRIKE);
+        }
+    }
+
+    private void onBalls(final List<String> logs) {
+        if (numberOfBalls > 0) {
+            logs.add(numberOfBalls + BALL);
         }
     }
 
--- src/test/java/baseball/domain/CountTest.java
+++ src/test/java/baseball/domain/CountTest.java
@@ -36,4 +36,71 @@ public class CountTest {
 
         assertThat(a).isEqualTo(b);
     }
+
+    @DisplayName("낫싱")
+    @Test
+    void nothing() {
+        final Count count = createCount(0, 0);
+        assertThat(count.toString()).isEqualTo("낫싱");
+    }
+
+    @DisplayName("1스트라이크")
+    @Test
+    void oneStrike() {
+        final Count count = createCount(1, 0);
+        assertThat(count.toString()).isEqualTo("1스트라이크");
+    }
+
+    @DisplayName("2스트라이크")
+    @Test
+    void twoStrikes() {
+        final Count count = createCount(2, 0);
+        assertThat(count.toString()).isEqualTo("2스트라이크");
+    }
+
+    @DisplayName("3스트라이크")
+    @Test
+    void threeStrikes() {
+        final Count count = createCount(3, 0);
+        assertThat(count.toString()).isEqualTo("3스트라이크");
+    }
+
+    @DisplayName("1볼")
+    @Test
+    void oneBall() {
+        final Count count = createCount(0, 1);
+        assertThat(count.toString()).isEqualTo("1볼");
+    }
+
+    @DisplayName("2볼")
+    @Test
+    void twoBalls() {
+        final Count count = createCount(0, 2);
+        assertThat(count.toString()).isEqualTo("2볼");
+    }
+
+    @DisplayName("3볼")
+    @Test
+    void threeBalls() {
+        final Count count = createCount(0, 3);
+        assertThat(count.toString()).isEqualTo("3볼");
+    }
+
+    @DisplayName("1스트라이크 1볼")
+    @Test
+    void oneStrikeOneBall() {
+        final Count count = createCount(1, 1);
+        assertThat(count.toString()).isEqualTo("1스트라이크 1볼");
+    }
+
+    @DisplayName("1스트라이크 2볼")
+    @Test
+    void oneStrikeTwoBalls() {
+        final Count count = createCount(1, 2);
+        assertThat(count.toString()).isEqualTo("1스트라이크 2볼");
+    }
+
+    private Count createCount(final int numberOfStrikes, final int numberOfBalls) {
+        return new Count(numberOfStrikes, numberOfBalls);
+    }
 }
