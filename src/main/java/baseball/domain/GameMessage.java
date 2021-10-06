829921d754c5610096a63a25581e869f3c3f8b2b
rura6502 <rura6502@hotmail.com>
2021-10-06 18:43:49 +0900

    fix(service): 메세지 오타 수정, 내용 디테일 업데이트

--- src/main/java/baseball/domain/GameMessage.java
+++ src/main/java/baseball/domain/GameMessage.java
@@ -4,11 +4,11 @@ public enum GameMessage {
 
     ASK_INPUT("숫자를 입력해주세요 : ")
     , END_SINGLE_GAME_WITH_THREE_STRIKES("3개의 숫자를 모두 맞히셨습니다! 게임 끝\n")
-    , ASK_RESTART_OR_DONE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n")
+    , ASK_RESTART_OR_DONE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
     , END_PROGRAM("모든 게임이 종료되었습니다. 프로그램이 종료됩니다.\n")
 
     , ERR_PUT_ONLY_1_OR_2(makeErrorMessage("1 또는 2를 입력해주세요.\n"))
-    , ERR_PUT_ONLY_THREE_NUMBERS(makeErrorMessage("3개의 숫자를 입력해주세요.\n"))
+    , ERR_PUT_ONLY_THREE_NUMBERS(makeErrorMessage("0을 제외한 3개의 중복되지 않는 숫자를 입력해주세요.\n"))
 
     ;
 
