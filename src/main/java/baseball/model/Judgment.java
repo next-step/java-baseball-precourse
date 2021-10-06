f35b7fef908f6bbe0a6ddea1c6773a93807d6ba5
jhhwang <jhhwang@diquest.com>
2021-10-06 23:53:30 +0900

    FEAT 판정 기능 추가

---
+++ src/main/java/baseball/model/Judgment.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import baseball.comm.Constant;
+import baseball.view.BaseballGame;
+
+public class Judgment {
+
+    private static final Judgment JUDGMENT = new Judgment();
+
+    private Judgment() {}
+
+    public static Judgment getInstance() {
+        return JUDGMENT;
+    }
+
+    public void judgment(String oppnentNumbers, String playerNumbers) {
+        for(int i = 0; i < Constant.NUM_SIZE; i++){
+            char checkChar = playerNumbers.charAt(i);
+            judgmentNumbers(oppnentNumbers,checkChar, i);
+        }
+    }
+
+    private void judgmentNumbers(String oppnentNumbers, char checkChar, int checkIndex) {
+        if(oppnentNumbers.charAt(checkIndex) == checkChar){
+            ++BaseballGame.strike;
+        }
+
+        if(oppnentNumbers.indexOf(checkChar) > -1){
+            ++BaseballGame.ball;
+        }
+    }
+
+}
