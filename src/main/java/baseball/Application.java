a48848151d5c70f98278ebb66e6cc563dd94b56f
islee <islee@howser.co.kr>
2021-10-06 19:03:15 +0900

    feat : 숫자야구 Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import baseball.domain.BaseballController;
+import baseball.domain.BaseballNumberGenerator;
+
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        do {
+            List<Integer> computerBaseballNumbers = BaseballNumberGenerator.generateBaseballNumbers();
+            BaseballController.startBaseballGame(computerBaseballNumbers);
+        } while(BaseballController.isNewGame());
     }
 }
+
+
+
+
+
