253aa0a945dce41d1cd6446d4db28e7eddcfa0fa
JaeHan-Kim <hgal8877@gmail.com>
2021-10-06 21:40:04 +0900

    feat (baseball): 야구게임 기능을 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.domain.BaseBallGame;
+import baseball.domain.BassBallNumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        List<Integer> answer;
+        do {
+            answer = BassBallNumberGenerator.createRandomNumbers();
+        } while(BaseBallGame.playBall(answer));
     }
 }
