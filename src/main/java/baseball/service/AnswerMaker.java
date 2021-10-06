ef4852fa766c853c2125a9bd8fb7374b80fdbfe2
rura6502 <rura6502@hotmail.com>
2021-10-06 16:28:51 +0900

    feat(service): 정답 생성기 구현
    정답(answer) 생성기 구현
    * answer validation 로직 구현

--- src/main/java/baseball/service/AnswerMaker.java
+++ src/main/java/baseball/service/AnswerMaker.java
@@ -1,14 +1,55 @@
 package baseball.service;
 
+import static nextstep.utils.Randoms.pickNumberInRange;
+
 public class AnswerMaker {
 
     public static String make() {
-        //!TODO 정답 생성기 구현
-        return "";
+        return make("");
+    }
+
+    private static String make(String answer) {
+        if (answer.length() == 3){
+            return answer;
+        }
+
+        String newNumber = Integer.toString(pickNumberInRange(1, 9));
+        while (answer.contains(newNumber)) {
+            newNumber = Integer.toString(pickNumberInRange(1, 9));;
+        }
+        answer += newNumber;
+        return make(answer);
     }
 
     public static boolean isValid(String answer) {
-        //!TODO regex 사용 검증
-        return true;
+        return answer.matches("^(?!.*(.).*\\1)[1-9]{3}$");
     }
+
+//    public static boolean isValid(String answer) {
+//        return !isContainsZero(answer)
+//          && isLengthThree(answer)
+//          && isOnlyDigits(answer)
+//          && isNoRepeat(answer);
+//    }
+//
+//    private static boolean isContainsZero(String answer) {
+//        return answer.contains("0");
+//    }
+//
+//    private static boolean isLengthThree(String answer) {
+//        return answer.length() == 3;
+//    }
+//
+//    private static boolean isOnlyDigits(String answer) {
+//        boolean result = true;
+//        for (char c: answer.toCharArray()) {
+//            result = result && Character.isDigit(c);
+//        }
+//        return result;
+//    }
+//
+//    private static boolean isNoRepeat(final String answer) {
+//
+//
+//    }
 }
