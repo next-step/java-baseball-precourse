e427612de3b95507f42476258831e1cd861bfdf7
MAC <Haua@yahoo.com>
2021-10-06 21:06:27 +0900

    feat(controller):최초등록

---
+++ src/main/java/baseball/controller/GameStartController.java
@@ -0,0 +1,35 @@
+package baseball.controller;
+
+import baseball.model.BallCount;
+import baseball.model.UserInputValue;
+import baseball.view.InputView;
+
+public class GameStartController {
+
+	private static final String THREE_STRIKE = "3스트라이크";
+	
+	public static void startGame(int randomValue) {
+		
+		String strikeAndBallResult = "";
+		
+		while(!strikeAndBallResult.equals(THREE_STRIKE)) {
+			int userInputNumber = numberException();
+			strikeAndBallResult = BallCount.checkStrikeAndBall(randomValue, userInputNumber);
+			
+			System.out.println(strikeAndBallResult);
+		}
+	}
+	
+	private static int numberException() {
+		String errCode = "ERROR";
+		int inputNumber = 0;
+		
+		while(errCode.equals("ERROR")) {
+			inputNumber = UserInputValue.getUserInput(InputView.getInput());
+			errCode = UserInputValue.getErrCode();
+		}
+		
+		System.out.println("테스트 로그 사용자 입력값 === " +inputNumber);
+		return inputNumber;
+	}
+}
