684ec0249ec775772198631ec446aa5d6e023867
kkwan0226 <kkwan0226@gmail.com>
2021-10-06 15:28:55 +0900

    feature(BaseballException.java, OutputView.java): Exception 출력 기능 구현
    Check List:
    1. BaseballException 구현
    2. OutputView(Error) 구현

---
+++ src/main/java/baseball/exception/BaseballException.java
@@ -0,0 +1,8 @@
+package baseball.exception;
+
+public class BaseballException extends RuntimeException {
+    
+    public BaseballException(String message) {
+        super(message);
+    }
+}
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import baseball.code.InGameText;
+import baseball.exception.BaseballException;
 
 public class OutputView {
     public static void ballCount(String ballCount) {
@@ -9,5 +10,9 @@ public class OutputView {
 
     public static void endGame() {
         System.out.println(InGameText.ENDGAME.getInGameText());
+    }
+
+    public static void error(BaseballException baseballException) {
+        System.out.println(baseballException.getMessage());
     }
 }
