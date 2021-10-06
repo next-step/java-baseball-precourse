c0697d29c57c4670f7c7a93db3996f6c75d57864
jongwon_lee <jongwon_lee@tmax.co.kr>
2021-10-06 13:48:47 +0900

    test: player input에 대한 테스트 코드 구현

--- src/test/java/baseball/model/PlayerBaseballNumberTest.java
+++ src/test/java/baseball/model/PlayerBaseballNumberTest.java
@@ -1,2 +1,37 @@
-package baseball.model;public class PlayerBaseballNumberTest {
+package baseball.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import baseball.exception.DuplicatedNumberException;
+import baseball.exception.ErrorMessage;
+import baseball.exception.InvalidFiguresNumberException;
+import baseball.exception.InvalidInputTypeException;
+
+public class PlayerBaseballNumberTest {
+
+	@DisplayName("PlayBaseballNumber 객체 잘 생성되는지 Test")
+	@Test
+	void createPlayerNumberTest() {
+		PlayerBaseballNumber.createPlayerBaseballNumber("275");
+	}
+
+	@DisplayName("PlayBaseballNumber 자리수 테스트 ")
+	@Test
+	void validateDuplicatedPlayerNumber() {
+		assertThatThrownBy(() -> PlayerBaseballNumber.createPlayerBaseballNumber("11"))
+			.isInstanceOf(InvalidFiguresNumberException.class)
+			.hasMessageContaining(ErrorMessage.INVALID_FIGURES_NUMBER);
+	}
+
+	@DisplayName("PlayBaseballNumber 문자 포함 숫자 Test")
+	@Test
+	void validatePlayerNumberWithChar() {
+		assertThatThrownBy(() -> PlayerBaseballNumber.createPlayerBaseballNumber("ab1"))
+			.isInstanceOf(InvalidInputTypeException.class)
+			.hasMessageContaining(ErrorMessage.INVALID_INPUT_TYPE);
+
+	}
 }
