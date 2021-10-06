ea512c9a9a24b541f2e7dd81c0fd78f35860e8ff
herbtea <herbtea30@gmail.com>
2021-10-06 15:45:30 +0900

    feat : 야구게임 기능 조합하여 개발

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,18 +1,72 @@
 package baseball;
 
+import nextstep.utils.Console;
+
 /**
  * Name : BaseBallGame <br/>
  * Description : 야구 게임 객체
  */
 public class BaseBallGame {
     public static final int DEFAULT_LENGTH = 3;
-    private int maxLength;
 
-    private BaseBallGame() {
-        this(DEFAULT_LENGTH);
-    }
+    private RandomNumberGenerator randomNumberGenerator;
+    private InputNumberValidator inputNumberValidator;
+    private BaseBallGameNumberComparator baseBallGameNumberComparator;
+    private BaseBallResultPrinter baseBallResultPrinter;
 
     public BaseBallGame(int maxLength) {
-        this.maxLength = maxLength;
+        randomNumberGenerator = new RandomNumberGenerator(maxLength);
+        inputNumberValidator = new InputNumberValidator();
+        baseBallGameNumberComparator = new BaseBallGameNumberComparator(maxLength);
+        baseBallResultPrinter = new BaseBallResultPrinter();
+    }
+
+    public static void main(String[] args) {
+        BaseBallGame baseBallGame = new BaseBallGame(DEFAULT_LENGTH);
+        baseBallGame.run();
+    }
+
+    private void run() {
+        String randomGenerated = randomNumberGenerator.generateNumbers();
+        int[] result;
+
+        do {
+            String userInputNumber = inputNumber();
+            result = compareNumber(randomGenerated, userInputNumber);
+        }while (result[0] != 3);
+        isGameEnd(result);
+    }
+
+    private void isGameEnd(int[] result) {
+        if(result[0] == 3) {
+            setGameFlag();
+        }
+    }
+
+    private void setGameFlag() {
+        System.out.println("게임 끝");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if(Console.readLine().equals("1")) {
+            run();
+        }
+    }
+
+    private int[] compareNumber(String randomGenerated, String userInputNumber) {
+        int[] result;
+        result = baseBallGameNumberComparator.getBaseBallGameResult(randomGenerated, userInputNumber);
+        baseBallResultPrinter.print(result);
+        return result;
+    }
+
+    private String inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = Console.readLine();
+        return validateInputNumber(inputNumber);
+    }
+
+    private String validateInputNumber(String inputNumber) {
+        if(inputNumberValidator.validate(inputNumber))
+            return inputNumber;
+        return inputNumber();
     }
 }
