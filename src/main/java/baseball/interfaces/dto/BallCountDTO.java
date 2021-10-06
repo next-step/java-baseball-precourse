33e9ce25043a507354e31dd7b9669451e8768ccd
jin338s <jin3378s@gmail.com>
2021-10-06 23:00:19 +0900

    Feat : 게임을 진행하기 위한 서비스 레이어 생성
    - 인풋을 받으면 게임 도메인으로 부터 결과를 확인하여 볼카운트를 전달한다.
    - 인터페이스로 도메인 객체를 노출하지 않기 위하여 dto를 추가 한다.

---
+++ src/main/java/baseball/interfaces/dto/BallCountDTO.java
@@ -0,0 +1,44 @@
+package baseball.interfaces.dto;
+
+import baseball.domain.BallCount;
+
+import java.util.ArrayList;
+
+public class BallCountDTO {
+
+    public int strikeCount;
+    public int ballCount;
+    public boolean isStrikeOut;
+
+    public static BallCountDTO fromModel(BallCount ballCount) {
+        return new BallCountDTO(ballCount.getStrikeCount(), ballCount.getBallCount(), ballCount.getIsStrikeOut());
+    }
+
+    public BallCountDTO(int strikeCount, int ballCount, boolean isStrikeOut) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+        this.isStrikeOut = isStrikeOut;
+    }
+
+    public String toString() {
+        ArrayList<String> result = new ArrayList<>();
+
+        if (this.strikeCount > 0) {
+            result.add(strikeCount + "스트라이크");
+        }
+
+        if (this.ballCount > 0) {
+            result.add(ballCount + "볼");
+        }
+
+
+        if (this.strikeCount == 0 && this.ballCount == 0) {
+            return "낫싱";
+        }
+
+        return String.join(" ", result);
+
+    }
+
+
+}
