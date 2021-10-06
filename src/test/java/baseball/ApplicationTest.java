657dc3e813e676bae6d6ba47733a710d9d4850f2
venaCode <venaCode93@gmail.com>
2021-10-06 23:01:33 +0900

    test: add test case
    -. test on user input

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -38,6 +38,39 @@ public class ApplicationTest extends NSTest {
         }
     }
 
+    @Test
+    void 입력값이_자릿수가_3자리가_아님() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("12356");
+            verify("[ERROR]입력한 자릿수가 3자리가 아닙니다.");
+        }
+    }
+
+    @Test
+    void 입력값이_중복값() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("775", "595");
+            verify("[ERROR]중복된 숫자가 입력되었습니다.");
+        }
+    }
+
+    @Test
+    void 입력값이_1에서_9까지의_숫자가_아님() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("ABC", "H24", "!@3", "012");
+            verify("[ERROR]0 또는 숫자가 아닌 문자가 포함되었습니다.");
+        }
+    }
+
     @AfterEach
     void tearDown() {
         outputStandard();
