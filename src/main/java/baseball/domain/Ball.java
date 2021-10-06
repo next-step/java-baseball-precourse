e7666a7a3c078455c1ed58a32c9817a9a48e76cc
김혜린 <sjgpfls@gmail.com>
2021-10-06 18:41:59 +0900

    refactor: Ball 메소드명 변경

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -2,26 +2,26 @@ package baseball.domain;
 
 import java.util.Objects;
 
-public class BallNumber {
+public class Ball {
 
 	private static final int MIN_NUMBER = 1;
 	private static final int MAX_NUMBER = 9;
 
 	private int number;
 
-	private BallNumber(int number) {
+	private Ball(int number) {
 		this.number = number;
 	}
 
-	private static boolean validNo(int number){
+	private static boolean isValidNo(int number){
 		return number >= MIN_NUMBER && number <= MAX_NUMBER;
 	}
 
-	public static BallNumber of(int number) {
-		if(!validNo(number)) {
+	public static Ball of(int number) {
+		if(!isValidNo(number)) {
 			throw new IllegalArgumentException("1~9까지의 숫자만 입력 가능합니다.");
 		}
-		return new BallNumber(number);
+		return new Ball(number);
 	}
 
 	@Override
@@ -30,7 +30,7 @@ public class BallNumber {
 			return true;
 		if (o == null || getClass() != o.getClass())
 			return false;
-		BallNumber that = (BallNumber)o;
+		Ball that = (Ball)o;
 		return number == that.number;
 	}
 
--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -5,12 +5,12 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BallNumberTest {
+public class BallTest {
 
 	@DisplayName("숫자 값이 1 ~ 9인지 검증")
 	@Test
-	void validNo_NOT_1_9() {
-		assertThatThrownBy(() -> BallNumber.of(10))
+	void isValidNo() {
+		assertThatThrownBy(() -> Ball.of(10))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("1~9까지의 숫자만 입력 가능합니다.");
 	}
