c844bfc9fc87d3e4136bbe3fe4c20c1f7c906bbc
kim-daeyong <kdy2353@gmail.com>
2022-04-13 00:03:39 +0900

    test : validInputTest 추가

--- src/test/java/baseball/game/PlayTest.java
+++ src/test/java/baseball/game/PlayTest.java
@@ -1,9 +1,11 @@
 package baseball.game;
 
 import baseball.model.Board;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayTest {
 
@@ -16,5 +18,14 @@ public class PlayTest {
         play.initGoal(board);
 
         assertThat(board.getGoal()[0]).isGreaterThan(0);
+    }
+
+    @Test
+    void validInputTest() {
+        Play play = new Play();
+
+        assertThatThrownBy(() -> {
+            play.validInput("1234");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("입력값은 3자리 숫자여야 합니다.");
     }
 }
