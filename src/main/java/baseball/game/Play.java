8f33e6c7bed55b89843bb93d81f30b7b4e86ce62
kim-daeyong <kdy2353@gmail.com>
2022-04-12 23:37:43 +0900

    feat : 난수생성 추가

--- src/main/java/baseball/game/Play.java
+++ src/main/java/baseball/game/Play.java
@@ -1,2 +1,16 @@
-package baseball.game;public class Play {
+package baseball.game;
+
+import baseball.model.Board;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Play {
+    public Board initGoal(Board board) {
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Randoms.pickNumberInRange(1, 9);;
+        }
+        board.setGoal(arr);
+
+        return board;
+    }
 }
