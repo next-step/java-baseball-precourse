e1da4f84d0ab2f7e677828f6de2626a342a0fcd0
kim-daeyong <kdy2353@gmail.com>
2022-04-12 23:38:12 +0900

    test : 난수 생성 테스트 추가

--- src/test/java/baseball/game/PlayTest.java
+++ src/test/java/baseball/game/PlayTest.java
@@ -1,2 +1,20 @@
-package baseball.game;public class PlayTest {
+package baseball.game;
+
+import baseball.model.Board;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayTest {
+
+    @Test
+    void initGoalTest() {
+        Board board = new Board();
+
+        Play play = new Play();
+
+        play.initGoal(board);
+
+        assertThat(board.getGoal()[0]).isGreaterThan(0);
+    }
 }
