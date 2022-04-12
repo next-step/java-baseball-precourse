42afe73b09437a24d428bfe9839038ecd434b575
kim-daeyong <kdy2353@gmail.com>
2022-04-12 23:16:45 +0900

    feat : 모델 추가

---
+++ src/main/java/baseball/model/Board.java
@@ -0,0 +1,22 @@
+package baseball.model;
+
+public class Board {
+    private int[] goal;
+    private int[] score;
+
+    public int[] getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int[] goal) {
+        this.goal = goal;
+    }
+
+    public int[] getScore() {
+        return score;
+    }
+
+    public void setScore(int[] score) {
+        this.score = score;
+    }
+}
