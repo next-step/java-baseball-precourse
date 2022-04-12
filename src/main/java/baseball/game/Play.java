48ad05066450e833e4c54d2522201e5665663260
kim-daeyong <kdy2353@gmail.com>
2022-04-13 00:02:52 +0900

    feat : console input 추가

--- src/main/java/baseball/game/Play.java
+++ src/main/java/baseball/game/Play.java
@@ -4,13 +4,18 @@ import baseball.model.Board;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Play {
-    public Board initGoal(Board board) {
+
+    public void validInput(String input) {
+        if (input.length() > 3) {
+            throw new IllegalArgumentException("입력값은 3자리 숫자여야 합니다.");
+        }
+    }
+
+    public void initGoal(Board board) {
         int[] arr = new int[3];
         for (int i = 0; i < 3; i++) {
             arr[i] = Randoms.pickNumberInRange(1, 9);;
         }
         board.setGoal(arr);
-
-        return board;
     }
 }
