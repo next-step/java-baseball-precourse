add8904cb500ad5b8ebe591ecf949c16637d1b3e
kim-daeyong <kdy2353@gmail.com>
2022-04-13 00:43:28 +0900

    fix : add board construct

--- src/main/java/baseball/model/Board.java
+++ src/main/java/baseball/model/Board.java
@@ -19,4 +19,9 @@ public class Board {
     public void setScore(int[] score) {
         this.score = score;
     }
+
+    public Board() {
+        this.goal = new int[3];
+        this.score = new int[2];
+    }
 }
