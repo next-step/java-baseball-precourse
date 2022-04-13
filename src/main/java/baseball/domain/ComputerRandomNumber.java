da702adf37bda8950ac84a5adf596c9e6734f62a
IMwoo_94 <leemeawoo@daum.net>
2022-04-13 14:11:29 +0900

    feat : random num array add
    컴퓨터 클래스에 arraylist를 사용하여 랜덤 random num array 생성
    camp.nextstep.edu.missionutils.Randoms의pickNumberInRange() 활용하여 랜덤숫자 3개를 생성하여 담는다.
    
    이때 중복된 숫자를 제외 하기 위해서 duplication 메소드를 만들어서 체크

---
+++ src/main/java/baseball/domain/ComputerRandomNumber.java
@@ -0,0 +1,42 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+public class ComputerRandomNumber {
+        // random num array 생성
+        // camp.nextstep.edu.missionutils.Randoms
+        // pickNumberInRange() 활용하여 랜덤숫자 3개를 생성하여 담는다.
+        // Computer class 생성
+
+        // 1 ~ 9의 숫자의 야구게임
+        private static final int START = 1;
+        private static final int END = 9;
+        // 랜덤숫자 3개 제한
+        private static final int COUNT = 3;
+
+        // 생성된 랜덤 숫자
+        private static int num;
+
+        // 랜덤 숫자 담을 리스트
+        ArrayList<Integer> randomNum = new ArrayList<Integer>();
+
+        public void RandomNum(){
+                // COUNT 갯수 만큼 랜덤 숫자 생성.
+                while(randomNum.size() < COUNT){
+                    num = Randoms.pickNumberInRange(START, END);
+                    RandomDuplicationCheck();
+                }
+        }
+
+        // 숫자 중복 체크
+        private void RandomDuplicationCheck (){
+                if(!randomNum.contains(num)){
+                    randomNum.add(num);
+                }
+        }
+
+        public void getRandomNum() {
+                System.out.println(randomNum);
+        }
+}
