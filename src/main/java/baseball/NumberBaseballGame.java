8fee6eb4389b011d7c3bd6d160fc3af66ddf7a6b
ssk <seungkyu0215@gmail.com>
2022-04-14 23:10:48 +0900

    Feat : 게임 셋업 로직 추가
    NumberBaseballGame 할당시 상대 플레이어를 생성하고 랜덤 숫자를 설정함

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -1,15 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberBaseballGame {
 
     private boolean play;
     private final int maxNumberLength;
+    private Player computer;
 
     public NumberBaseballGame(int maxNumberLength){
         this.maxNumberLength = maxNumberLength;
+        this.play = true;
+        setUpPlayer();
     }
 
-    private void setUp(){
+    private void setUpPlayer(){
+        this.computer = new Player(this.randomNumbers());
+    }
+
+    private String randomNumbers(){
+        String numbers = new String();
+        while (numbers.length() < maxNumberLength){
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+
+            if(numbers.indexOf(randomNum) < 0){
+                numbers += randomNum;
+            }
+        }
+        return numbers;
     }
 
     public boolean isPlay() { return this.play; }
