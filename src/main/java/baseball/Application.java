a7318a8db133f20d99fc1345903e53ac25622174
Jinmo Im <jinmo.im@gmail.com>
2022-04-15 00:23:34 +0900

    feat: 재시작 선택시 잘못된 입력에 대한 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,11 +9,14 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         String line = "1";
-        while (!Objects.equals(line, "2")) {
+        while (Objects.equals(line, "1") && !Objects.equals(line, "2")) {
             Game game = new Game();
             game.play();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             line = readLine();
+        }
+        if (!Objects.equals(line, "2")) {
+            throw new IllegalArgumentException("your input is wrong");
         }
     }
 }
