0e9255a7ef832fa3f798903fb2e3d894cd855fac
Jinmo Im <jinmo.im@gmail.com>
2022-04-14 23:40:29 +0900

    feat: 게임 종료 후 재시작할지 종료할지 선택할 수 있음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,18 @@ package baseball;
 
 import baseball.game.Game;
 
+import java.util.Objects;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        Game game = new Game();
-        game.play();
+        String line = "1";
+        while (!Objects.equals(line, "2")) {
+            Game game = new Game();
+            game.play();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            line = readLine();
+        }
     }
 }
