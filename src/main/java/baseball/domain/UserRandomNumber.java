ea32c26c33b9b0a6802fb1dfa6649de8c6597981
IMwoo_94 <leemeawoo@daum.net>
2022-04-14 15:43:23 +0900

    feat : 사용자 입력 숫자 벨리데이션 체크
    사용자 입력 숫자에 대해서 길이가 3이 아닌 경우 exception 발생

--- src/main/java/baseball/domain/UserRandomNumber.java
+++ src/main/java/baseball/domain/UserRandomNumber.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import baseball.exception.SizeMisseException;
 
 public class UserRandomNumber {
 
@@ -11,28 +12,36 @@ public class UserRandomNumber {
         }
 
         // 입력 받은 내용 arraylist에 담아둔다.
-        private ArrayList<Integer> BaseBallNumberFormat(String userNumbers) {
+        private ArrayList<Integer> BaseBallNumberFormat(String requireBaseBallNumber) {
+            //사이즈 검증 후 arraylist에 담는다.
+            validateUserNumbers(requireBaseBallNumber);
             ArrayList<Integer> userNumbersList = new ArrayList<>();
-            for (int i = 0; i < userNumbers.length(); i++) {
-                userNumbersList.add((int)userNumbers.charAt(i));
+            for (int i = 0; i < requireBaseBallNumber.length(); i++) {
+                userNumbersList.add((int)requireBaseBallNumber.charAt(i));
             }
             return userNumbersList;
         }
 
-        public int ballCount(ArrayList<Integer> randomNumbers) {
+        public int ballCount(ArrayList<Integer> computerRandomNumbers) {
 
         }
 
-        private boolean isBall(int targetIndex, ArrayList<Integer> randomNumbers) {
+        private boolean isBall(int targetIndex, ArrayList<Integer> computerRandomNumbers) {
 
         }
 
-        public int strikeCount(ArrayList<Integer> randomNumbers) {
+        public int strikeCount(ArrayList<Integer> computerRandomNumbers) {
 
         }
 
-        private boolean isStrike(int targetIndex, ArrayList<Integer> randomNumbers) {
+        private boolean isStrike(int targetIndex, ArrayList<Integer> computerRandomNumbers) {
 
+        }
+
+        private void validateUserNumbers(String requireBaseBallNumber) {
+            if (requireBaseBallNumber.length() != 3) {
+                throw new SizeMisseException();
+            }
         }
 
 }
---
+++ src/main/java/baseball/exception/SizeMisseException.java
@@ -0,0 +1,9 @@
+package baseball.exception;
+
+public class SizeMisseException extends IllegalArgumentException{
+        private static final String SIZE_MISS = "3개의 숫자를 입력해야 합니다.";
+
+        public SizeMisseException() {
+            super(SIZE_MISS);
+        }
+}
