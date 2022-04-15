4b2115080975e0de78908c685933a6a0df7e724a
Jinmo Im <jinmo.im@gmail.com>
2022-04-15 20:52:56 +0900

    test: 오름차순, 내림차순에 대한 Unit test를 GameUtilTest에서 구현하도록 함
    HashSet을 사용하면서 무작위 3개 숫자가 오름차순으로만 반환되는 버그가 있었기에,
    앞으로 재발되지 말라는 의미에서 추가한 테스트임

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -8,16 +8,9 @@ import java.util.Arrays;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
-    @Test
-    @DisplayName("checkAnswer() 컴퓨터의 숫자가 오름차순일 때, 다른 자리의 숫자를 넘기면, 볼")
-    void checkAnswer_WithComputersNumbersAscending_PassNumberNotInSamePosition_Ball() {
-        GameState state = new GameState(Arrays.asList(1, 2, 3));
-        Judgment result = Game.checkAnswer(state,3, 0);
-        assertThat(result).isEqualTo(Judgment.BALL);
-    }
 
     @Test
-    @DisplayName("checkAnswer() 컴퓨터의 숫자가 내림차순일 때, 다른 자리의 숫자를 넘기면, 볼")
+    @DisplayName("checkAnswer() 다른 자리의 숫자를 넘기면, 볼")
     void checkAnswer_WithComputersNumbersDescending_PassNumberNotInSamePosition_Ball() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         Judgment result = Game.checkAnswer(state,1, 0);
@@ -25,15 +18,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("checkAnswer() 컴퓨터의 숫자가 오름차순일 때, 같은 자리의 숫자를 넘기면, 스트라이크")
-    void checkAnswer_WithComputersNumbersAscending_PassNumberInSamePosition_Strike() {
-        GameState state = new GameState(Arrays.asList(1, 2, 3));
-        Judgment result = Game.checkAnswer(state,1, 0);
-        assertThat(result).isEqualTo(Judgment.STRIKE);
-    }
-
-    @Test
-    @DisplayName("checkAnswer() 컴퓨터의 숫자가 내림차순일 때, 같은 자리의 숫자를 넘기면, 스트라이크")
+    @DisplayName("checkAnswer() 같은 자리의 숫자를 넘기면, 스트라이크")
     void checkAnswer_WithComputersNumbersDescending_PassNumberInSamePosition_Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         Judgment result = Game.checkAnswer(state,3, 0);
@@ -41,17 +26,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 오름차순일 때, 다른 자리의 숫자 3개일 때, 3볼 0스트라이크")
-    void judge_WithComputersNumbersAscending_Pass3NumbersNotInSamePosition_3Balls() {
-        GameState state = new GameState(Arrays.asList(1, 2, 3));
-        NumberToIndex answer = new NumberToIndex("231");
-        RoundScore result = Game.judge(state, answer);
-        assertThat(result.getNumBalls()).isEqualTo(3);
-        assertThat(result.getNumStrikes()).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 내림차순일 때, 다른 자리의 숫자 3개일 때, 3볼 0스트라이크")
+    @DisplayName("judge() 다른 자리의 숫자 3개일 때, 3볼 0스트라이크")
     void judge_WithComputersNumbersDescending_Pass3NumbersNotInSamePosition_3Balls() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("213");
@@ -61,17 +36,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 오름차순일 때, 다른 자리의 숫자 2개와 같은 자리의 숫자 1개일 때, 2볼 1스트라이크")
-    void judge_WithComputersNumbersAscending_Pass2NumbersNotInSamePosition1NumberInSamePosition_2Balls1Strike() {
-        GameState state = new GameState(Arrays.asList(1, 2, 3));
-        NumberToIndex answer = new NumberToIndex("213");
-        RoundScore result = Game.judge(state, answer);
-        assertThat(result.getNumBalls()).isEqualTo(2);
-        assertThat(result.getNumStrikes()).isEqualTo(1);
-    }
-
-    @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 내림차순일 때, 다른 자리의 숫자 2개와 같은 자리의 숫자 1개일 때, 2볼 1스트라이크")
+    @DisplayName("judge() 다른 자리의 숫자 2개와 같은 자리의 숫자 1개일 때, 2볼 1스트라이크")
     void judge_WithComputersNumbersDescending_Pass2NumbersNotInSamePosition1NumberInSamePosition_2Balls1Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("231");
@@ -81,17 +46,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 오름차순일 때, 같은 자리의 숫자 3개일 때, 0볼 3스트라이크")
-    void judge_WithComputersNumbersAscending_Pass3NumbersInSamePosition_3Strikes() {
-        GameState state = new GameState(Arrays.asList(1, 2, 3));
-        NumberToIndex answer = new NumberToIndex("123");
-        RoundScore result = Game.judge(state, answer);
-        assertThat(result.getNumBalls()).isEqualTo(0);
-        assertThat(result.getNumStrikes()).isEqualTo(3);
-    }
-
-    @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 내림차순일 때, 같은 자리의 숫자 3개일 때, 0볼 3스트라이크")
+    @DisplayName("judge() 같은 자리의 숫자 3개일 때, 0볼 3스트라이크")
     void judge_WithComputersNumbersDescending_Pass3NumbersInSamePosition_3Strikes() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("321");
@@ -101,7 +56,7 @@ public class GameTest {
     }
 
     @Test
-    @DisplayName("judge() 컴퓨터의 숫자가 내림차순일 때, 같은 자리의 숫자 3개일 때, 0볼 0스트라이크")
+    @DisplayName("judge() 같은 자리의 숫자 3개일 때, 0볼 0스트라이크")
     void judge_Pass3NumbersNotContained_0Ball0Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("456");
---
+++ src/test/java/baseball/game/GameUtilTest.java
@@ -0,0 +1,37 @@
+package baseball.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameUtilTest {
+    @Test
+    @DisplayName("randomNumbers() 무작위 숫자가 오름차순일 때")
+    void randomNumbers_Ascending() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    List<Integer> result = GameUtil.randomNumbers(9, 1, 9);
+                    assertThat(result).hasSameElementsAs(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+                },
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        );
+    }
+
+    @Test
+    @DisplayName("randomNumbers() 무작위 숫자가 내림차순일 때")
+    void randomNumbers_Descending() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    List<Integer> result = GameUtil.randomNumbers(9, 1, 9);
+                    assertThat(result).hasSameElementsAs(Arrays.asList(9, 8, 7, 6, 5, 4, 3, 2, 1));
+                },
+                9, 8, 7, 6, 5, 4, 3, 2, 1
+        );
+    }
+
+}
