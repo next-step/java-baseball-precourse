a5667da8b59747d27753471b91c2ef03c11bf4ff
Jinmo Im <jinmo.im@gmail.com>
2022-04-15 22:02:23 +0900

    test: 워딩 변경 number -> digit

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -11,7 +11,7 @@ public class GameTest {
 
     @Test
     @DisplayName("checkAnswer() 다른 자리의 숫자를 넘기면, 볼")
-    void checkAnswer_WithComputersNumbersDescending_PassNumberNotInSamePosition_Ball() {
+    void checkAnswer_WithComputersDigitsDescending_PassDigitNotInSamePosition_Ball() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         Judgment result = Game.checkAnswer(state,1, 0);
         assertThat(result).isEqualTo(Judgment.BALL);
@@ -19,7 +19,7 @@ public class GameTest {
 
     @Test
     @DisplayName("checkAnswer() 같은 자리의 숫자를 넘기면, 스트라이크")
-    void checkAnswer_WithComputersNumbersDescending_PassNumberInSamePosition_Strike() {
+    void checkAnswer_WithComputersDigitsDescending_PassDigitInSamePosition_Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         Judgment result = Game.checkAnswer(state,3, 0);
         assertThat(result).isEqualTo(Judgment.STRIKE);
@@ -27,7 +27,7 @@ public class GameTest {
 
     @Test
     @DisplayName("judge() 다른 자리의 숫자 3개일 때, 3볼 0스트라이크")
-    void judge_WithComputersNumbersDescending_Pass3NumbersNotInSamePosition_3Balls() {
+    void judge_WithComputersDigitsDescending_Pass3DigitsNotInSamePosition_3Balls() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("213");
         RoundScore result = Game.judge(state, answer);
@@ -37,7 +37,7 @@ public class GameTest {
 
     @Test
     @DisplayName("judge() 다른 자리의 숫자 2개와 같은 자리의 숫자 1개일 때, 2볼 1스트라이크")
-    void judge_WithComputersNumbersDescending_Pass2NumbersNotInSamePosition1NumberInSamePosition_2Balls1Strike() {
+    void judge_WithComputersDigitsDescending_Pass2DigitsNotInSamePosition1DigitInSamePosition_2Balls1Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("231");
         RoundScore result = Game.judge(state, answer);
@@ -47,7 +47,7 @@ public class GameTest {
 
     @Test
     @DisplayName("judge() 같은 자리의 숫자 3개일 때, 0볼 3스트라이크")
-    void judge_WithComputersNumbersDescending_Pass3NumbersInSamePosition_3Strikes() {
+    void judge_WithComputersDigitsDescending_Pass3DigitsInSamePosition_3Strikes() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("321");
         RoundScore result = Game.judge(state, answer);
@@ -57,7 +57,7 @@ public class GameTest {
 
     @Test
     @DisplayName("judge() 같은 자리의 숫자 3개일 때, 0볼 0스트라이크")
-    void judge_Pass3NumbersNotContained_0Ball0Strike() {
+    void judge_Pass3DigitsNotContained_0Ball0Strike() {
         GameState state = new GameState(Arrays.asList(3, 2, 1));
         NumberToIndex answer = new NumberToIndex("456");
         RoundScore result = Game.judge(state, answer);
