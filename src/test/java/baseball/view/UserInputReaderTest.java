58085170e7764c457a593fc173eea34baeba59ef
uyoung <uyoung5410@gmail.com>
2022-04-17 00:49:38 +0900

    test: 변경, 입력값 검증을 mock을 이용하도록 변경

--- src/test/java/baseball/view/UserInputReaderTest.java
+++ src/test/java/baseball/view/UserInputReaderTest.java
@@ -1,15 +1,17 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
 
 class UserInputReaderTest {
     private final UserInputReader reader = new UserInputReader();
@@ -18,26 +20,32 @@ class UserInputReaderTest {
     @ParameterizedTest
     @ValueSource(strings = {"12a", "a12", "dd0", "#a1"})
     void notOnlyNumber(String input) {
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        assertThatThrownBy(reader::readBallNumber)
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            mock.when(Console::readLine).thenReturn(input);
+            assertThatThrownBy(reader::readBallNumber)
                 .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
     @DisplayName("아무것도 입력하지 않은 경우")
     @Test
     void empty() {
-        System.setIn(new ByteArrayInputStream("\n".getBytes()));
-        assertThatThrownBy(reader::readBallNumber)
-                .isInstanceOf(IllegalArgumentException.class);
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            mock.when(Console::readLine).thenReturn("%n");
+            assertThatThrownBy(reader::readBallNumber)
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
     @DisplayName("숫자만 입력한 경우")
     @ParameterizedTest
     @ValueSource(strings = {"12", "123", "1234", "12345"})
     void onlyNumber(String input) {
-        int inputLength = input.length();
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-        List<Integer> result = reader.readBallNumber();
-        assertThat(result.size()).isEqualTo(inputLength);
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            int inputLength = input.length();
+            mock.when(Console::readLine).thenReturn(input);
+            List<Integer> result = reader.readBallNumber();
+            assertThat(result.size()).isEqualTo(inputLength);
+        }
     }
 }
