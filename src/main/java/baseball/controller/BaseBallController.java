427090ce4f11de6e0a45f54d866ff082351b6d6e
wxxsoo <wxxsox@gmail.com>
2022-04-17 08:24:11 +0900

    feat: 숫자야구 게임 결과 노출 기능 구현
    - 낫싱, 볼, 스트라이크 판단 및 결과 노출
    - 3스트라이크의 경우 종료 여부 확인 메시지 노출

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,23 +1,72 @@
 package baseball.controller;
 
+import baseball.model.Computer;
+import baseball.model.Player;
 import baseball.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseBallController {
 
     private boolean exit = true;
     private static final String EXIT = "2";
+    private static final String WINNER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
 
     public void play() {
+        Computer computer = new Computer();
+        Player player = new Player();
         while (exit) {
-            InputView.inputNumber();
-            String exitInput = InputView.inputExit();
-            if(isExit(exitInput)) {
-                exit = false;
+            String input = InputView.inputNumber();
+            player.setNumber(convertToList(input));
+            int strike = computer.countStrike(player.getNumber());
+            int ball = computer.countBall(strike, player.getNumber());
+            System.out.println(getResultMessage(strike, ball));
+            if(isDone(strike)) {
+                if(isExit(InputView.inputExit())) {
+                    exit = false;
+                }
+                computer.generateRandomNumber();
             }
         }
     }
 
+    public List<Integer> convertToList(String input) {
+        return Arrays.stream(input.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
     public boolean isExit(String input) {
         return input.equals(EXIT);
+    }
+
+    public boolean isDone(int strike) {
+        if(strike == 3) {
+            System.out.println(WINNER_MESSAGE);
+            return true;
+        }
+        return false;
+    }
+
+    public String getResultMessage(int strike, int ball) {
+        if(strike == 0 && ball == 0) {
+            return NOTHING_MESSAGE;
+        }
+
+        return getBaseBallResultMessage(strike, ball);
+    }
+
+    private String getBaseBallResultMessage(int strike, int ball) {
+        StringBuilder sb = new StringBuilder();
+        if (ball > 0) {
+            sb.append(ball).append(BALL_MESSAGE).append(" ");
+        }
+        if (strike > 0) {
+            sb.append(strike).append(STRIKE_MESSAGE);
+        }
+        return sb.toString();
     }
 }
