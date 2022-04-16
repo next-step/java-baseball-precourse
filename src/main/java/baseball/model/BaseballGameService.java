778d56f7e6bcfb1ec9cbe23ae51e63cacf76626c
ul8415 <ul8415@gmail.com>
2022-04-16 17:49:05 +0900

    feat : 입력된 숫자에 대한 야구게임 결과 처리 및 예외처리 기능 추가
    - 비교는 각 자리수에 해당하는 숫자가 같으면 strike 처리, 같지 않지만 포함된 경우에는 ball 처리
    - 입력 값에 대한 유효성 체크 추가 (throw IllegalArgumentException)
    - 입력 값(String)에 대한 List 변환 추가

--- src/main/java/baseball/model/BaseballGameService.java
+++ src/main/java/baseball/model/BaseballGameService.java
@@ -3,6 +3,7 @@ package baseball.model;
 import baseball.vo.CompareResultVO;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BaseballGameService {
@@ -20,7 +21,50 @@ public class BaseballGameService {
     }
 
     public CompareResultVO compareInputToAnswer(String inputNumber, List<Integer> answerNumberList) {
-        return new CompareResultVO(0, 0);
+        validateInputNumber(inputNumber);
+        List<Integer> inputNumberList = parseStringToIntegerList(inputNumber);
+        int ballCount = 0, strikeCount = 0;
+        for (int i = 0; i < answerNumberList.size(); i++) {
+            if (answerNumberList.get(i).intValue() == inputNumberList.get(i).intValue()) {
+                strikeCount++;
+                continue;
+            }
+            if (answerNumberList.contains(inputNumberList.get(i))) {
+                ballCount++;
+            }
+        }
+        return new CompareResultVO(ballCount, strikeCount);
+    }
+
+    private List<Integer> parseStringToIntegerList(String inputNumber) {
+        int parseInput = Integer.parseInt(inputNumber);
+        List<Integer> inputNumberList = new ArrayList<>();
+        while (parseInput > 0) {
+            int number = parseInput % 10;
+            validateNonZero(number);
+            parseInput = parseInput / 10;
+            inputNumberList.add(number);
+        }
+        Collections.reverse(inputNumberList);
+        return inputNumberList;
+    }
+
+    private void validateNonZero(int number) {
+        if (number == 0) {
+            throw new IllegalArgumentException("0은 사용할 수 없습니다.");
+        }
+    }
+
+    private void validateInputNumber(String inputNumber) {
+        try {
+            int parseInput = Integer.parseInt(inputNumber);
+
+            if (parseInput < 100 || parseInput > 999) {
+                throw new IllegalArgumentException("3자리수가 아닙니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
     }
 
     public List<Integer> getAnswerNumberList() {
