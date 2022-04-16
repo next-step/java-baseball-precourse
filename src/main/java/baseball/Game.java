f7497d69bc1a69085e4564d8c4fde38c84cbf58b
gywjd <j07@hanmail.net>
2022-04-16 16:16:16 +0900

    feat: 결과 출력
    - 스트라이크 : 같은 수, 같은 자리
    - 볼 : 같은 수, 다른 자리
    - 낫싱 : 같은 수가 없음

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class Game {
     public void play() {
         start();
-        progress();
         end();
     }
 
@@ -22,12 +22,14 @@ public class Game {
             answer.add(Randoms.pickNumberInRange(1, 9));
         }
         System.out.println(answer);
+
+        progress(answer);
     }
 
-    private void progress() {
+    private void progress(LinkedHashSet<Integer> answer) {
         System.out.println("게임 진행");
 
-         // 게임 진행
+        // 게임 진행
         System.out.print("숫자를 입력해주세요: ");
         String input = Console.readLine();
         System.out.println(input);
@@ -49,6 +51,38 @@ public class Game {
         if(temp.size() != 3) {
             throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
         }
+
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        LinkedList<Integer> targetNumber = new LinkedList<>(answer);
+        System.out.println(targetNumber);
+
+        LinkedList<Integer> inputNumber = new LinkedList<>();
+        for(int i = 0; i < input.length(); i++) {
+            inputNumber.add(Integer.parseInt(input.substring(i, i + 1)));
+        }
+        System.out.println(inputNumber);
+
+        for(int i = 0; i < input.length(); i++) {
+            if(targetNumber.contains(inputNumber.get(i))) {
+                if(targetNumber.get(i) == inputNumber.get(i)) {
+                    strikeCount++;
+                } else {
+                    ballCount++;
+                }
+            }
+        }
+
+        String hint_strike = (strikeCount == 0) ? "" : (strikeCount + "스트라이크 ");
+        String hint_ball = (ballCount == 0) ? "" : (ballCount + "볼 ");
+        if(strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        } else {
+            System.out.println(hint_ball + hint_strike);
+        }
+
+        progress(answer);
     }
 
     private void end() {
