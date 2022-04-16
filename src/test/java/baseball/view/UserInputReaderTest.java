1b883b8a10d89170545611d1bb8d07def0412eea
uyoung <uyoung5410@gmail.com>
2022-04-17 01:03:40 +0900

    test: 추가, 재시작 플래그 입력 테스트

--- src/test/java/baseball/view/UserInputReaderTest.java
+++ src/test/java/baseball/view/UserInputReaderTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.mockStatic;
 class UserInputReaderTest {
     private final UserInputReader reader = new UserInputReader();
 
-    @DisplayName("숫자와 문자를 섞어서 입력한 경우")
+    @DisplayName("정답_숫자와 문자를 섞어서 입력한 경우")
     @ParameterizedTest
     @ValueSource(strings = {"12a", "a12", "dd0", "#a1"})
     void notOnlyNumber(String input) {
@@ -27,7 +27,7 @@ class UserInputReaderTest {
         }
     }
 
-    @DisplayName("아무것도 입력하지 않은 경우")
+    @DisplayName("정답_아무것도 입력하지 않은 경우")
     @Test
     void empty() {
         try (MockedStatic<Console> mock = mockStatic(Console.class)) {
@@ -37,7 +37,7 @@ class UserInputReaderTest {
         }
     }
 
-    @DisplayName("숫자만 입력한 경우")
+    @DisplayName("정답_숫자만 입력한 경우")
     @ParameterizedTest
     @ValueSource(strings = {"12", "123", "1234", "12345"})
     void onlyNumber(String input) {
@@ -46,6 +46,35 @@ class UserInputReaderTest {
             mock.when(Console::readLine).thenReturn(input);
             List<Integer> result = reader.readBallNumber();
             assertThat(result.size()).isEqualTo(inputLength);
+        }
+    }
+
+    @DisplayName("재시작_1입력 (재시작)")
+    @Test
+    void restart() {
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            mock.when(Console::readLine).thenReturn("1");
+            assertThat(reader.readReGameFlag()).isTrue();
+        }
+    }
+
+    @DisplayName("재시작_2입력 (종료)")
+    @Test
+    void stop() {
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            mock.when(Console::readLine).thenReturn("2");
+            assertThat(reader.readReGameFlag()).isFalse();
+        }
+    }
+
+    @DisplayName("재시작_허용되지않은 값")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "123", " ", ""})
+    void invalid(String input) {
+        try (MockedStatic<Console> mock = mockStatic(Console.class)) {
+            mock.when(Console::readLine).thenReturn(input);
+            assertThatThrownBy(reader::readReGameFlag)
+                .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
