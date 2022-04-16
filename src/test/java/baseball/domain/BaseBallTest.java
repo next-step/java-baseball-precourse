7f7150b80f3d28c1c139ba1857239f44ed958b4f
uyoung <uyoung5410@gmail.com>
2022-04-17 01:16:33 +0900

    test: 추가, 컴퓨터 볼 자동생성 테스트 랜덤으로 생성이 되는지와 랜덤값 생성 중 중복숫자가 나와도 잘 되는지 확인

--- src/test/java/baseball/domain/BaseBallTest.java
+++ src/test/java/baseball/domain/BaseBallTest.java
@@ -1,15 +1,21 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
 
 class BaseBallTest {
     @DisplayName("볼 개수를 적게 전달")
@@ -118,5 +124,27 @@ class BaseBallTest {
         assertThat(compareResult.getStrike()).isEqualTo(1);
         assertThat(compareResult.getBall()).isEqualTo(2);
         assertThat(compareResult.isNothing()).isFalse();
+    }
+
+    @DisplayName("컴퓨터 볼 생성")
+    @Test
+    void generateComputerBall() {
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(1, 5, 6);
+            BaseBall baseBall = BaseBall.generateComputerBall();
+            assertThat(baseBall.toString()).isEqualTo("156");
+        }
+    }
+
+    @DisplayName("컴퓨터 볼 생성 (랜덤값 생성 중 중복이 등장한 경우)")
+    @Test
+    void generateComputerBall_random_dup() {
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 5, 1, 1, 6);
+            BaseBall baseBall = BaseBall.generateComputerBall();
+            assertThat(baseBall.toString()).isEqualTo("156");
+        }
     }
 }
