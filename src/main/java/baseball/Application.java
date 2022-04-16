9b6c77adfdb06e2cf0d1bd9230ee66a6b6de50f3
ssk <seungkyu0215@gmail.com>
2022-04-17 02:32:49 +0900

    Feat : 게임 종료 및 재시작 로직 추가
    Application.java : 숫자야구게임 구현
    countBollAndStrike : 볼,스트라이크 카운트 버그 수정
    printResultMessage, restart, gameOver : 테스트 통과를 위한 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberBaseballGame game = new NumberBaseballGame(3);
+
+        while (game.isPlay()){
+            String value = Console.readLine();
+            if(!game.playing(value)){
+                game.restart();
+            }
+        }
     }
 }
--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -57,7 +57,10 @@ public class NumberBaseballGame {
     public ResultState countBollAndStrike(String defenseNum, String attackNum){
         ResultState result = new ResultState();
         for(int i = 0; i < maxNumberLength; i++){
-            if(defenseNum.charAt(i) == attackNum.charAt(i)) result.setStrike(result.getStrike() + 1);
+            if(defenseNum.charAt(i) == attackNum.charAt(i)) {
+                result.setStrike(result.getStrike() + 1);
+                continue;
+            }
             if(defenseNum.indexOf(attackNum.charAt(i)) > -1) result.setBoll(result.getBoll() + 1);
         }
         return result;
@@ -65,17 +68,24 @@ public class NumberBaseballGame {
 
     private String printResultMessage(ResultState resultCount){
         if(resultCount.isNothing()) return "낫싱";
-        if(resultCount.getStrike() == maxNumberLength) return maxNumberLength + "개의 숫자를 모두 맞히셨습니다. 게임종료";
+        //if(resultCount.getStrike() == maxNumberLength) return maxNumberLength + "개의 숫자를 모두 맞히셨습니다.";
         StringBuilder msg = new StringBuilder();
-        if(resultCount.getBoll() > 0) msg.append(resultCount.getBoll()).append(" 볼 ");
-        if(resultCount.getStrike() > 0) msg.append(resultCount.getStrike()).append(" 스트라이크 ");
+        if(resultCount.getBoll() > 0) msg.append(resultCount.getBoll()).append("볼 ");
+        if(resultCount.getStrike() > 0) msg.append(resultCount.getStrike()).append("스트라이크 ");
         return msg.toString();
     }
 
-    public boolean restart(){
-        return true;
+    public void restart(){
+        //System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        String readLine = Console.readLine();
+        if(readLine.equals("2")){
+            this.gameOver();
+        }
+        this.setUpPlayer();
     }
 
     public void gameOver(){
+        this.play = false;
+        System.out.println("게임 종료");
     }
 }
