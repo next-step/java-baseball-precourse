695780a65b14957bad08c7f46d68efc6a0db3665
Timothy <timothy@jungnanum.com>
2022-04-16 22:28:58 +0900

    test: Randoms_pickNumberInRange_사용_테스트() 생성
    Randoms.pickNumberInRange()와 Set을 사용하여 3자리의 랜덤한 숫자들을 생성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -26,6 +32,29 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 숫자_3개_매칭_테스트() {
+        assertEquals(123, 123);
+    }
+
+    @Test
+    void Randoms_pickNumberInRange_사용_테스트() {
+        // give
+        final int startInclusive = 1;
+        final int endInclusive = 9;
+        final int gameNumbersLen = 3;
+        final Set<Integer> gameNumberSet = new HashSet<>();
+
+        // when
+        do {
+            gameNumberSet.add(Randoms.pickNumberInRange(startInclusive, endInclusive));
+        } while (gameNumberSet.size() < gameNumbersLen);
+
+        // then
+        assertEquals(gameNumbersLen, gameNumberSet.size());
+        System.out.println(gameNumberSet);
     }
 
     @Override
