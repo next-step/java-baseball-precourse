9fd1db276209c93fadce666f46a1f9cdc88ad46c
hugehoo <tbnsok40@gmail.com>
2022-04-16 21:04:41 +0900

    refactoring/ BaseballGame 클래스 리팩토링

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,17 +5,15 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class BaseballGame {
 
     public static void start() {
-        boolean start = true;
 
-        while (start) {
+        boolean isStart = true;
+        while (isStart) {
             Computer computer = new Computer();
-            computer.throwAutoThreeTimes(); // TODO : Computer 클래스의 main() 메서드로 만들 수 있을까?
+            computer.generateThreeNumbers(); // TODO : 고민 : Computer 클래스의 main() 메서드로 만들 수 있을까?
 
-            boolean result = init(computer);
-            start = result;
+            isStart = init(computer);
         }
         terminate();
-
     }
 
     public static boolean init(Computer computer) {
@@ -23,26 +21,26 @@ public class BaseballGame {
         while (!ThreeStrike) {
             Player player = new Player();
             player.drawNumbers();
-            System.out.println("컴퓨터 : " + computer.inputs);
-            CalculateCounts calculate = new CalculateCounts();
-            ThreeStrike = calculate.compareEach(computer.inputs, player.inputs);
+            GameProcessor calculate = new GameProcessor();
+            ThreeStrike = calculate.compareComputerPlayerNumbers(computer.computerNumberList, player.inputs);
         }
         return retry();
     }
 
-    // TODO : Enum 추가
-    // TODO : println() 하드코딩 치환
     private static boolean retry() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+
+        final String RETRY = "1";
+        final String TERMINATE = "2";
+
+        MessagePrinter.printAskRetry();
         String continueFlag = readLine();
 
-        if (continueFlag.equals("1")) {
-            System.out.println("게임을 다시 시작합니다.");
+        if (continueFlag.equals(RETRY)) {
+            MessagePrinter.printRetry();
             return true;
         }
 
-        if (continueFlag.equals("2")) {
-            System.out.println("게임 종료");
+        if (continueFlag.equals(TERMINATE)) {
             return false;
         }
 
@@ -50,7 +48,6 @@ public class BaseballGame {
     }
 
     public static void terminate() {
-        System.out.println("게임을 종료합니다.");
-        return;
+        MessagePrinter.printTerminate();
     }
 }
