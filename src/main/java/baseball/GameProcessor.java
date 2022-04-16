bbfe91d9e1468d20c56f44903c8c04948cd29d90
hugehoo <tbnsok40@gmail.com>
2022-04-16 21:01:56 +0900

    refactoring/ GameProcessor 클래스 리팩토링

--- src/main/java/baseball/GameProcessor.java
+++ src/main/java/baseball/GameProcessor.java
@@ -3,12 +3,13 @@ package baseball;
 import java.util.List;
 import java.util.Objects;
 
-public class CalculateCounts {
+public class GameProcessor {
 
-    public int strikeCount = 0;
-    public int ballCount = 0;
+    public int countStrike = 0;
+    public int countBall = 0;
+    public int STRIKE = 3;
 
-    public boolean compareEach(List<Integer> computerNumberList, List<String> playerNumberList) {
+    public boolean compareComputerPlayerNumbers(List<Integer> computerNumberList, List<String> playerNumberList) {
         int count = 3;
         for (int index = 0; index < count; index++) {
             Integer computerNumber = computerNumberList.get(index);
@@ -17,37 +18,19 @@ public class CalculateCounts {
             calculateStrikes(computerNumber, playerNumber);
             calculateBall(index, computerNumberList, playerNumber);
         }
-        printResult();
-        return strikeCount == 3;
-    }
-
-    // TODO : 프린터 클래스 새로 생성
-    // TODO : 하드코딩 치환 필요한가?
-    private void printResult() {
-        if (ballCount == 0 && strikeCount == 0) {
-            System.out.println("낫싱");
-            return;
-        }
-        if (ballCount == 0) {
-            System.out.printf("%d스트라이크 \n",strikeCount);
-            return;
-        }
-        if (strikeCount == 0) {
-            System.out.printf("%d볼 \n", ballCount);
-            return;
-        }
-        System.out.printf("%d볼 %d스트라이크 \n", ballCount, strikeCount);
+        MessagePrinter.printResult(countStrike, countBall);
+        return countStrike == STRIKE;
     }
 
     public void calculateStrikes(Integer computerNumber, Integer playerNumber) {
         if (Objects.equals(computerNumber, playerNumber)) {
-            strikeCount += 1;
+            countStrike += 1;
         }
     }
 
     public void calculateBall(int i, List<Integer> computerNumber, Integer playerNumber) {
         if (computerNumber.contains(playerNumber) && !Objects.equals(computerNumber.get(i), playerNumber)) {
-            ballCount += 1;
+            countBall += 1;
         }
     }
 
