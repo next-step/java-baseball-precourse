f249ca507d24ad8c3c7fbf811c35759e4cf0961d
juno <juno@iheuron.com>
2022-04-16 17:43:52 +0900

    feat: 야구 게임 비즈니스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import baseball.dto.BaseballDto;
+import baseball.service.GameService;
+import baseball.service.ConsoleService;
+
+import static baseball.constants.BaseballConstants.START_CODE;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean isStartGame = true;
+
+        while(isStartGame) {
+            newGame();
+            isStartGame = isContinueGame();
+        }
+    }
+
+    private static void newGame() {
+        String gameNumber = GameService.newNumber(3);
+
+        while(true) {
+            String inputNumber = ConsoleService.doInputNumber();
+            BaseballDto baseball = GameService.checkStrikeAndBall(gameNumber, inputNumber);
+            ConsoleService.printGameScore(baseball);
+
+            if (baseball.getStrike() == 3) break;
+        }
+    }
+
+    private static boolean isContinueGame() throws IllegalArgumentException{
+        String number = ConsoleService.doInputGameCode();
+
+        if(START_CODE.equals(number)) {
+            return true;
+        }
+
+        return false;
     }
 }
