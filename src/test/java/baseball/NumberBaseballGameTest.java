09b8b74f4f99e9fc72578ae27fb4a3c72e6360b6
ssk <seungkyu0215@gmail.com>
2022-04-17 01:57:44 +0900

    Test : 게임 플레이 테스트 추가

--- src/test/java/baseball/NumberBaseballGameTest.java
+++ src/test/java/baseball/NumberBaseballGameTest.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,10 +15,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberBaseballGameTest {
 
+    private NumberBaseballGame game;
+    @BeforeEach
+    void setup(){
+        game = new NumberBaseballGame(3);
+    }
+
     @Test
     @DisplayName("상대방(컴퓨터) 설정 확인")
     void setup_computer(){
-        NumberBaseballGame game = new NumberBaseballGame(3);
         assertThat(game).isNotNull();
         assertThat(game.isPlay()).isTrue();
     }
@@ -24,8 +32,14 @@ class NumberBaseballGameTest {
     @DisplayName("숫자 검증후 예외처리")
     @ValueSource(strings = {"", "12","1234", "112"})
     void user_validate_number(String testValue){
-        NumberBaseballGame game = new NumberBaseballGame(3);
         assertThatIllegalArgumentException().isThrownBy(() -> game.validateNumber(testValue));
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 검증후 예외처리")
+    @ValueSource(strings = {"123", "456","789"})
+    void play_the_game(String testValue){
+        assertDoesNotThrow(() -> game.playing(testValue));
     }
 
 }
