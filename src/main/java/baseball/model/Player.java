670c87572c5aecb20446118d2b083e0ecba163e3
wxxsoo <wxxsox@gmail.com>
2022-04-17 08:22:15 +0900

    feat: Player 모델 구현

---
+++ src/main/java/baseball/model/Player.java
@@ -0,0 +1,19 @@
+package baseball.model;
+
+import java.util.List;
+
+public class Player {
+    List<Integer> number;
+
+    public Player() {
+
+    }
+
+    public List<Integer> getNumber() {
+        return number;
+    }
+
+    public void setNumber(List<Integer> number) {
+        this.number = number;
+    }
+}
