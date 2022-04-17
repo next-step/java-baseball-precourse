de35cb2569b73ea3ddacb2fdc6d30cbfea3eae83
sungjinIm <dlatjd0707@naver.com>
2022-04-18 02:36:12 +0900

    refactor : test case 미동작 case 수정

--- src/test/java/baseball/core/model/player/HitterTest.java
+++ src/test/java/baseball/core/model/player/HitterTest.java
@@ -1,13 +1,17 @@
 package baseball.core.model.player;
 
+import java.io.ByteArrayInputStream;
 import org.junit.jupiter.api.Test;
 
 public class HitterTest {
 
+    String testStr = "123";//failcase : 1234
     @Test
     void hittingTest(){
-        TestHitterRepository h = new TestHitterRepository();
-        h.hitting();
+        ByteArrayInputStream in = new ByteArrayInputStream(testStr.getBytes());
+        System.setIn(in);
+        TestHitterRepository hiter = new TestHitterRepository();
+        hiter.hitting();
     }
 
 }
