af41c9b147d7640ed32adb9b69252616651ce2ea
Heonseop <shinsub6120@gmail.com>
2022-04-18 05:24:01 +0900

    Feat : Game 클래스 구현

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,58 @@
+package baseball.domain;
+
+import baseball.domain.player.Player;
+import java.util.List;
+
+public class Game {
+    GameStatus gameStatus;
+    GameRule gameRule;
+    Player computer;
+    Player user;
+
+    public Game(Player computer, Player user, GameRule gameRule) {
+        this.gameStatus = GameStatus.PLAY;
+        this.gameRule = gameRule;
+        this.computer = computer;
+        this.user = user;
+    }
+
+    public void setGameStatus(GameStatus gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+    public GameStatus getGameStatus() {
+        return this.gameStatus;
+    }
+
+    public boolean isEndGame() {
+        return this.gameStatus == GameStatus.END;
+    }
+
+    public GameRule getGameRule() {
+        return gameRule;
+    }
+
+    public void setGameRule(GameRule gameRule) {
+        this.gameRule = gameRule;
+    }
+
+    public Player getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Player computer) {
+        this.computer = computer;
+    }
+
+    public Player getUser() {
+        return user;
+    }
+
+    public void setUser(Player user) {
+        this.user = user;
+    }
+
+    public void playGame(List<Integer> inputNumbers){
+        user.setNumbers(inputNumbers);
+    }
+}
