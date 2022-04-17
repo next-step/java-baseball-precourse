c4acf7269ea93afb746c70c30d397572774b43dd
hugehoo <tbnsok40@gmail.com>
2022-04-17 11:19:33 +0900

    feature/ 프로그램 메인 메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,12 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+
 public class Application {
 
     // TODO: 프로그램 구현
     public static void main(String[] args) {
-        BaseballGame.start();
+        BaseballGameController baseballGame = new BaseballGameController();
+        baseballGame.start();
     }
 }
