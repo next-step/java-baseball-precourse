aad176be342bdcdd6785a522f99188a18f7afdc6
Jinmo Im <jinmo.im@gmail.com>
2022-04-17 21:16:14 +0900

    chore: 필요없는 import 삭제

--- src/main/java/baseball/game/NumberToIndex.java
+++ src/main/java/baseball/game/NumberToIndex.java
@@ -5,8 +5,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import static baseball.game.GameConfig.*;
-
 public class NumberToIndex {
     private Map<Integer, Integer> m = new HashMap<>();
     private int index = 0;
