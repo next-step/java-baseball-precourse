245fc94866777636b9e04f57b66b7d49d58ca131
Heonseop <shinsub6120@gmail.com>
2022-04-18 04:32:40 +0900

    Feat : GameRule, GameStatus 구현

---
+++ src/main/java/baseball/domain/GameRule.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+public class GameRule {
+    public int MIN_NUM = 1;
+    public int MAX_NUM = 9;
+    public int SIZE = 3;
+
+    public GameRule() {
+    }
+
+    public GameRule(int minNum, int maxNum, int size) {
+        this.MIN_NUM = minNum;
+        this.MAX_NUM = maxNum;
+        this.SIZE = size;
+    }
+}
