2d847d20582f3593fcc61632fde2924e18997190
ul8415 <ul8415@gmail.com>
2022-04-17 13:37:17 +0900

    test : 테스트코드 리팩토링
    - 개별 처리부분을 ParameterizedTest로 변경
    - assertThrows를 assertThatIllegalArgumentException로 변경

--- src/test/java/baseball/model/BaseballGameServiceTest.java
+++ src/test/java/baseball/model/BaseballGameServiceTest.java
@@ -1,13 +1,19 @@
 package baseball.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import baseball.vo.CompareResultVO;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class BaseballGameServiceTest {
 
@@ -18,7 +24,6 @@ class BaseballGameServiceTest {
     void createAnswer() {
         baseballGameService.createAnswer();
         List<Integer> answerNumberList = baseballGameService.getAnswerNumberList();
-
         assertAll(
                 () -> assertEquals(3, answerNumberList.size(), "정답은 3자리 숫자여야한다."),
                 () -> assertFalse(answerNumberList.contains(0), "정답 숫자에는 0을 포함하지 않는다."),
@@ -28,57 +33,28 @@ class BaseballGameServiceTest {
     }
 
     @DisplayName("사용자 야구숫자 입력과 정답을 비교")
-    @Test
-    void compareInputToAnswer() {
+    @ParameterizedTest(name = " 정답 725, 입력 ''{0}'' => ''{1}''스트라이크 ''{2}''볼")
+    @CsvSource(value = {"751:1:1", "931:0:0", "725:3:0"}, delimiter = ':')
+    void compareInputToAnswer(String inputNumber, int strikeCount, int ballCount) {
 
         List<Integer> answerNumberList = Arrays.asList(7, 2, 5);
-        String inputNumber = "751";
 
         CompareResultVO compareResultVO = baseballGameService.compareInputToAnswer(inputNumber, answerNumberList);
 
-        assertAll("결과는 1스트라이크 1볼이다.",
-                () -> assertEquals(1, compareResultVO.getBallCount()),
-                () -> assertEquals(1, compareResultVO.getStrikeCount())
-        );
-
-        inputNumber = "931";
-
-        CompareResultVO compareResultVO1 = baseballGameService.compareInputToAnswer(inputNumber, answerNumberList);
-
         assertAll(
-                "결과는 0스트라이크 0볼이다.",
-                () -> assertEquals(0, compareResultVO1.getBallCount()),
-                () -> assertEquals(0, compareResultVO1.getStrikeCount())
-        );
-
-        inputNumber = "725";
-
-        CompareResultVO compareResultVO2 = baseballGameService.compareInputToAnswer(inputNumber, answerNumberList);
-
-        assertAll("결과는 3스트라이크 0볼이다.",
-                () -> assertEquals(0, compareResultVO2.getBallCount()),
-                () -> assertEquals(3, compareResultVO2.getStrikeCount())
+                () -> assertEquals(strikeCount, compareResultVO.getStrikeCount(), "스트라이크"),
+                () -> assertEquals(ballCount, compareResultVO.getBallCount(), "볼")
         );
     }
 
     @DisplayName("사용자 야구숫자 입력에 대한 예외처리")
-    @Test
-    void compareInputToAnswer_throw() {
+    @ParameterizedTest(name = "입력 ''{0}'' => ''{1}''")
+    @CsvSource(value = {"test$t:숫자만 입력할 수 있습니다.", "12:3자리수가 아닙니다.", "103:0은 사용할 수 없습니다.", "779:중복된 숫자는 사용할 수 없습니다."}
+            , delimiter = ':')
+    void compareInputToAnswer_throw(String inputNumber, String expectedMessage) {
 
-        List<Integer> answerNumberList = Arrays.asList(3, 6, 9);
-
-        assertAll(() -> assertThrows(IllegalArgumentException.class,
-                () -> baseballGameService.compareInputToAnswer("tes$t", answerNumberList),
-                "숫자가 아니면 IllegalArgumentException이 발생한다."),
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> baseballGameService.compareInputToAnswer("12", answerNumberList),
-                        "3자리수가 아니면 IllegalArgumentException이 발생한다."),
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> baseballGameService.compareInputToAnswer("103", answerNumberList),
-                        "입력에 0이 포함되면 IllegalArgumentException이 발생한다."),
-                () -> assertThrows(IllegalArgumentException.class,
-                        () -> baseballGameService.compareInputToAnswer("779", answerNumberList),
-                        "입력에 중복된 숫자가 포함되면 IllegalArgumentException이 발생한다.")
-        );
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> baseballGameService.compareInputToAnswer(inputNumber, Collections.emptyList()))
+                .withMessage(expectedMessage);
     }
 }
