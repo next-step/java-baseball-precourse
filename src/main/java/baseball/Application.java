d4d8146b6f2d41e58ec854da5c7af555e1627a71
Timothy <timothy@jungnanum.com>
2022-04-18 07:21:11 +0900

    feat: Controller(Operator)가 Player로부터 숫자를 입력받아 임시저장을 한 뒤, ValidatorService를 통해 야구게임의 입력 값 검증
    OperatorController에서 ValidatorService로 야구게임 입력 값을 검증
    ApplicationTest.예외_테스트()로 Test case의 테스트 passed 포함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.controller.OperatorController;
+
 public class Application {
+    private static OperatorController operatorController = OperatorController.getInstance();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        operatorController.initOperator();
     }
 }
---
+++ src/main/java/baseball/controller/OperatorController.java
@@ -0,0 +1,65 @@
+package baseball.controller;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import baseball.service.ComputerService;
+import baseball.service.ValidatorService;
+import baseball.vo.enumtype.InterfaceMsg;
+import baseball.vo.enumtype.ValidationMsg;
+
+public class OperatorController {
+    private ComputerService computerService = ComputerService.getInstance();
+    private ValidatorService validatorService = ValidatorService.getInstance();
+
+    private String inputGameNumber;
+
+    // start: Singleton Holder
+    private OperatorController() {
+//        initOperator();
+    }
+
+    private static class InnerComputerService {
+        private static final OperatorController instance = new OperatorController();
+    }
+
+    public static OperatorController getInstance() {
+        return InnerComputerService.instance;
+    }
+    // end: Singleton Holder
+
+    public void initOperator() {
+        computerService.initGameNumber(); // 상대방(컴퓨터)가 새로운 값을 갖도록 함
+        printMenu();
+//        computerService.getGameNumberDto().getGameNumberList();
+    }
+
+    private void printMenu() {
+        System.out.println(InterfaceMsg.GAME_INFO.getValue());
+        while (true) {
+            String inputGameNumber = readLine();
+            System.out.println(inputGameNumber);
+            if (inputGameNumber.equals("2")) break;
+            System.out.print(InterfaceMsg.REQUEST_INPUT.getValue());
+            operateGame(inputGameNumber);
+        }
+    }
+
+    private void operateGame(String inputGameNumber) {
+        validateInputNumber(inputGameNumber); // 야구게임 입력 숫자 검증
+        this.inputGameNumber = inputGameNumber;
+        // 사용자에게 야구게임 입력값을 저장하도록 메시지를 보냄 - userGameNumber
+        // 컴퓨터에게 사용자가 갖고 있는 야구게임 입력값을 전달하여 그 결과에 따라 게임을 진행
+    }
+
+    /**
+     * 야구게임 입력 숫자 검증
+     * @param userGameNumber : String
+     */
+    private void validateInputNumber(String userGameNumber) {
+        ValidationMsg validationMsg = validatorService.validationUserInput(userGameNumber);
+        if (validationMsg != ValidationMsg.PROPER_TYPE) {
+            System.out.println(validationMsg.getValue());
+            throw new IllegalArgumentException();
+        }
+    }
+}
