87bf489a08d2a93735cb8a56f8bd6cec8fb1bf49
Jongmin Yoon <jongmin4943@gmail.com>
2022-04-17 16:53:51 +0900

    게임 실행 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.game.BaseBallGame;
+import baseball.game.Game;
+
 public class Application {
+    private static final Game Game = new BaseBallGame();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game.start();
     }
 }
---
+++ src/test/java/baseball/game/GameTest.java
@@ -0,0 +1,12 @@
+package baseball.game;
+
+import baseball.game.BaseBallGame;
+import baseball.game.Game;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void 게임_시작_테스트() {
+        Game game = new BaseBallGame();
+    }
+}
