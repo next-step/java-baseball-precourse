9299e22a08abe1961e81156f2d10a6a37e64ba33
ssisksl77 <ssiskl77@gmail.com>
2022-04-17 22:23:01 +0900

    [feat]: Application.java에 GameManager를 실행시켜라
     - GameManager가 의존하는 구현체들을 추가하여 초기화합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,13 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final int numberOfAttemptNumber = 3;
+        final GameManager gm = new GameManager(
+                numberOfAttemptNumber,
+                new DefaultRandomNumberGenerator(numberOfAttemptNumber),
+                camp.nextstep.edu.missionutils.Console::readLine,
+                new CommandLineView());
+
+        gm.startGame();
     }
 }
