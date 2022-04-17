8d8f06834fe4c3cff19901ca26382c0a272fbf06
kyungrae <rlarudfo93@gmail.com>
2022-04-17 09:52:48 +0900

    메소드 주석 추가 & readme 업데이트

--- src/main/java/baseball/controller/BullsAndGameController.java
+++ src/main/java/baseball/controller/BullsAndGameController.java
@@ -12,11 +12,27 @@ public class BullsAndGameController {
 
     public void start() {
         do {
-            Answer answer = getAnswer();
+            Answer answer = generateAnswer();
             BullsAndCowsGame game = new BullsAndCowsGame(answer);
             play(game);
             View.answer();
         } while (restart());
+    }
+
+    /**
+     * Answer 객체는 3 숫자가 모두 다름을 보장한다.
+     * 따라서 같은 수가 포함된 3 수가 생성되면 재귀 호출을 통해 3 수가 다름을 보장한다.
+     * @return 3 수가 모두 다른 정답 객체
+     */
+    private Answer generateAnswer() {
+        Answer result;
+        try {
+            result = new Answer(String.valueOf(Randoms.pickNumberInRange(1, 9)) +
+                    Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9));
+        } catch (IllegalArgumentException e) {
+            result = generateAnswer();
+        }
+        return result;
     }
 
     private void play(BullsAndCowsGame game) {
@@ -26,17 +42,6 @@ public class BullsAndGameController {
             Result result = game.challenge(trial);
             View.result(result);
         }
-    }
-
-    private Answer getAnswer() {
-        Answer result;
-        try {
-            result = new Answer(String.valueOf(Randoms.pickNumberInRange(1, 9)) +
-                    Randoms.pickNumberInRange(1, 9) + Randoms.pickNumberInRange(1, 9));
-        } catch (IllegalArgumentException e) {
-            result = getAnswer();
-        }
-        return result;
     }
 
     private boolean restart() {
--- src/main/java/baseball/model/Answer.java
+++ src/main/java/baseball/model/Answer.java
@@ -19,7 +19,11 @@ public class Answer {
             throw new IllegalArgumentException("Trial number range is between 1 and 9.");
     }
 
-
+    /**
+     * Answer & Trial 모두 크기가 3 이고 서로 모두 다름을 보장 하므로 N^2 알고리즘을 사용한다.
+     * @param trial 정답을 맞추기 위한 시도 (숫자가 모두 다름이 보장된다.)
+     * @return strike & ball 정보가 기록된 결과
+     */
     Result challenge(Trial trial) {
         int strikeCnt = 0, ballCnt = 0;
         for (int i = 0; i < answer.length(); i++) {
