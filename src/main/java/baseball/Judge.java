d52d1e9ba497ad8e0b7b0f6dc7e21c2821d95507
felixpark <felixpark@kakao.com>
2022-04-17 22:49:52 +0900

    Feat: Add countBall
    컴퓨터가 만든 수와 사용자가 입력한 수를 비교하여
    각 자리수에 대하여 같은 수가 다른자리에 있는 숫자의 갯수를
    리턴한다.

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,6 +1,19 @@
 package baseball;
 
+import java.util.List;
+
 public class Judge {
 
+    public int countBall(List<Integer> computer, List<Integer> player) {
+        int ball = 0;
+        for(int i = 0; i < player.size(); i++) {
+            ball += checkBall(computer, player, i);
+        }
+        return ball;
+    }
+
+    private int checkBall(List<Integer> computer, List<Integer> player, int i) {
+        return computer.contains(player.get(i)) && computer.get(i) !=  player.get(i) ? 1 : 0;
+    }
 
 }
