78cf351acceca0b68eef912bfcc4cf7c78042a84
gywjd <j07@hanmail.net>
2022-04-17 17:04:21 +0900

    refactor: 상수 생성
    - 매직 넘버
    - 메시지

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,6 +6,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.LinkedHashSet;
 
 public class Game {
+
+    public static final String GAME_RESTART = "1";
+    public static final String GAME_OVER = "2";
+
+    public static final String MESSAGE_INFO_INPUT = "숫자를 입력해주세요: ";
+    public static final String MESSAGE_INFO_SELECT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String MESSAGE_ERROR_INPUT = "입력이 올바르지 않습니다.";
+
+    public static final int ANSWER_SIZE = 3;
+    public static final int RANGE_MIN = 1;
+    public static final int RANGE_MAX = 9;
+
     public void play() {
         start();
     }
@@ -16,8 +28,9 @@ public class Game {
         // 정답 생성(서로 다른 임의의 수 3개 선택)
         LinkedHashSet<Integer> answer = new LinkedHashSet<>();
 
-        while(answer.size() < 3) {
-            answer.add(Randoms.pickNumberInRange(1, 9));
+        while(answer.size() < ANSWER_SIZE) {
+            answer.add(Randoms.pickNumberInRange(RANGE_MIN, RANGE_MAX));
+
         }
         System.out.println(answer);
 
@@ -28,7 +41,7 @@ public class Game {
         System.out.println("게임 진행");
 
         // 게임 진행
-        System.out.print("숫자를 입력해주세요: ");
+        System.out.print(MESSAGE_INFO_INPUT);
         String input = Console.readLine();
         System.out.println(input);
 
@@ -46,14 +59,15 @@ public class Game {
     private void end() {
         System.out.println("게임 종료");
 
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(MESSAGE_INFO_SELECT);
+
         String input = Console.readLine().trim();
 
-        if(!input.equals("1") && !input.equals("2")) {
-            throw new IllegalArgumentException("입력이 올바르지 않습니다.");
+        if(!input.equals(GAME_RESTART) && !input.equals(GAME_OVER)) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_INPUT);
         }
 
-        if(input.equals("1")) {
+        if(input.equals(GAME_RESTART)) {
             start();
         }
     }
--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -4,23 +4,35 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 public class Umpire {
+
+    public static final String HINT_STRIKE = "스트라이크 ";
+    public static final String HINT_BALL = "볼 ";
+    public static final String HINT_NOTHING = "낫싱";
+
+    public static final String MESSAGE_INFO_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    public static final String MESSAGE_ERROR_INPUT_3DIGIT = "3자리의 숫자만 입력가능합니다.";
+    public static final String MESSAGE_ERROR_INPUT_INVALID = "숫자(1~9)가 아닌 문자가 입력되었습니다.";
+    public static final String MESSAGE_ERROR_INPUT_DUPLICATE = "중복된 숫자가 입력되었습니다.";
+
+    public static final int INPUT_LENGTH = 3;
+
     public void validate(String input) {
-        if(input.length() != 3) {
-            throw new IllegalArgumentException("3자리의 숫자만 입력가능합니다.");
+        if(input.length() != INPUT_LENGTH) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_INPUT_3DIGIT);
         }
 
         LinkedHashSet<Integer> temp = new LinkedHashSet<>();
         for(int i = 0; i < input.length(); i++) {
             int tempValue = input.charAt(i) - '0';
             if(tempValue < 1 || tempValue > 9) {
-                throw new IllegalArgumentException("숫자(1~9)가 아닌 문자가 입력되었습니다.");
+                throw new IllegalArgumentException(MESSAGE_ERROR_INPUT_INVALID);
             }
 
             temp.add(tempValue);
         }
 
-        if(temp.size() != 3) {
-            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+        if(temp.size() != INPUT_LENGTH) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_INPUT_DUPLICATE);
         }
     }
 
@@ -48,15 +60,15 @@ public class Umpire {
         }
 
         if(strikeCount == 3) {
-            System.out.println(strikeCount + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            System.out.println(strikeCount + HINT_STRIKE);
+            System.out.println(MESSAGE_INFO_SUCCESS);
             return true;
         }
 
-        String hint_strike = (strikeCount == 0) ? "" : (strikeCount + "스트라이크 ");
-        String hint_ball = (ballCount == 0) ? "" : (ballCount + "볼 ");
+        String hint_strike = (strikeCount == 0) ? "" : (strikeCount + HINT_STRIKE);
+        String hint_ball = (ballCount == 0) ? "" : (ballCount + HINT_BALL);
         if(strikeCount == 0 && ballCount == 0) {
-            System.out.println("낫싱");
+            System.out.println(HINT_NOTHING);
         } else {
             System.out.println(hint_ball + hint_strike);
         }
