a8649d8ff547b59bf02aba765678878945f3e7cb
Cronian <cronian89@gmail.com>
2022-04-18 00:38:11 +0900

    update Application.java
    - 실행 가능하도록 프로그램 셋팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.controller.BaseBallGameController;
+import baseball.controller.BaseBallGameControllerImpl;
+import baseball.model.BaseBallGameModel;
+import baseball.model.BaseBallGameModelImpl;
+import baseball.view.BaseBallGameView;
+import baseball.view.BaseBallGameViewImpl;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBallGameModel model = new BaseBallGameModelImpl();
+        BaseBallGameView view = new BaseBallGameViewImpl();
+        BaseBallGameController controller = new BaseBallGameControllerImpl(model,view);
+        controller.gameStart();
     }
 }
