4f822b96c40f0ced93b90f3b3e503438d35f286b
diqksrk <diqksrk123@naver.com>
2022-04-18 22:29:31 +0900

    refactor: 메시지 구체화 추가

---
+++ src/main/java/baseball/common/Constants.java
@@ -0,0 +1,12 @@
+package baseball.common;
+
+public class Constants {
+    public static final int  ANSWER_RANGE_MIN_RANDOM_NUM = 1;
+    public static final int  ANSWER_RANGE_MAX_RANDOM_NUM = 9;
+    public static final int  USER_NUMBER_LENGTH = 3;
+
+    public static final int  INQUIRE_PLAYER_WIN_STRIKE_VALUE = 3;
+
+    public static final String GAME_STATUS_RESTART_NUMBER = "1";
+    public static final String GAME_STATUS_END_NUMBER     = "2";
+}
