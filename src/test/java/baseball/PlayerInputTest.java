de0e670ddcb01e9ecd8f47907732ec8d85069ef9
felixpark <felixpark@kakao.com>
2022-04-18 21:36:43 +0900

    Test: Test for PlayInput.java
    사용자로부터 들어오는 입력값 처리를 올바르게 하는지 테스트

--- src/test/java/baseball/PlayerInputTest.java
+++ src/test/java/baseball/PlayerInputTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.*;
@@ -43,9 +44,32 @@ class PlayerInputTest {
         System.setOut(new PrintStream(outputStream));
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
-
         assertThatThrownBy(() -> {
             List<Integer> playerNumbers = playerInput.getPlayerNumber();
+        }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("잘못된 숫자");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true","2,false"})
+    @DisplayName("게임 재시작 및 종료를 위한 입력 테스트(정상입력)")
+    void doAgain(String number, String expected) {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        InputStream inputStream = new ByteArrayInputStream(number.getBytes());
+        System.setIn(inputStream);
+        assertThat(playerInput.doAgain()).isEqualTo(Boolean.parseBoolean(expected));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"12", "3"})
+    @DisplayName("게임 재시작 및 종료를 위한 입력 테스트(비정상입력)")
+    void doAgainForIllegalArgumentException(String number) {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        InputStream inputStream = new ByteArrayInputStream(number.getBytes());
+        System.setIn(inputStream);
+        assertThatThrownBy(() -> {
+            playerInput.doAgain();
         }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("잘못된 숫자");
     }
 }
