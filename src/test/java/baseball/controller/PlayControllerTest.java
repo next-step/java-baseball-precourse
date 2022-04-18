a82ef9fc27ba119c39a8aee700e9ef05589d23eb
Krad <tokraddu@gmail.com>
2022-04-18 20:09:44 +0900

    feat: 플레이 컨트롤러
    스트라이크 존 초기화
    투수 초기화
    심판 초기화
    투수 입력 받은 값 심판 전달
    심판 판정 및 게임 진행 컨트롤
    입력값에 따라 게임 종료
    
    Closes #TASK_BASE_007

---
+++ src/test/java/baseball/controller/PlayControllerTest.java
@@ -0,0 +1,12 @@
+package baseball.controller;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayControllerTest {
+    @Test
+    public void play(){
+    }
+}
