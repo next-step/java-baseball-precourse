8ea5ce3d6621bbf5b73d5a278947807f615398a0
Timothy <timothy@jungnanum.com>
2022-04-18 17:44:09 +0900

    test: Player가 입력한 숫자값을 Computer에게 전달하여 보관
    Operator를 통해, PlayerService가 입력한 숫자값을 Computer에게 전달하여 보관 후, 입력 값과 Computer가 가진 값을 서로 비교하여 테스트
    
    BREAKING CHANGE: player가_가진_숫자값과_computer가_가진_숫자값_비교() test case 추가

--- src/test/java/baseball/controller/OperatorControllerTest.java
+++ src/test/java/baseball/controller/OperatorControllerTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import baseball.service.ComputerService;
+import baseball.service.ComputerServiceTest;
+import baseball.service.PlayerService;
 import baseball.service.ValidatorServiceTest;
 import baseball.vo.enumtype.InterfaceMsg;
 import baseball.vo.enumtype.ValidationMsg;
@@ -15,6 +17,10 @@ import org.junit.jupiter.api.Test;
 
 public class OperatorControllerTest  extends NsTest {
     private ComputerService computerService = ComputerService.getInstance();
+    private ComputerServiceTest computerServiceTest = ComputerServiceTest.getInstance();
+    private PlayerService playerService = PlayerService.getInstance();
+
+    private String inputGameNumber;
 
     @Test
     void Computer에게_새로운_야구게임이_시작됨을_알리고_Computer로부터_받은_값_출력() {
@@ -54,7 +60,25 @@ public class OperatorControllerTest  extends NsTest {
         });
     }
 
+    @Test
+    void player가_가진_숫자값과_computer가_가진_숫자값_비교() {
+        // give
+        String inputGameNumber = "365";
+        // when
+        playerService.setPlayerInputGameNumber(inputGameNumber);
+        computerServiceTest.setPlayerInputGameNumberList(playerService.getPlayerInputGameNumberDto().getGameNumberList());
+        // then
+        assertEquals(inputGameNumber, listInteger2String(computerServiceTest.getPlayerInputGameNumberList()));
+    }
 
+    /**
+     * List<Integer>를 입력받아, 하나의 문자열로 반환
+     * @param playerInputList
+     * @return
+     */
+    private String listInteger2String(List<Integer> playerInputList) {
+        return playerInputList.toString().replaceAll("[^0-9]", "");
+    }
 
     @Override
     protected void runMain() {
