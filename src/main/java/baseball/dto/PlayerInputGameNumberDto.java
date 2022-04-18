f836ceb42a2377edf099f33fda069ba51fe64fe1
Timothy <timothy@jungnanum.com>
2022-04-18 17:08:53 +0900

    style: 접근제한자 추가
    소스에 private 추가

--- src/main/java/baseball/dto/PlayerInputGameNumberDto.java
+++ src/main/java/baseball/dto/PlayerInputGameNumberDto.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class PlayerInputGameNumberDto {
 
     private String gameNumber;
-    List<Integer> gameNumberList;
+    private List<Integer> gameNumberList;
 
     public PlayerInputGameNumberDto() {
     }
