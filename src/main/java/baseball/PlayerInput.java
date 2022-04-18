2fc73fde177a1e2600c55aa6f2cce9e9df23a0eb
felixpark <felixpark@kakao.com>
2022-04-18 21:35:58 +0900

    Feat: Add PlayerInput.java
    게임 재실행을 위한 입력과 숫자 추측을 위한 입력을 받는 클래스.

--- src/main/java/baseball/PlayerInput.java
+++ src/main/java/baseball/PlayerInput.java
@@ -9,7 +9,16 @@ public class PlayerInput {
 
     static final String messageForRequireNumber = "숫자를 입력해주세요 :";
     static final String messageForIllegalNumber = "잘못된 숫자를 입력하였습니다.";
+    static final String messageForMoreGame = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+
+    public boolean doAgain() throws IllegalArgumentException{
+        System.out.println(messageForMoreGame);
+        String moreGame = Console.readLine();
+        if(!moreGame.equals("1") && !moreGame.equals("2"))
+            throw new IllegalArgumentException(messageForIllegalNumber);
+        return moreGame.equals("1");
+    }
 
 
     public List<Integer> getPlayerNumber() throws IllegalArgumentException {
