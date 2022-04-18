44bea9bf033bea0e965eb25b536fa5131e96f58a
deokmoon <deok2moon@gmail.com>
2022-04-18 23:04:53 +0900

    Feat: User 테스트코드 작성 입력부 검증 구현(길이 및 타입)

--- src/test/java/baseball/model/UserTest.java
+++ src/test/java/baseball/model/UserTest.java
@@ -7,15 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserTest {
     @Test()
     void 입력한_숫자_길이_테스트() {
+        // given, when
         Player user = new User("123", 3);
-
+        // then
         assertThat(user.getNumbers().size()).isEqualTo(3);
     }
 
     @Test()
     void 입력한_숫자_정상_저장_확인() {
+        // given, when
         Player user = new User("123", 3);
-
+        // then
         assertThat(user.getNumbers().get(0)).isEqualTo(1);
         assertThat(user.getNumbers().get(1)).isEqualTo(2);
         assertThat(user.getNumbers().get(2)).isEqualTo(3);
