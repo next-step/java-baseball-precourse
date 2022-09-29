8db1627b238b7a1e7d5f501b098a4f7c0e6f20d0
felixpark <felixpark@kakao.com>
2022-09-29 16:12:19 +0900

    test: 숫자값 검증 기능에 대한 테스트
    -값의 자리수 검증 테스트
    -값이 1부터 9까지의 숫자로만 이루어져 있는지 검증 테스트
    -값이 서로 다른 수로 이루어져 있는지 검증 테스트

---
+++ src/test/java/baseball/BallValidatorTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BallValidatorTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:false", "123:true","1234:false"}, delimiter = ':')
+    @DisplayName("값의 자리수가 세자리인지 검증하는 기능에 대한 테스트")
+    void isInputHasRightLength(String input, boolean expected) {
+        assertEquals(BallValidator.isInputHasRightLength(input), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"000:false", "111:true","11a:false","aaa:false"}, delimiter = ':')
+    @DisplayName("값이 1부터 9까지의 숫자로만 이루어져 있는지 검증하는 기능에 대한 테스트")
+    void isInputContainsOnlyOneToNine(String input, boolean expected) {
+        assertEquals(BallValidator.isInputContainsOnlyOneToNine(input), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:true", "112:false", "111:false"}, delimiter = ':')
+    @DisplayName("값이 서로 다른 수로 이루어져 있는지 검증하는 기능에 대한 테스트")
+    void isInputContainsUniqueNumbers(String input, boolean expected) {
+        assertEquals(BallValidator.isInputContainsUniqueNumbers(input), expected);
+    }
+}
