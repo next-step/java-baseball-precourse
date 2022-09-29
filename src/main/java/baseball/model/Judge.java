e0e9371f67745af0acefbd7241f9553f8ef48f30
jungpil98 <jungpil98@ajou.ac.kr>
2022-09-29 14:14:03 +0900

    feat: 스트라이크, 볼, 낫싱 판별기능 추가

--- src/main/java/baseball/model/Judge.java
+++ src/main/java/baseball/model/Judge.java
@@ -1,5 +1,8 @@
 package baseball.model;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class Judge {
 
     private static int ballCount;
@@ -7,20 +10,14 @@ public class Judge {
 
     private static final int STRIKE_OUT = 3;
 
+    private final StringBuilder ballCountResult = new StringBuilder();
+
     public static int getBallCount() {
         return ballCount;
     }
 
     public static int getStrikeCount() {
         return strikeCount;
-    }
-
-    public static void increaseStrikeCount() {
-        strikeCount += 1;
-    }
-
-    public static void increaseBallCount() {
-        ballCount += 1;
     }
 
     public Judge() {
@@ -34,5 +31,69 @@ public class Judge {
 
     public static boolean isStrikeOut() {
         return strikeCount == STRIKE_OUT;
+    }
+
+    @Override
+    public String toString() {
+        noteBallCount();
+        return ballCountResult.toString();
+    }
+
+    public static void countBalls(List<String> computerNumbers, String userInputNumbers) {
+        Judge judge = new Judge();
+
+        for (int i=0; i<computerNumbers.size(); i++) {
+            String element = computerNumbers.get(i);
+            countBall(userInputNumbers, element, i);
+        }
+    }
+
+    private static void countBall(String userInputNumbers, String element, int i) {
+        if (userInputNumbers.contains(element) && userInputNumbers.indexOf(element) == i) {
+            increaseStrikeCount();
+        }
+        if (userInputNumbers.contains(element) && userInputNumbers.indexOf(element) != i) {
+            increaseBallCount();
+        }
+    }
+
+    private void noteBallCount() {
+        noteBallToBallCount();
+        appendSpace();
+        noteStrikeToBallCount();
+        appendSpace();
+        noteNothingToBallCount();
+    }
+
+    private void noteStrikeToBallCount() {
+        if (strikeCount > 0) {
+            ballCountResult.append(String.format("%d스트라이크", strikeCount));
+        }
+    }
+
+    private void noteBallToBallCount() {
+        if (ballCount > 0) {
+            ballCountResult.append(String.format("%d볼", ballCount));
+        }
+    }
+
+    private void noteNothingToBallCount() {
+        if (strikeCount == 0 && ballCount == 0) {
+            ballCountResult.append("낫싱");
+        }
+    }
+
+    private void appendSpace() {
+        if (ballCountResult.length() > 0) {
+            ballCountResult.append(" ");
+        }
+    }
+
+    private static void increaseStrikeCount() {
+        strikeCount += 1;
+    }
+
+    private static void increaseBallCount() {
+        ballCount += 1;
     }
 }
