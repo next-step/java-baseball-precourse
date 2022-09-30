ec84e175208eaec03416dcf6557baa4544f78be4
felixpark <felixpark@kakao.com>
2022-09-30 15:54:47 +0900

    chore: assertJ 로 변경

--- src/test/java/baseball/BallValidatorTest.java
+++ src/test/java/baseball/BallValidatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class BallValidatorTest {
 
@@ -12,20 +12,20 @@ class BallValidatorTest {
     @CsvSource(value = {"1:false", "123:true","1234:false"}, delimiter = ':')
     @DisplayName("값의 자리수가 세자리인지 검증하는 기능에 대한 테스트")
     void isInputHasRightLength(String input, boolean expected) {
-        assertEquals(BallValidator.isInputHasRightLength(input), expected);
+        assertThat(BallValidator.isInputHasRightLength(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"000:false", "111:true","11a:false","aaa:false"}, delimiter = ':')
     @DisplayName("값이 1부터 9까지의 숫자로만 이루어져 있는지 검증하는 기능에 대한 테스트")
     void isInputContainsOnlyOneToNine(String input, boolean expected) {
-        assertEquals(BallValidator.isInputContainsOnlyOneToNine(input), expected);
+        assertThat(BallValidator.isInputContainsOnlyOneToNine(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"123:true", "112:false", "111:false"}, delimiter = ':')
     @DisplayName("값이 서로 다른 수로 이루어져 있는지 검증하는 기능에 대한 테스트")
     void isInputContainsUniqueNumbers(String input, boolean expected) {
-        assertEquals(BallValidator.isInputContainsUniqueNumbers(input), expected);
+        assertThat(BallValidator.isInputContainsUniqueNumbers(input)).isEqualTo(expected);
     }
 }
