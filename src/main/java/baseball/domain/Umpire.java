1a3e965c0400856eea197c02ddc44209952c2a63
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-01 15:11:22 +0900

    feat: 게임 플레이어가 입력한 숫자에 대한 결과 출력
    - 스트라이크, 볼

---
+++ src/main/java/baseball/domain/Umpire.java
@@ -0,0 +1,41 @@
+package baseball.domain;
+
+import java.util.LinkedList;
+
+public class Umpire {
+
+    public static final String IS_STRIKE = "스트라이크 ";
+    public static final String IS_BALL = "볼 ";
+
+    public void judge() {
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        LinkedList<Integer> answer = new LinkedList<>();
+        LinkedList<Integer> input = new LinkedList<>();
+
+        for (int index = 0; index < answer.size(); index++) {
+            strikeCount = Judgement.getStrikeCount(strikeCount, answer, input, index);
+            ballCount = Judgement.getBallCount(ballCount, answer, input, index);
+        }
+
+        call(strikeCount, ballCount);
+    }
+
+    private void call(int strikeCount, int ballCount) {
+        String isStrike = "";
+        String isBall = "";
+
+        if (strikeCount > 0) {
+            isStrike = strikeCount + IS_STRIKE;
+        }
+        if (ballCount > 0) {
+            isBall = ballCount + IS_BALL;
+        }
+
+        if (strikeCount > 0 || ballCount > 0) {
+            System.out.println(isBall + isStrike);
+        }
+    }
+
+}
