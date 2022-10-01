8f47d75c28733ef9bd22fd92b61e51a93929b463
jungpil98 <jungpil98@ajou.ac.kr>
2022-10-01 23:23:20 +0900

    chore: 패키지 최적화

--- src/test/java/baseball/model/UserTest.java
+++ src/test/java/baseball/model/UserTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserTest {
 
