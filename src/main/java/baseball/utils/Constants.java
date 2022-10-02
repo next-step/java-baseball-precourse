d1b9f0643c106653f3a28f7cdd3f60ab32810e9e
STO440 <supportyoo@sk.com>
2022-10-03 03:07:25 +0900

    feat(controller, view): Match Controller, View 추가
    add MatchView.java, MatchViewTest.java, MatchController.java, InputValidator.java, ConvertUtil.java, Constants.java
    
    class added:
     - Match Controller 개발 - 메인 야구게임 로직
     - Match Controller 기능 테스트

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,20 @@
+package baseball.utils;
+
+public class Constants {
+    public static final int MAX_NUMBERS = 3;
+    public static final String CONTINUE_FLAG = "1";
+    public static final int STOP_FLAG = 2;
+    public static final int INPUT_MIN = 0;
+    public static final int INPUT_MAX = 999;
+    public static final String TYPE_NUMBER_PLEASE = "숫자를 입력해주세요 : ";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
+    public static final String INCORRECT_INPUT_TYPE = "숫자가 아닌 값을 입력하였습니다.";
+    public static final String INCORRECT_SIZE_OF_NUMBER = "자릿수가 일치하지 않습니다.";
+    public static final String DUPLECATE_NUMBER_EXISTS = "중복된 입력값이 존재합니다.";
+    public static final String OUT_OF_INPUT_RANGE = "입력 범위를 초과하였습니다.";
+    public static final String THREE_STRIKE_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String CONTINUE_GAME_OR_NOT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
