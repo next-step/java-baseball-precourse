ef4d73a7ab6b365c5f4d63f3a612272131019aa6
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-02 16:44:18 +0900

    feat: 선택한 3개의 숫자를 모두 맞히면 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,11 +4,19 @@ import baseball.domain.Game;
 import java.util.LinkedHashSet;
 
 public class Application {
+
+    public static final String IS_WIN = "3스트라이크 ";
+    public static final String MESSAGE_INFO_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+
     public static void main(String[] args) {
         Game game = new Game();
         LinkedHashSet<Integer> answer = game.generateAnswer();
 
-        String result = game.play(answer);
-        System.out.println(result);
+        String result = "";
+        while (!result.equals(IS_WIN)) {
+            result = game.play(answer);
+            System.out.println(result);
+        }
+        System.out.println(MESSAGE_INFO_WIN);
     }
 }
