45b29b1c1d9dad8c2b12f299bdd45e6395b5b6f4
sevoon0909 <sevoon0909@icloud.com>
2022-10-02 17:51:42 +0900

    feat:기본 메세지 및 초기 상수값

---
+++ src/main/java/baseball/common/COMMON_CONSTANT.java
@@ -0,0 +1,11 @@
+package baseball.common;
+
+public class COMMON_CONSTANT {
+
+	public static final Integer NUMBER_RANGE = 3;
+	public static final Integer START_RANGE = 1;
+	public static final Integer END_RANGE = 9;
+	public static final boolean COMPLETE = true;
+	public static final boolean FAIL = false;
+
+}
