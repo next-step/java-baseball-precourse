18e21aaadf7beaf507aa6f1a0078539bc116e5c6
unknown <jiseong4577@gmail.com>
2022-10-03 02:58:33 +0900

    사용자 숫자 입력 및 컴퓨터 숫자 설정
    Randoms.pickNumberInRange(0,999)을 사용하여 컴퓨터 숫자 설정,
    Console.readLine()을 사용하여 사용자 입력 값 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String computerPickNumString = Randoms.pickNumberInRange(0,999)+ "";
+
+        while(true){
+            System.out.print("숫자를 입력해주세요: ");
+            String inputNumString = Console.readLine();
+        }
+
     }
+
 }
