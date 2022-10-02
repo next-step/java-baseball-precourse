b61f04b17ad67b5f3ec0ab7dfa13bf10cfbd568a
STO440 <supportyoo@sk.com>
2022-10-03 03:08:28 +0900

    feat(Application): 메인 로직 추가
    edit Application.java, ApplicationTest.java
    
    class added:
     - Application - MatchController 연결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.MatchController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MatchController matchController = new MatchController();
+        matchController.startMatchWithComputer();
     }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,10 @@
 package baseball;
 
+import baseball.controller.MatchController;
+import baseball.model.Match;
+import baseball.model.Player;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -10,6 +14,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void existRestart() {
+        Player inputPlayer = new Player();
+        Player guessPlayer = new Player();
+        guessPlayer.setNumbers(Arrays.asList(3, 9, 2));
+        Match match = new Match(inputPlayer, guessPlayer);
+        match.setInputPlayerNumber(Arrays.asList(8, 7, 1));
+        assertThat(match.getPlayerBallCount()).isEqualTo(0);
+        assertThat(match.getPlayerStrikeCount()).isEqualTo(0);
+    }
+
+    /*@Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
@@ -18,10 +33,10 @@ class ApplicationTest extends NsTest {
                 },
                 1, 3, 5, 5, 8, 9
         );
-    }
+    }*/
 
     @Test
-    void 예외_테스트() {
+    void exceptionTest() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
