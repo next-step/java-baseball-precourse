7ad80eb4eb7b09eb42a6c2377878aafb62548bec
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-02 17:27:37 +0900

    feat: 게임 시작 여부 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,24 @@
 package baseball;
 
 import baseball.domain.Game;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.LinkedHashSet;
 
 public class Application {
 
     public static final String IS_WIN = "3스트라이크 ";
     public static final String MESSAGE_INFO_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    public static final String MESSAGE_INFO_SELECT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String MESSAGE_INFO_GAME_OVER = "게임 종료";
+    public static final String MESSAGE_ERROR_INPUT = "입력이 올바르지 않습니다.";
+    public static final String GAME_RESTART = "1";
+    public static final String GAME_OVER = "2";
 
     public static void main(String[] args) {
+        start();
+    }
+
+    private static void start() {
         Game game = new Game();
         LinkedHashSet<Integer> answer = game.generateAnswer();
 
@@ -18,5 +28,22 @@ public class Application {
             System.out.println(result);
         }
         System.out.println(MESSAGE_INFO_WIN);
+        end();
+    }
+
+    private static void end() {
+        System.out.println(MESSAGE_INFO_SELECT);
+        String input = Console.readLine().trim();
+
+        if (!input.equals(GAME_RESTART) && !input.equals(GAME_OVER)) {
+            throw new IllegalArgumentException(MESSAGE_ERROR_INPUT);
+        }
+
+        if (input.equals(GAME_RESTART)) {
+            start();
+        }
+        if (input.equals(GAME_OVER)) {
+            System.out.println(MESSAGE_INFO_GAME_OVER);
+        }
     }
 }
