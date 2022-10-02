0a9a765eb798c0c6fbed414eb7ffa2633fe37fee
sevoon0909 <sevoon0909@icloud.com>
2022-10-02 17:53:05 +0900

    feat:앱실행 모듈

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import baseball.service.BaseBallService;
+
 public class Application {
+
+    public static void processRun(BaseBallService service){
+
+        while(service.isAlive()){
+            service.viewingOutputText();
+            service.processRun();
+        }
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램 구현Z
+        BaseBallService baseBallService = new BaseBallService();
+        processRun(baseBallService);
+
     }
 }
