83b1199183a08e24c1b1bcd95e47007b55b66fae
nayoung <nayoung.tech@gmail.com>
2022-10-03 19:04:52 +0900

    fix: Change gameNumbers data type to Set of integer

--- src/main/java/baseball/game/model/GameNumber.java
+++ src/main/java/baseball/game/model/GameNumber.java
@@ -3,13 +3,13 @@ package baseball.game.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashSet;
-
+import java.util.Set;
 
 public class GameNumber {
     public int ANSWER_LENGTH = 3;
 
     public void generateAnswer() {
-        HashSet<Integer> answer = new HashSet<>();
+        Set<Integer> answer = new HashSet<>();
         while (answer.size() < ANSWER_LENGTH) {
             int number = generateRandomNumber();
             answer.add(number);
